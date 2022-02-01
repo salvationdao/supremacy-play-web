a7e2be944c3ac4f6f797d62294a355216841a7e5
James Li <jayli3n@gmail.com>
2022-02-01 13:57:58 +0800

    Add in server env variable (#7)
    * Mech stats
    
    * Add env variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT_HOSTNAME = '10.25.26.181:8084'
+export const API_ENDPOINT_HOSTNAME = process.env.REACT_APP_GAME_SERVER_HOST || 'localhost:8084'
 export const LOG_API_CALLS = true
 export const UI_OPACITY = 0.93
 export const NOTIFICATION_TIME = 10000
