156ba6326b79b4e07ddadbc52eb9266476ee72a6
James Li <jayli3n@gmail.com>
2022-02-02 10:42:57 +0800

    Added live stream src as env variable

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ npm start
 NINJA_NPM_TOKEN=[get from BitWarden]
 REACT_APP_GAME_SERVER_HOST=[game server address]
 REACT_APP_PASSPORT_SERVER_HOST=[passport server address]
+REACT_APP_STREAM_SITE=[the src of the video live stream]
 ```
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const GAME_SERVER_HOSTNAME = process.env.REACT_APP_GAME_SERVER_HOST || 'localhost:8084'
 export const PASSPORT_SERVER_HOSTNAME = process.env.REACT_APP_PASSPORT_SERVER_HOST || 'localhost:8086'
+export const STREAM_SITE = process.env.REACT_APP_STREAM_SITE
 export const LOG_API_CALLS = true
 export const UI_OPACITY = 0.93
 export const NOTIFICATION_TIME = 10000
