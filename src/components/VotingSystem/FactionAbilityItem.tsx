2f2e5a9858d57c1c9a3707373d18cae8b62c90a1
James Li <jayli3n@gmail.com>
2022-02-08 16:57:30 +0800

    Done with faction abilities #27 (#38)
    * Remove notification container, moved to component
    
    * Keep live voting chart within iframe when resize
    
    * Refactor
    
    * Disable vote buttons if on cooldown
    
    * Top message not selectable
    
    * Some Ui rearrange for faction actions
    
    * Style price
    
    * Add price check ticker arrow
    
    * Style price
    
    * Battle ability on its own and slides away
    
    * Up to faction abilities
    
    * Todo.txt
    
    * Todo.txt from game testing
    
    * add new endpoint
    
    * Increase notification duration
    
    * Winner receives factionAbility and not battleAbility
    
    * Relocate notifications to the right and voting actions take full
    
    * Battle ability doesnt slide away, it stays but just opaque
    
    * Top message not inside the voting section, rid battle top msg
    
    * Fix live chart resize bounds
    
    * Ton of refactoring and faction ability added
    
    * Faction ability show values
    
    * Fix dupe key error
    
    * Fix faction ability sup cost
    
    * 6 decimals
    
    * 2 decimals
    
    * Disable voting button hover for faction abilities
    
    * Added contribution bar
    
    Co-authored-by: lolivise <tom790924@gmail.com>

--- src/components/VotingSystem/FactionAbilityItem.tsx
+++ src/components/VotingSystem/FactionAbilityItem.tsx
@@ -1,6 +1,6 @@
 import { Theme } from '@mui/material/styles'
 import { useTheme } from '@mui/styles'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Fade, Stack, Typography } from '@mui/material'
 import BigNumber from 'bignumber.js'
 import { useCallback, useEffect, useState } from 'react'
 import { ClipThing, VotingButton } from '..'
@@ -9,63 +9,56 @@ import HubKey from '../../keys'
 import { zoomEffect } from '../../theme/keyframes'
 import { colors } from '../../theme/theme'
 import { FactionAbility, FactionAbilityTargetPrice } from '../../types'
+import { useToggle } from '../../hooks'
 
-const ContributionBar = ({ isVoting, isCooldown }: { isVoting: boolean; isCooldown: boolean }) => {
-    const { factionsColor } = useGame()
-
-    // Array order is (Red Mountain, Boston, Zaibatsu). [[colorArray], [ratioArray]]
-    const [voteRatio, setVoteRatio] = useState<[number, number, number]>([33, 33, 33])
-
-    useEffect(() => {
-        setVoteRatio([33, 33, 33])
-    }, [isCooldown])
-
-    const subBar = useCallback(
-        (color: string, ratio: number) => (
-            <Box
-                sx={{
-                    position: 'relative',
-                    width: isCooldown ? '33.33%' : `${ratio}%`,
-                    height: '100%',
-                    transition: 'all .25s',
-                    opacity: isVoting ? 1 : 0.4,
-                    backgroundColor: color,
-                }}
-            >
-                <Typography
-                    key={ratio}
-                    variant="caption"
-                    sx={{
-                        position: 'absolute',
-                        top: -16,
-                        left: '50%',
-                        transform: 'translateX(-50%)',
-                        color,
-                        fontWeight: 'fontWeightBold',
-                        animation: `${zoomEffect()} 300ms ease-out`,
-                    }}
-                >
-                    {Math.round(ratio)}%
-                </Typography>
-            </Box>
-        ),
-        [isVoting, isCooldown],
-    )
+const ContributionBar = ({
+    color,
+    initialTargetCost,
+    currentSups,
+    supsCost,
+}: {
+    color: string
+    initialTargetCost: BigNumber
+    currentSups: BigNumber
+    supsCost: BigNumber
+}) => {
+    const percent = currentSups.dividedBy(supsCost).toNumber() * 100
 
     return (
-        <Box sx={{ width: '100%', px: 1.5, py: 1, pb: 1.2, backgroundColor: '#00000050', borderRadius: 1 }}>
+        <Stack
+            direction="row"
+            alignItems="center"
+            spacing={1}
+            sx={{ width: '100%', px: 1.5, py: 1.2, backgroundColor: '#00000050', borderRadius: 1 }}
+        >
             <Stack
                 direction="row"
-                alignSelf="stretch"
                 alignItems="center"
-                justifyContent="center"
-                sx={{ mt: 1.6, height: 5.5, px: 0.5 }}
+                justifyContent="flex-start"
+                sx={{ flex: 1, height: 5.5, backgroundColor: `${colors.text}20` }}
             >
-                {subBar(factionsColor?.redMountain || '#C24242', voteRatio[0])}
-                {subBar(factionsColor?.boston || '#428EC1', voteRatio[1])}
-                {subBar(factionsColor?.zaibatsu || '#FFFFFF', voteRatio[2])}
+                <Box
+                    sx={{
+                        width: `${percent}%`,
+                        height: '100%',
+                        transition: 'all .25s',
+                        backgroundColor: color,
+                    }}
+                ></Box>
             </Stack>
-        </Box>
+
+            <Typography
+                key={percent}
+                variant="caption"
+                sx={{
+                    fontWeight: 'fontWeightBold',
+                    lineHeight: 1,
+                    animation: `${zoomEffect()} 300ms ease-out`,
+                }}
+            >
+                {Math.round(percent)}%
+            </Typography>
+        </Stack>
     )
 }
 
@@ -82,11 +75,21 @@ interface FactionAbilityItemProps {
 export const FactionAbilityItem = ({ factionAbility, pricing }: FactionAbilityItemProps) => {
     const { send } = useWebsocket()
     const theme = useTheme<Theme>()
+    const [initialTargetCost, setInitialTargetCost] = useState<BigNumber>(new BigNumber('0'))
+    const [refresh, toggleRefresh] = useToggle()
 
     const factionAbilityID = factionAbility.id
     const currentSups = new BigNumber(pricing.currentSups).dividedBy('1000000000000000000')
     const supsCost = new BigNumber(pricing.supsCost).dividedBy('1000000000000000000')
+    const shouldReset = pricing.shouldReset
     const isVoting = supsCost.isGreaterThanOrEqualTo(currentSups)
+
+    useEffect(() => {
+        if (shouldReset) {
+            setInitialTargetCost(supsCost)
+            toggleRefresh()
+        }
+    }, [shouldReset])
 
     const onContribute = useCallback(
         (amount: number) => async () => {
@@ -111,125 +114,144 @@ export const FactionAbilityItem = ({ factionAbility, pricing }: FactionAbilityIt
     const { label, colour, imageUrl } = factionAbility
 
     return (
-        <Box>
-            <ClipThing
-                border={{ isFancy: true, borderColor: theme.factionTheme.primary, borderThickness: '1.5px' }}
-                clipSize="6px"
-            >
-                <Box sx={{ backgroundColor: colors.darkNavy }}>
-                    <Stack direction="row" sx={{ height: 118, minWidth: 180 }}>
-                        <ClipThing
-                            border={{ isFancy: true, borderColor: theme.factionTheme.primary, borderThickness: '1px' }}
-                            clipSize="6px"
-                            fillHeight
-                        >
-                            <Box
-                                sx={{
-                                    backgroundColor: colour,
-                                    height: '100%',
-                                    width: 95,
-                                    backgroundImage: `url(${imageUrl})`,
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundPosition: 'center',
-                                    backgroundSize: 'cover',
-                                }}
-                            />
-                        </ClipThing>
-
-                        <Stack
-                            spacing={1}
-                            alignItems="flex-start"
-                            sx={{ flex: 1, backgroundColor: colors.darkNavy, px: 2, py: 1.2 }}
-                        >
-                            <Stack
-                                spacing={1.2}
-                                direction="row"
-                                alignItems="center"
-                                justifyContent="space-between"
-                                alignSelf="stretch"
-                            >
-                                <Typography
-                                    variant="body1"
-                                    sx={{
-                                        fontWeight: 'fontWeightBold',
-                                        fontFamily: 'Nostromo Regular Medium',
-                                        color: colour,
-                                        overflow: 'hidden',
-                                        textOverflow: 'ellipsis',
-                                        whiteSpace: 'nowrap',
-                                        maxWidth: 200,
+        <Box key={refresh}>
+            <Fade in={true}>
+                <Box>
+                    <ClipThing
+                        border={{ isFancy: true, borderColor: theme.factionTheme.primary, borderThickness: '1.5px' }}
+                        clipSize="6px"
+                    >
+                        <Box sx={{ backgroundColor: colors.darkNavy }}>
+                            <Stack direction="row" sx={{ height: 118, minWidth: 180 }}>
+                                <ClipThing
+                                    border={{
+                                        isFancy: true,
+                                        borderColor: theme.factionTheme.primary,
+                                        borderThickness: '1px',
                                     }}
+                                    clipSize="6px"
+                                    fillHeight
                                 >
-                                    {label}
-                                </Typography>
+                                    <Box
+                                        sx={{
+                                            backgroundColor: colour,
+                                            height: '100%',
+                                            width: 95,
+                                            backgroundImage: `url(${imageUrl})`,
+                                            backgroundRepeat: 'no-repeat',
+                                            backgroundPosition: 'center',
+                                            backgroundSize: 'cover',
+                                        }}
+                                    />
+                                </ClipThing>
 
-                                <Stack direction="row" alignItems="center" justifyContent="center">
-                                    <Typography
-                                        key={`currentSups-${currentSups}.toFixed()}`}
-                                        variant="body2"
-                                        sx={{
-                                            lineHeight: 1,
-                                            color: `${colour} !important`,
-                                            animation: `${zoomEffect(1.5)} 300ms ease-out`,
-                                        }}
+                                <Stack
+                                    spacing={1}
+                                    alignItems="flex-start"
+                                    sx={{ flex: 1, backgroundColor: colors.darkNavy, px: 2, py: 1.2 }}
+                                >
+                                    <Stack
+                                        spacing={1.2}
+                                        direction="row"
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                        alignSelf="stretch"
                                     >
-                                        {currentSups.toFixed(2)}
-                                    </Typography>
-                                    <Typography variant="body2" sx={{ lineHeight: 1, color: `${colour} !important` }}>
-                                        &nbsp;/&nbsp;
-                                    </Typography>
-                                    <Typography
-                                        key={`supsCost-${supsCost.toFixed()}`}
-                                        variant="body2"
-                                        sx={{
-                                            lineHeight: 1,
-                                            color: `${colour} !important`,
-                                            animation: `${zoomEffect(1.5)} 300ms ease-out`,
-                                        }}
-                                    >
-                                        {supsCost.toFixed(2)}
-                                    </Typography>
-                                    <Typography variant="body2" sx={{ lineHeight: 1, color: `${colour} !important` }}>
-                                        &nbsp;SUPS
-                                    </Typography>
+                                        <Typography
+                                            variant="body1"
+                                            sx={{
+                                                fontWeight: 'fontWeightBold',
+                                                fontFamily: 'Nostromo Regular Medium',
+                                                color: colour,
+                                                overflow: 'hidden',
+                                                textOverflow: 'ellipsis',
+                                                whiteSpace: 'nowrap',
+                                                maxWidth: 200,
+                                            }}
+                                        >
+                                            {label}
+                                        </Typography>
+
+                                        <Stack direction="row" alignItems="center" justifyContent="center">
+                                            <Typography
+                                                key={`currentSups-${currentSups.toFixed()}`}
+                                                variant="body2"
+                                                sx={{
+                                                    lineHeight: 1,
+                                                    color: `${colour} !important`,
+                                                    animation: `${zoomEffect(1.5)} 300ms ease-out`,
+                                                }}
+                                            >
+                                                {currentSups.toFixed(2)}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                sx={{ lineHeight: 1, color: `${colour} !important` }}
+                                            >
+                                                &nbsp;/&nbsp;
+                                            </Typography>
+                                            <Typography
+                                                key={`supsCost-${supsCost.toFixed()}`}
+                                                variant="body2"
+                                                sx={{
+                                                    lineHeight: 1,
+                                                    color: `${colour} !important`,
+                                                    animation: `${zoomEffect(1.5)} 300ms ease-out`,
+                                                }}
+                                            >
+                                                {supsCost.toFixed(2)}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                sx={{ lineHeight: 1, color: `${colour} !important` }}
+                                            >
+                                                &nbsp;SUPS
+                                            </Typography>
+                                        </Stack>
+                                    </Stack>
+
+                                    <ContributionBar
+                                        color={colour}
+                                        initialTargetCost={initialTargetCost}
+                                        currentSups={currentSups}
+                                        supsCost={supsCost}
+                                    />
+
+                                    <Stack direction="row" spacing={0.4} sx={{ mt: 0.6, width: '100%' }}>
+                                        <VotingButton
+                                            color={colour}
+                                            amount={1}
+                                            cost={1}
+                                            isVoting={isVoting}
+                                            onClick={onContribute(1)}
+                                            suffix="SUP"
+                                            disableHover
+                                        />
+                                        <VotingButton
+                                            color={colour}
+                                            amount={25}
+                                            cost={25}
+                                            isVoting={isVoting}
+                                            onClick={onContribute(25)}
+                                            suffix="SUP"
+                                            disableHover
+                                        />
+                                        <VotingButton
+                                            color={colour}
+                                            amount={100}
+                                            cost={100}
+                                            isVoting={isVoting}
+                                            onClick={onContribute(100)}
+                                            suffix="SUP"
+                                            disableHover
+                                        />
+                                    </Stack>
                                 </Stack>
                             </Stack>
-
-                            {/* <ContributionBar /> */}
-
-                            <Stack direction="row" spacing={0.4} sx={{ mt: 0.6, width: '100%' }}>
-                                <VotingButton
-                                    color={colour}
-                                    amount={1}
-                                    cost={1}
-                                    isVoting={isVoting}
-                                    onClick={onContribute(1)}
-                                    suffix="SUP"
-                                    disableHover
-                                />
-                                <VotingButton
-                                    color={colour}
-                                    amount={25}
-                                    cost={25}
-                                    isVoting={isVoting}
-                                    onClick={onContribute(25)}
-                                    suffix="SUP"
-                                    disableHover
-                                />
-                                <VotingButton
-                                    color={colour}
-                                    amount={100}
-                                    cost={100}
-                                    isVoting={isVoting}
-                                    onClick={onContribute(100)}
-                                    suffix="SUP"
-                                    disableHover
-                                />
-                            </Stack>
-                        </Stack>
-                    </Stack>
+                        </Box>
+                    </ClipThing>
                 </Box>
-            </ClipThing>
+            </Fade>
         </Box>
     )
 }
