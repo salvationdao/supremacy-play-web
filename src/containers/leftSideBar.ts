0cc6beb18550a00d2d97b016db2fa945efbf016d
James Li <jayli3n@gmail.com>
2022-02-22 00:07:15 +0800

    Disables previous battle button if no data

--- src/containers/leftSideBar.ts
+++ src/containers/leftSideBar.ts
@@ -1,13 +1,16 @@
-import { createContainer } from 'unstated-next'
-import { useToggle } from '../hooks'
+import { createContainer } from "unstated-next"
+import { useToggle } from "../hooks"
 
 // Control left side bar button and open states
 export const LeftSideBarContainer = createContainer(() => {
     const [isEndBattleDetailOpen, toggleIsEndBattleDetailOpen] = useToggle()
+    const [isEndBattleDetailEnabled, toggleIsEndBattleDetailEnabled] = useToggle()
 
     return {
         isEndBattleDetailOpen,
+        isEndBattleDetailEnabled,
         toggleIsEndBattleDetailOpen,
+        toggleIsEndBattleDetailEnabled,
     }
 })
 
