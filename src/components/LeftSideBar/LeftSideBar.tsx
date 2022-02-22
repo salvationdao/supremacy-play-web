761d24f6de6b26ecf0a51939d5aa9ab6bbb9a072
James Li <jayli3n@gmail.com>
2022-02-23 04:33:58 +0800

    Left side bar text color

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -55,6 +55,7 @@ const SideButton = ({
                     fontWeight: "fontWeightBold",
                     whiteSpace: "nowrap",
                     fontFamily: "Nostromo Regular Bold",
+                    color: user && user.faction ? user.faction.theme.secondary : "#FFFFFF",
                 }}
             >
                 {text}
