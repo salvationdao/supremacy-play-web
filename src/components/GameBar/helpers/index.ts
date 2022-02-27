9fe5ccd5a91e18f878c95c8bc436ab9143034ac3
James Li <jayli3n@gmail.com>
2022-02-27 20:58:16 +0800

    Wallet sups rounds down

--- src/components/GameBar/helpers/index.ts
+++ src/components/GameBar/helpers/index.ts
@@ -11,7 +11,7 @@ export const Capitalize = (str: string): string => str[0].toUpperCase() + str.su
  * @returns {boolean}
  */
 export const isObject = (item: any) => {
-	return item && typeof item === "object" && !Array.isArray(item)
+    return item && typeof item === "object" && !Array.isArray(item)
 }
 
 /**
@@ -20,84 +20,84 @@ export const isObject = (item: any) => {
  * @param ...sources
  */
 export const mergeDeep = (target: any, ...sources: any): any => {
-	if (!sources.length) return target
-	const source = sources.shift()
+    if (!sources.length) return target
+    const source = sources.shift()
 
-	if (isObject(target) && isObject(source)) {
-		for (const key in source) {
-			if (isObject(source[key])) {
-				if (!target[key]) Object.assign(target, { [key]: {} })
-				mergeDeep(target[key], source[key])
-			} else {
-				Object.assign(target, { [key]: source[key] })
-			}
-		}
-	}
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} })
+                mergeDeep(target[key], source[key])
+            } else {
+                Object.assign(target, { [key]: source[key] })
+            }
+        }
+    }
 
-	return mergeDeep(target, ...sources)
+    return mergeDeep(target, ...sources)
 }
 
 export const shadeColor = (hexColor: string, percent: number) => {
-	let R = parseInt(hexColor.substring(1, 3), 16)
-	let G = parseInt(hexColor.substring(3, 5), 16)
-	let B = parseInt(hexColor.substring(5, 7), 16)
+    let R = parseInt(hexColor.substring(1, 3), 16)
+    let G = parseInt(hexColor.substring(3, 5), 16)
+    let B = parseInt(hexColor.substring(5, 7), 16)
 
-	R = parseInt((R * (100 + percent)) / 100 + "")
-	G = parseInt((G * (100 + percent)) / 100 + "")
-	B = parseInt((B * (100 + percent)) / 100 + "")
+    R = parseInt((R * (100 + percent)) / 100 + "")
+    G = parseInt((G * (100 + percent)) / 100 + "")
+    B = parseInt((B * (100 + percent)) / 100 + "")
 
-	R = R < 255 ? R : 255
-	G = G < 255 ? G : 255
-	B = B < 255 ? B : 255
+    R = R < 255 ? R : 255
+    G = G < 255 ? G : 255
+    B = B < 255 ? B : 255
 
-	const RR = R.toString(16).length === 1 ? "0" + R.toString(16) : R.toString(16)
-	const GG = G.toString(16).length === 1 ? "0" + G.toString(16) : G.toString(16)
-	const BB = B.toString(16).length === 1 ? "0" + B.toString(16) : B.toString(16)
+    const RR = R.toString(16).length === 1 ? "0" + R.toString(16) : R.toString(16)
+    const GG = G.toString(16).length === 1 ? "0" + G.toString(16) : G.toString(16)
+    const BB = B.toString(16).length === 1 ? "0" + B.toString(16) : B.toString(16)
 
-	return "#" + RR + GG + BB
+    return "#" + RR + GG + BB
 }
 
 export const getRandomArbitrary = (min: number, max: number): number => {
-	return Math.random() * (max - min) + min
+    return Math.random() * (max - min) + min
 }
 
 export const numFormatter = (num: number) => {
-	if (num > 999 && num < 1000000) {
-		return (num / 1000).toFixed(1) + "K"
-	} else if (num > 1000000) {
-		return (num / 1000000).toFixed(1) + "M"
-	} else if (num < 900) {
-		return num + ""
-	}
+    if (num > 999 && num < 1000000) {
+        return (num / 1000).toFixed(1) + "K"
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(1) + "M"
+    } else if (num < 900) {
+        return num + ""
+    }
 }
 
 export const delayedPromiseResolve = (result: any, delay: number = 500): Promise<any> =>
-	new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve(result)
-		}, delay)
-	})
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(result)
+        }, delay)
+    })
 
 export const supFormatter = (num: string, fixedAmount: number | undefined = 0): string => {
-	const supTokens = new BigNumber(num)
-	if (supTokens.isZero()) return supTokens.toFixed(fixedAmount)
-	return supTokens.dividedBy(new BigNumber("1000000000000000000")).toFixed(fixedAmount)
+    const supTokens = new BigNumber(num)
+    if (supTokens.isZero()) return supTokens.toFixed(fixedAmount)
+    return Math.floor(supTokens.dividedBy(new BigNumber("1000000000000000000")).toNumber()).toFixed(fixedAmount)
 }
 
 export const hexToRGB = (hex: string, alpha?: number): string => {
-	const h = "0123456789ABCDEF"
-	const r = h.indexOf(hex[1]) * 16 + h.indexOf(hex[2])
-	const g = h.indexOf(hex[3]) * 16 + h.indexOf(hex[4])
-	const b = h.indexOf(hex[5]) * 16 + h.indexOf(hex[6])
-	if (alpha) return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")"
-	else return "rgb(" + r + ", " + g + ", " + b + ")"
+    const h = "0123456789ABCDEF"
+    const r = h.indexOf(hex[1]) * 16 + h.indexOf(hex[2])
+    const g = h.indexOf(hex[3]) * 16 + h.indexOf(hex[4])
+    const b = h.indexOf(hex[5]) * 16 + h.indexOf(hex[6])
+    if (alpha) return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")"
+    else return "rgb(" + r + ", " + g + ", " + b + ")"
 }
 
 export const getObjectFromArrayByKey = (array: any[], idValue: string, idName = "id") => {
-	for (let i = 0; i < array.length; i++) {
-		if (array[i][idName] === idValue) {
-			return array[i]
-		}
-	}
-	return undefined
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][idName] === idValue) {
+            return array[i]
+        }
+    }
+    return undefined
 }
