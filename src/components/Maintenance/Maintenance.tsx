bff252f2242b7e3a33dd737ac1703f68bde61f1c
James Li <jayli3n@gmail.com>
2022-02-27 11:31:53 +0800

    Refactor

---
+++ src/components/Maintenance/Maintenance.tsx
@@ -0,0 +1,105 @@
+import { Box, Stack, Typography } from "@mui/material"
+import moment from "moment"
+import { useEffect, useState } from "react"
+import { MAINTENANCE_END_TIME } from "../../constants"
+import { useInterval } from "../../hooks"
+import { colors } from "../../theme/theme"
+
+export const Maintenance = () => {
+    const [endTime] = useState<Date>(new Date(MAINTENANCE_END_TIME))
+    const [, setTimeRemain] = useState<number>(0)
+    const [delay, setDelay] = useState<number | null>(null)
+    const [hours, setHours] = useState<number>()
+    const [minutes, setMinutes] = useState<number>()
+    const [seconds, setSeconds] = useState<number>()
+
+    useEffect(() => {
+        if (endTime) {
+            setDelay(1000)
+            const d = moment.duration(moment(endTime).diff(moment()))
+            setTimeRemain(Math.max(Math.round(d.asSeconds()), 0))
+            return
+        }
+        setDelay(null)
+    }, [])
+
+    useInterval(() => {
+        setTimeRemain((t) => Math.max(t - 1, 0))
+        setHours(Math.max(moment(endTime).diff(moment(), "hours"), 0))
+        setMinutes(Math.max(moment(endTime).diff(moment(), "minutes") % 24, 0))
+        setSeconds(Math.max(moment(endTime).diff(moment(), "seconds") % 60, 0))
+    }, delay)
+
+    return (
+        <Box
+            sx={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: "rgba(0,0,0,0.5)",
+                backgroundImage: "url(/mech.jpeg)",
+                backgroundSize: "cover",
+                backgroundPosition: "bottom right",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Stack
+                sx={{
+                    position: "absolute",
+                    left: "50%",
+                    top: "50%",
+                    transform: "translate(-50%, -50%)",
+                    px: 4.2,
+                    py: 4,
+                    borderRadius: 1,
+                    zIndex: 999,
+                    backgroundColor: "rgba(0,0,0,0.5)",
+                }}
+            >
+                <Typography
+                    variant="h5"
+                    sx={{
+                        mb: 0.5,
+                        color: colors.text,
+                        fontFamily: "Nostromo Regular Bold",
+                        textAlign: "center",
+                    }}
+                >
+                    GAME RESUMES
+                </Typography>
+                <Typography
+                    variant="body1"
+                    sx={{
+                        color: colors.neonBlue,
+                        fontFamily: "Nostromo Regular Medium",
+                        textAlign: "center",
+                    }}
+                >
+                    SUNDAY 7:30 AM PST
+                    <br />
+                    (SUNDAY 3:30 PM PERTH TIME)
+                </Typography>
+
+                <Stack direction="row" justifyContent="space-around" sx={{ mt: 2 }}>
+                    <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
+                        <Typography sx={{ color: colors.neonBlue }}>{hours}</Typography>
+                        <Typography>HOURS</Typography>
+                    </Stack>
+                    <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
+                        <Typography sx={{ color: colors.neonBlue }}>{minutes}</Typography>
+                        <Typography>MINUTES</Typography>
+                    </Stack>
+                    <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
+                        <Typography sx={{ color: colors.neonBlue }}>{seconds}</Typography>
+                        <Typography>SECONDS</Typography>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Box>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, Stack, ThemeProvider, Typography } from "@mui/material"
+import { Box, Button, Stack, ThemeProvider } from "@mui/material"
 import { Theme } from "@mui/material/styles"
 import { DrawerProvider, GAMEBAR_CONSTANTS, WalletProvider } from "./components/GameBar"
 import GameBar from "./components/GameBar"
 import * as Sentry from "@sentry/react"
-import moment from "moment"
 import { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 import {
@@ -19,6 +18,7 @@ import {
     WarMachineQueue,
     WarMachineStats,
     Notifications,
+    Maintenance,
 } from "./components"
 import {
     CONTROLS_HEIGHT,
@@ -27,6 +27,8 @@ import {
     SENTRY_CONFIG,
     SUPREMACY_PAGE,
     TOKEN_SALE_PAGE,
+    TRAILER_VIDEO,
+    UNDER_MAINTENANCE,
 } from "./constants"
 import {
     AuthProvider,
@@ -39,7 +41,7 @@ import {
     useDimension,
 } from "./containers"
 import { mergeDeep, shadeColor } from "./helpers"
-import { useInterval, useToggle } from "./hooks"
+import { useToggle } from "./hooks"
 import { colors, theme } from "./theme/theme"
 import { FactionThemeColor, UpdateTheme } from "./types"
 
@@ -60,114 +62,14 @@ if (SENTRY_CONFIG) {
     })
 }
 
-const Countdown = () => {
-    const [endTime] = useState<Date>(new Date("2022-02-27T07:30:00.000Z"))
-    const [, setTimeRemain] = useState<number>(0)
-    const [delay, setDelay] = useState<number | null>(null)
-    const [hours, setHours] = useState<number>()
-    const [minutes, setMinutes] = useState<number>()
-    const [seconds, setSeconds] = useState<number>()
-
-    useEffect(() => {
-        if (endTime) {
-            setDelay(1000)
-            const d = moment.duration(moment(endTime).diff(moment()))
-            setTimeRemain(Math.max(Math.round(d.asSeconds()), 0))
-            return
-        }
-        setDelay(null)
-    }, [])
-
-    useInterval(() => {
-        setTimeRemain((t) => Math.max(t - 1, 0))
-        setHours(Math.max(moment(endTime).diff(moment(), "hours"), 0))
-        setMinutes(Math.max(moment(endTime).diff(moment(), "minutes") % 24, 0))
-        setSeconds(Math.max(moment(endTime).diff(moment(), "seconds") % 60, 0))
-    }, delay)
-
-    return (
-        <Stack
-            sx={{
-                px: 4.2,
-                py: 4,
-                borderRadius: 1,
-                zIndex: 999,
-                backgroundColor: "rgba(0,0,0,0.5)",
-            }}
-        >
-            <Typography
-                variant="h5"
-                sx={{
-                    mb: 0.5,
-                    color: colors.text,
-                    fontFamily: "Nostromo Regular Bold",
-                    textAlign: "center",
-                }}
-            >
-                GAME RESUMES
-            </Typography>
-            <Typography
-                variant="body1"
-                sx={{
-                    color: colors.neonBlue,
-                    fontFamily: "Nostromo Regular Medium",
-                    textAlign: "center",
-                }}
-            >
-                SUNDAY 7:30 AM PST
-                <br />
-                (SUNDAY 3:30 PM PERTH TIME)
-            </Typography>
-
-            <Stack direction="row" justifyContent="space-around" sx={{ mt: 2 }}>
-                <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
-                    <Typography sx={{ color: colors.neonBlue }}>{hours}</Typography>
-                    <Typography>HOURS</Typography>
-                </Stack>
-                <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
-                    <Typography sx={{ color: colors.neonBlue }}>{minutes}</Typography>
-                    <Typography>MINUTES</Typography>
-                </Stack>
-                <Stack alignItems="center" sx={{ px: 2, py: 1.5, backgroundColor: "#00000040" }}>
-                    <Typography sx={{ color: colors.neonBlue }}>{seconds}</Typography>
-                    <Typography>SECONDS</Typography>
-                </Stack>
-            </Stack>
-        </Stack>
-    )
-}
-
 const AppInner = () => {
     const { user, gameserverSessionID } = useAuth()
     const { mainDivDimensions, streamDimensions } = useDimension()
     const [haveSups, toggleHaveSups] = useToggle()
-    const [trailerEnded, toggleTrailerEnded] = useToggle(true)
+    const [trailerEnded, toggleTrailerEnded] = useToggle()
     const [watchedTrailer, setWatchedTrailer] = useState(localStorage.getItem("watchedTrailer") == "true")
 
-    if (haveSups !== "nello") {
-        return (
-            <Box
-                sx={{
-                    position: "fixed",
-                    left: 0,
-                    top: 0,
-                    height: "100vh",
-                    width: "100vw",
-                    backgroundColor: "rgba(0,0,0,0.5)",
-                    backgroundImage: "url(/mech.jpeg)",
-                    backgroundSize: "cover",
-                    backgroundPosition: "bottom right",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <LoadMessage />
-                <Countdown />
-            </Box>
-        )
-    }
+    if (UNDER_MAINTENANCE) return <Maintenance />
 
     // Trailer video
     if (!trailerEnded) {
@@ -185,23 +87,27 @@ const AppInner = () => {
                     },
                 }}
             >
-                {watchedTrailer && (
-                    <Button
-                        variant="contained"
-                        sx={{
-                            position: "absolute",
-                            top: 30,
-                            right: 30,
-                            zIndex: 99,
+                <Button
+                    variant="contained"
+                    disabled={!watchedTrailer}
+                    sx={{
+                        position: "absolute",
+                        top: 30,
+                        right: 30,
+                        zIndex: 99,
+                        backgroundColor: colors.darkNavy,
+                        borderRadius: 0.7,
+                        ":hover": { opacity: 0.8, backgroundColor: colors.darkNavy },
+                        ":disabled": {
+                            color: "#FFFFFF80",
                             backgroundColor: colors.darkNavy,
-                            borderRadius: 0.7,
-                            ":hover": { opacity: 0.8, backgroundColor: colors.darkNavy },
-                        }}
-                        onClick={() => toggleTrailerEnded(true)}
-                    >
-                        SKIP
-                    </Button>
-                )}
+                            opacity: 0.6,
+                        },
+                    }}
+                    onClick={() => toggleTrailerEnded(true)}
+                >
+                    SKIP
+                </Button>
 
                 <video
                     disablePictureInPicture
@@ -220,10 +126,7 @@ const AppInner = () => {
                     controls
                     autoPlay
                 >
-                    <source
-                        src="https://player.vimeo.com/progressive_redirect/playback/681913587/rendition/1080p?loc=external&signature=6d5bf3570be8bd5e9e57a6a786964a99d067957fbcf9e3a40b6914c085c9b3e9"
-                        type="video/mp4"
-                    />
+                    <source src={TRAILER_VIDEO} type="video/mp4" />
                 </video>
             </Stack>
         )
@@ -240,7 +143,6 @@ const AppInner = () => {
                 passportServerHost={PASSPORT_SERVER_HOST}
                 MechQueueComponent={<WarMachineQueue />}
             />
-            <Countdown />
             <Stack
                 sx={{
                     mt: `${GAMEBAR_CONSTANTS.gameBarHeight}px`,
@@ -274,7 +176,7 @@ const AppInner = () => {
                         <LoadMessage />
                         <Stream haveSups={haveSups} toggleHaveSups={toggleHaveSups} />
 
-                        {/* {user && haveSups && (
+                        {user && haveSups && (
                             <Box>
                                 <VotingSystem />
                                 <MiniMap />
@@ -284,19 +186,11 @@ const AppInner = () => {
                                 <BattleEndScreen />
                                 <BattleHistory />
                             </Box>
-                        )} */}
+                        )}
                     </Box>
                 </Stack>
 
-                <Box
-                    sx={{
-                        position: "relative",
-                        width: "100%",
-                        height: CONTROLS_HEIGHT,
-                    }}
-                >
-                    <Controls />
-                </Box>
+                <Controls />
             </Stack>
 
             {/* Just the under background, glimpse of it is visible when drawers open / close */}
