143f8d435700a2c679a0c90aa783861684e21d84
Yong Tan <ultimatoonmaster@gmail.com>
2022-03-11 15:11:41 +0800

    Using file blob names for faction_logo_id

--- src/components/BattleEndScreen/Sections/SectionTopSups.tsx
+++ src/components/BattleEndScreen/Sections/SectionTopSups.tsx
@@ -40,7 +40,7 @@ export const SectionTopSups = ({ battleEndDetail }: { battleEndDetail: BattleEnd
                                 text={u.username}
                                 imageUrl={
                                     u.faction_logo_id
-                                        ? `${PASSPORT_SERVER_HOST_IMAGES}/api/files/${u.faction_logo_id}`
+                                        ? `${PASSPORT_SERVER_HOST_IMAGES}/api/files/item/${u.faction_logo_id}`
                                         : undefined
                                 }
                                 variant="h5"
