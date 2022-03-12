6029f823d8be265f5023558f574217151160ba72
Alex Dunmow <alex@ninjasoftware.com.au>
2022-03-13 02:35:43 +0800

    remove havesups

--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -66,28 +66,24 @@ export const Stream = ({ haveSups, toggleHaveSups }: { haveSups: boolean; toggle
     }
     return (
         <Stack sx={{ width: "100%", height: "100%" }}>
-            {faction_id && haveSups ? (
-                <video
-                    key={currentStream?.stream_id}
-                    id={"remoteVideo"}
-                    muted={isMute}
-                    ref={vidRefCallback}
-                    autoPlay
-                    controls={false}
-                    playsInline
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        aspectRatio: STREAM_ASPECT_RATIO_W_H.toString(),
-                        width: iframeDimensions.width,
-                        height: iframeDimensions.height,
-                    }}
-                />
-            ) : (
-                <Message render={renderTopMessage} haveSups={haveSups} toggleHaveSups={toggleHaveSups} />
-            )}
+            <video
+                key={currentStream?.stream_id}
+                id={"remoteVideo"}
+                muted={isMute}
+                ref={vidRefCallback}
+                autoPlay
+                controls={false}
+                playsInline
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    aspectRatio: STREAM_ASPECT_RATIO_W_H.toString(),
+                    width: iframeDimensions.width,
+                    height: iframeDimensions.height,
+                }}
+            />
         </Stack>
     )
 }
