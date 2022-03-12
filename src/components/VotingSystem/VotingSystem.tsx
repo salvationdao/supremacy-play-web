83006a51e1d9ec82b7c9af18c777e2f39fb69d6a
hjpunzalan <hj.punzalan@hotmail.com>
2022-03-12 18:27:27 +0800

    allow voting system to be resizable

--- src/components/VotingSystem/VotingSystem.tsx
+++ src/components/VotingSystem/VotingSystem.tsx
@@ -1,9 +1,10 @@
 import { Box, Slide, Stack } from "@mui/material"
 import { Theme } from "@mui/material/styles"
 import { useTheme } from "@mui/styles"
-import { ClipThing, BattleAbilityItem, FactionAbilities } from ".."
-import { useDimension, useGame, BribeStageResponse } from "../../containers"
-import { useGameServerAuth } from "../../containers"
+import { SyntheticEvent, useState } from "react"
+import { Resizable, ResizeCallbackData } from "react-resizable"
+import { BattleAbilityItem, ClipThing, FactionAbilities } from ".."
+import { BribeStageResponse, useDimension, useGame, useGameServerAuth } from "../../containers"
 
 export const VotingSystem = () => {
     const { bribeStage } = useGame()
@@ -12,6 +13,9 @@ export const VotingSystem = () => {
 
 const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) => {
     const { user } = useGameServerAuth()
+    const initialSize = { width: 390, height: 410, minWidth: 350 }
+    const [containerWidth, setContainerWidth] = useState<number>(initialSize.width)
+    const [containerHeight, setContainerHeight] = useState<number>(initialSize.height)
     const theme = useTheme<Theme>()
     const {
         streamDimensions: { height },
@@ -20,6 +24,12 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
     const isBattleStarted = bribeStage && bribeStage.phase !== "HOLD"
 
     if (!user || !user.faction) return null
+
+    const onResize = (e?: SyntheticEvent<Element, Event>, data?: ResizeCallbackData) => {
+        const { size } = data || { size: { width: containerWidth, height: containerHeight } }
+        setContainerWidth(size.width)
+        setContainerHeight(size.height)
+    }
 
     return (
         <Stack
@@ -30,47 +40,72 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
                 zIndex: 14,
                 overflow: "hidden",
                 filter: "drop-shadow(0 3px 3px #00000050)",
-                minWidth: 390,
             }}
         >
             <Slide in={isBattleStarted} direction="right">
                 <Box>
-                    <ClipThing
-                        border={{ isFancy: true, borderThickness: "3px", borderColor: user.faction.theme.primary }}
-                        clipSize="10px"
-                    >
-                        <Box sx={{ backgroundColor: theme.factionTheme.background, pl: 0.9, pr: 2, pt: 1.8, pb: 2 }}>
+                    <Resizable
+                        height={containerHeight}
+                        width={containerWidth}
+                        minConstraints={[initialSize.minWidth, initialSize.height]}
+                        maxConstraints={[500, initialSize.height]}
+                        onResize={onResize}
+                        resizeHandles={["e"]}
+                        handle={() => (
                             <Box
                                 sx={{
-                                    flex: 1,
-                                    // 100vh, 160px gap bottom, 10px gap above, 56px for the title
-                                    maxHeight: `calc(${height}px - 160px - 10px - 56px)`,
-                                    overflowY: "auto",
-                                    overflowX: "hidden",
-                                    pl: 1.1,
-                                    py: 0.2,
-                                    direction: "rtl",
-                                    scrollbarWidth: "none",
-                                    "::-webkit-scrollbar": {
-                                        width: 4,
-                                    },
-                                    "::-webkit-scrollbar-track": {
-                                        background: "#FFFFFF15",
-                                        borderRadius: 3,
-                                    },
-                                    "::-webkit-scrollbar-thumb": {
-                                        background: theme.factionTheme.primary,
-                                        borderRadius: 3,
-                                    },
+                                    pointerEvents: "all",
+                                    position: "absolute",
+                                    top: 0,
+                                    right: 0,
+                                    cursor: "ew-resize",
+                                    zIndex: 50,
+                                    width: "10px",
+                                    height: containerHeight,
                                 }}
+                            />
+                        )}
+                    >
+                        <ClipThing
+                            border={{ isFancy: true, borderThickness: "3px", borderColor: user.faction.theme.primary }}
+                            clipSize="10px"
+                            innerSx={{ width: containerWidth, height: containerHeight }}
+                        >
+                            <Box
+                                sx={{ backgroundColor: theme.factionTheme.background, pl: 0.9, pr: 2, pt: 1.8, pb: 2 }}
                             >
-                                <Stack spacing={2.5} sx={{ direction: "ltr" }}>
-                                    <BattleAbilityItem />
-                                    <FactionAbilities />
-                                </Stack>
+                                <Box
+                                    sx={{
+                                        flex: 1,
+                                        // 100vh, 160px gap bottom, 10px gap above, 56px for the title
+                                        maxHeight: `calc(${height}px - 160px - 10px - 56px)`,
+                                        overflowY: "auto",
+                                        overflowX: "hidden",
+                                        pl: 1.1,
+                                        py: 0.2,
+                                        direction: "rtl",
+                                        scrollbarWidth: "none",
+                                        "::-webkit-scrollbar": {
+                                            width: 4,
+                                        },
+                                        "::-webkit-scrollbar-track": {
+                                            background: "#FFFFFF15",
+                                            borderRadius: 3,
+                                        },
+                                        "::-webkit-scrollbar-thumb": {
+                                            background: theme.factionTheme.primary,
+                                            borderRadius: 3,
+                                        },
+                                    }}
+                                >
+                                    <Stack spacing={2.5} sx={{ direction: "ltr" }}>
+                                        <BattleAbilityItem />
+                                        <FactionAbilities />
+                                    </Stack>
+                                </Box>
                             </Box>
-                        </Box>
-                    </ClipThing>
+                        </ClipThing>
+                    </Resizable>
                 </Box>
             </Slide>
         </Stack>
