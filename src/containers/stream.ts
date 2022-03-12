12eccf1e00e672b4166a17d9b388e06aafff0665
James Li <jayli3n@gmail.com>
2022-03-12 19:47:51 +0800

    Remove automatic resolution

--- src/containers/stream.ts
+++ src/containers/stream.ts
@@ -189,7 +189,7 @@ export const StreamContainer = createContainer(() => {
                             if (!webRtc || !webRtc.current || !webRtc.current.getStreamInfo) return
                             webRtc.current.getStreamInfo(currentStream.stream_id)
                         } else if (info == "streamInformation") {
-                            const resolutions: number[] = [0]
+                            const resolutions: number[] = []
                             obj["streamInfo"].forEach((entry: StreamInfoEntry) => {
                                 // get resolutions from server response and added to an array.
                                 if (!resolutions.includes(entry["streamHeight"])) {
@@ -230,9 +230,7 @@ export const StreamContainer = createContainer(() => {
 
         currentResolution,
         setCurrentResolution,
-
         streamResolutions,
-        setStreamResolutions,
 
         volume,
         setVolume,
