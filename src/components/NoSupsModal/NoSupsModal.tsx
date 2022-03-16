15c7274c4fe9c5e8c5bb5bd24c1a6efcea2c0052
James Li <jayli3n@gmail.com>
2022-03-16 16:22:28 +0800

    Styles

--- src/components/NoSupsModal/NoSupsModal.tsx
+++ src/components/NoSupsModal/NoSupsModal.tsx
@@ -1,20 +1,21 @@
-import { Alert, Box, Button, Dialog, Divider, Link, Modal, Stack, Typography } from "@mui/material"
+import { Box, Button, Divider, Link, Modal, Stack, Typography } from "@mui/material"
 import { colors } from "../../theme/theme"
-import React, { useEffect } from "react"
+import { useEffect } from "react"
 import { useToggle } from "../../hooks/useToggle"
 import WarningAmberIcon from "@mui/icons-material/WarningAmber"
 import { TOKEN_SALE_PAGE, PASSPORT_WEB } from "../../constants"
-import { useWallet } from "../../containers/wallet"
-import BigNumber from "bignumber.js"
+import { useGameServerAuth } from "../../containers"
 
-export const NoSupsModal = () => {
+export const NoSupsModal = ({ haveSups }: { haveSups: boolean }) => {
+    const { user } = useGameServerAuth()
     const [open, toggleOpen] = useToggle(false)
-    const { onWorldSups } = useWallet()
 
     useEffect(() => {
-        const newBigNum = new BigNumber(0)
-        onWorldSups?.isEqualTo(newBigNum) ? toggleOpen(true) : null
-    }, [onWorldSups])
+        if (!haveSups) return toggleOpen(true)
+        toggleOpen(false)
+    }, [haveSups])
+
+    if (!user) return null
 
     return (
         <Box>
@@ -25,9 +26,9 @@ export const NoSupsModal = () => {
                         top: "50%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
-                        maxWidth: 500,
-                        px: 4,
-                        py: 3,
+                        maxWidth: "60rem",
+                        px: "3.2rem",
+                        py: "2.4rem",
                         backgroundColor: `${colors.darkNavyBlue}`,
                         outline: "1px solid #FFFFFF",
                         borderRadius: 1,
@@ -36,36 +37,33 @@ export const NoSupsModal = () => {
                 >
                     <Stack spacing={2}>
                         <Box sx={{ display: "flex", alignItems: "center" }}>
-                            <WarningAmberIcon color="warning" sx={{ fontSize: "4rem", marginRight: ".5rem" }} />
-                            <Typography
-                                sx={{
-                                    fontFamily: "Nostromo Regular Bold",
-                                    textTransform: "uppercase",
-                                    fontSize: "2rem",
-                                }}
-                                variant="h3"
-                            >
-                                The connected account does not own $sups
+                            <WarningAmberIcon color="warning" sx={{ fontSize: "4rem", mr: "1.3rem" }} />
+                            <Typography variant="h4" sx={{ fontFamily: "Nostromo Regular Black" }}>
+                                NOT ENOUGH $SUPS
                             </Typography>
                         </Box>
 
-                        <Typography sx={{ fontFamily: "Nostromo Regular Bold", fontSize: "1.25rem" }} variant="caption">
+                        <Typography sx={{ fontFamily: "Nostromo Regular Bold" }}>
                             In order to experience the Battle Arena to its&apos; maximum potential, including voting on
                             in game abilities, viewing the minimap and individual mech health bars, your wallet must
                             contain $SUPS.
                         </Typography>
+
                         <Divider />
-                        <Typography sx={{ fontFamily: "Nostromo Regular Bold", fontSize: "1.75rem" }} variant="h4">
+
+                        <Typography variant="h5" sx={{ fontFamily: "Nostromo Regular Bold" }}>
                             To Obtain $SUPS:
                         </Typography>
-                        <Typography sx={{ fontFamily: "Nostromo Regular Bold", fontSize: "1.25rem" }} variant="caption">
+
+                        <Typography sx={{ fontFamily: "Nostromo Regular Bold" }}>
                             1. Navigate to the{" "}
                             <Link target="_blank" href={TOKEN_SALE_PAGE} color={colors.neonBlue}>
                                 token sale here
                             </Link>{" "}
                             or click the &quot;Get SUPS&quot; button in the top right-hand corner of the stream.
                         </Typography>
-                        <Typography sx={{ fontFamily: "Nostromo Regular Bold", fontSize: "1.25rem" }} variant="caption">
+
+                        <Typography sx={{ fontFamily: "Nostromo Regular Bold" }}>
                             2. From PancakeSwap, the tokens will reside in your off-world wallet. To use them on-world,
                             and in game, you will have to deposit your $SUPS into your connected account. You can{" "}
                             <Link target="_blank" href={PASSPORT_WEB + "deposit"} color={colors.neonBlue}>
@@ -73,7 +71,8 @@ export const NoSupsModal = () => {
                             </Link>
                             .
                         </Typography>
-                        <Typography sx={{ fontFamily: "Nostromo Regular Bold", fontSize: "1.25rem" }} variant="caption">
+
+                        <Typography sx={{ fontFamily: "Nostromo Regular Bold" }}>
                             3. Come back and enjoy the Battle Arena to its&apos; fullest.
                         </Typography>
                     </Stack>
@@ -81,8 +80,8 @@ export const NoSupsModal = () => {
                     <Button
                         variant="outlined"
                         sx={{
-                            mt: 3,
-                            py: 0.8,
+                            mt: "2.4rem",
+                            py: "1rem",
                             width: "100%",
                             color: colors.neonBlue,
                             backgroundColor: colors.darkNavy,
