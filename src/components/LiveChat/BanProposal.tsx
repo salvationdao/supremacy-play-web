925eb01cc7ba74699b2b151fab2b46c862d453c5
James Li <jayli3n@gmail.com>
2022-03-29 18:46:20 +0800

    WIP

--- src/components/LiveChat/BanProposal.tsx
+++ src/components/LiveChat/BanProposal.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "@mui/material"
+import { useChat } from "../../containers"
 
 export const BanProposal = () => {
+    const { banProposal } = useChat()
+
+    if (!banProposal) return null
+
     return <Stack>Hello</Stack>
 }
