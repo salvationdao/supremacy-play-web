8a387067bbee132cf5d8d9e368aba40eaf89d662
James Li <jayli3n@gmail.com>
2022-03-29 15:23:33 +0800

    wip

--- src/components/BattleEndScreen/Sections/SectionBottom.tsx
+++ src/components/BattleEndScreen/Sections/SectionBottom.tsx
@@ -44,7 +44,7 @@ export const SectionBottom = ({ battleEndDetail }: { battleEndDetail: BattleEndD
                 direction="row"
                 spacing=".64rem"
                 alignItems="flex-end"
-                sx={{ mr: "auto", pb: ".48rem", height: "100%", opacity: 0.5 }}
+                sx={{ mr: "auto", pb: ".48rem", height: "100%" }}
             >
                 <Typography variant="body2" sx={{ color: "grey !important" }}>
                     BATTLE ID #{battle_identifier.toString().padStart(4, "0")} ({moment(started_at).format("h:mm A")} to{" "}
