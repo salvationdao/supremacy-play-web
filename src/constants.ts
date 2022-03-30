e9ab7dbddb8dceba6e7edfb248c0e76f5cbb3935
Ivan Ng <87626385+ivanngyb@users.noreply.github.com>
2022-03-30 20:07:23 +0800

    Updated README (#255)
    * Added prismic to How to play section
    
    * Additional checks
    
    * Fix
    
    * Added skeleton loading
    
    * Fixed skeleton
    
    * Fixed access token for prismic
    
    * Styling
    
    * Fix
    
    * added to readme
    
    * Fixed readme

--- src/constants.ts
+++ src/constants.ts
@@ -76,4 +76,4 @@ export const TRAILER_VIDEO =
 export const UNDER_MAINTENANCE =
     process.env.REACT_APP_MAINTENANCE_PAGE == "true" && !localStorage.getItem("NinjaSecrets@!")
 
-export const PRISMIC_ACCESS_TOKEN = process.env.REACT_PRISMIC_ACCESS_TOKEN
+export const PRISMIC_ACCESS_TOKEN = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN
