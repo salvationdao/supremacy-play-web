a02d6dcc84a9576419e3eb0fa841d8811794b730
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-01 16:50:22 +0800

    History drawer with history entries

--- src/components/Assets/HistoryDrawer.tsx
+++ src/components/Assets/HistoryDrawer.tsx
@@ -1,8 +1,12 @@
-import { Drawer, Typography } from "@mui/material"
+import { Box, Drawer, Stack, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
+import { SvgGoldBars, SvgZaibatsuKillIcon } from "../../assets"
 import { RIGHT_DRAWER_WIDTH } from "../../constants"
+import { SocketState, useGameServerWebsocket } from "../../containers"
+import { GameServerKeys } from "../../keys"
 import { colors } from "../../theme/theme"
 import { Asset } from "../../types/assets"
+import { PercentageDisplay } from "./PercentageDisplay"
 
 export interface HistoryDrawerProps {
     open: boolean
@@ -11,7 +15,19 @@ export interface HistoryDrawerProps {
 }
 
 export const HistoryDrawer = ({ open, onClose, asset }: HistoryDrawerProps) => {
+    const { state, send } = useGameServerWebsocket()
     const [shouldRender, setShouldRender] = useState(false)
+
+    useEffect(() => {
+        if (state !== SocketState.OPEN || !send) return
+        ;(async () => {
+            const resp = await send(GameServerKeys.BattleMechHistoryList, {
+                mech_id: asset.id,
+            })
+
+            console.log(resp)
+        })()
+    }, [])
 
     useEffect(() => {
         const t = setTimeout(() => {
@@ -30,13 +46,207 @@ export const HistoryDrawer = ({ open, onClose, asset }: HistoryDrawerProps) => {
                 width: `${RIGHT_DRAWER_WIDTH}rem`,
                 flexShrink: 0,
                 zIndex: 9999,
-                "& .MuiDrawer-paper": {
+            }}
+            PaperProps={{
+                sx: {
                     width: `${RIGHT_DRAWER_WIDTH}rem`,
                     backgroundColor: colors.darkNavy,
+                    padding: "1rem",
                 },
             }}
         >
-            <Typography variant="h4">{asset.data.mech.name || asset.data.mech.label}</Typography>
+            <Typography
+                variant="h5"
+                sx={{
+                    marginBottom: "1rem",
+                }}
+            >
+                {asset.data.mech.name || asset.data.mech.label}
+            </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    margin: "1rem 0",
+                }}
+            >
+                <Box
+                    component="img"
+                    src={asset.data.mech.image_url}
+                    alt={`Image for ${asset.data.mech.name} || ${asset.data.mech.label}`}
+                    sx={{
+                        maxHeight: "180px",
+                    }}
+                />
+                <Stack
+                    spacing="1rem"
+                    justifyContent="center"
+                    sx={{
+                        marginBottom: "1rem",
+                    }}
+                >
+                    <PercentageDisplay displayValue="70%" percentage={70} label="Win Rate" />
+                    <PercentageDisplay displayValue="50%" percentage={50} label="Survival Rate" color={colors.gold} />
+                    <PercentageDisplay displayValue="80%" percentage={80} label="Kill Rate" color={colors.red} />
+                </Stack>
+            </Box>
+            <Typography variant="h6">Recent Matches</Typography>
+            <Stack
+                spacing=".6rem"
+                sx={{
+                    overflowY: "auto",
+                    overflowX: "hidden",
+                    direction: "ltr",
+                    scrollbarWidth: "none",
+                    "::-webkit-scrollbar": {
+                        width: ".4rem",
+                    },
+                    "::-webkit-scrollbar-track": {
+                        background: "#FFFFFF15",
+                        borderRadius: 3,
+                    },
+                    "::-webkit-scrollbar-thumb": {
+                        background: colors.assetsBanner,
+                        borderRadius: 3,
+                    },
+                }}
+            >
+                <HistoryEntry
+                    mapName="Desert City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/desert_city.jpg"
+                    isWin={true}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Neo Tokyo"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/neo_tokyo.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Urban City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/urban_city.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Arctic Bay"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/artic_bay.jpg"
+                    isWin={true}
+                    mechSurvived={true}
+                />
+                <HistoryEntry
+                    mapName="Desert City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/desert_city.jpg"
+                    isWin={true}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Neo Tokyo"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/neo_tokyo.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Urban City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/urban_city.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Arctic Bay"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/artic_bay.jpg"
+                    isWin={true}
+                    mechSurvived={true}
+                />
+                <HistoryEntry
+                    mapName="Desert City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/desert_city.jpg"
+                    isWin={true}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Neo Tokyo"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/neo_tokyo.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Urban City"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/urban_city.jpg"
+                    isWin={false}
+                    mechSurvived={false}
+                />
+                <HistoryEntry
+                    mapName="Arctic Bay"
+                    backgroundImage="https://ninjasoftware-static-media.s3.ap-southeast-2.amazonaws.com/supremacy/maps/artic_bay.jpg"
+                    isWin={true}
+                    mechSurvived={true}
+                />
+            </Stack>
         </Drawer>
+    )
+}
+
+interface HistoryEntryProps {
+    mapName: string
+    isWin: boolean
+    mechSurvived: boolean
+    backgroundImage?: string
+}
+
+const HistoryEntry = ({ mapName, isWin, mechSurvived, backgroundImage }: HistoryEntryProps) => {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                minHeight: "70px",
+                padding: "0.8rem 1.04rem",
+                background: `center center`,
+                backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.8) 20%, ${
+                    isWin ? colors.green : colors.red
+                }80), url(${backgroundImage})`,
+                backgroundSize: "cover",
+                // filter: "grayscale(1)",
+            }}
+        >
+            <Box>
+                <Typography>{mapName}</Typography>
+                <Typography
+                    variant="h5"
+                    sx={{
+                        fontFamily: ["Nostromo Regular Bold", "Roboto", "Helvetica", "Arial", "sans-serif"].join(","),
+                    }}
+                >
+                    {isWin ? "Victory" : "Defeat"}
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <Typography
+                        variant="subtitle2"
+                        sx={{
+                            marginRight: ".5rem",
+                            textTransform: "uppercase",
+                            color: mechSurvived ? colors.neonBlue : colors.grey,
+                        }}
+                    >
+                        {mechSurvived ? "Mech Survived" : "Mech Destroyed"}
+                    </Typography>
+                    {mechSurvived && <SvgGoldBars size="1.5rem" />}
+                </Box>
+            </Box>
+            <SvgZaibatsuKillIcon
+                sx={{
+                    alignSelf: "center",
+                    marginLeft: "auto",
+                }}
+                size="4rem"
+            />
+        </Box>
     )
 }
---
+++ src/components/Assets/PercentageDisplay.tsx
@@ -0,0 +1,100 @@
+import { keyframes } from "@emotion/react"
+import { Box, styled, Typography } from "@mui/material"
+import { colors } from "../../theme/theme"
+
+interface PercentageDisplayProps {
+    displayValue: string
+    percentage: number
+    label: string
+    size?: number
+    color?: string
+}
+
+export const PercentageDisplay: React.VoidFunctionComponent<PercentageDisplayProps> = ({
+    displayValue,
+    percentage,
+    label,
+    size,
+    color,
+}) => {
+    const radius = size ? size / (2 * 1.11) : 20
+    const circumference = Math.PI * 2 * radius
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Box
+                sx={{
+                    position: "relative",
+                    width: "100%",
+                    maxWidth: 1.11 * radius * 2,
+                    minHeight: 1.11 * radius * 2,
+                    marginBottom: ".5rem",
+                }}
+            >
+                <StyledSVG
+                    width={1.11 * radius * 2}
+                    height={1.11 * radius * 2}
+                    sx={{
+                        transform: "rotate(-90deg)",
+                    }}
+                >
+                    <StyledCircle
+                        r={1.11 * radius}
+                        cx={1.11 * radius}
+                        cy={1.11 * radius}
+                        sx={{
+                            fill: color ? `${color}65` : `${colors.neonBlue}65`,
+                        }}
+                    />
+                    <StyledCircle
+                        r={radius}
+                        cx={1.11 * radius}
+                        cy={1.11 * radius}
+                        sx={{
+                            fill: colors.darkerNavy,
+                            stroke: color || colors.neonBlue,
+                            strokeDasharray: `${(circumference * percentage) / 100} ${circumference}`,
+                            strokeWidth: 0.22 * radius,
+                            transition: "stroke-dasharray .2s ease-out",
+                            animation: `${generateStrokeKeyframes(percentage, circumference)} 1.5s ease-out`,
+                        }}
+                    />
+                </StyledSVG>
+                <Typography
+                    variant="h5"
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                    }}
+                >
+                    {displayValue}
+                </Typography>
+            </Box>
+            <Typography variant="caption" textAlign="center">
+                {label}
+            </Typography>
+        </Box>
+    )
+}
+
+const StyledSVG = styled("svg")({})
+
+const StyledCircle = styled("circle")({})
+
+const generateStrokeKeyframes = (percentage: number, circumference: number) => {
+    return keyframes`
+	0% {
+	  stroke-dasharray: 0;
+	}
+	100% {
+		stroke-dasharray: ${(circumference * percentage) / 100} ${circumference};
+	}
+  `
+}
