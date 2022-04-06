e86367adf3dcdac7e369817e49c80d67806ed1dd
James Li <jayli3n@gmail.com>
2022-04-07 00:02:56 +0800

    Update

--- src/components/LiveChat/ChatSend/ChatSend.tsx
+++ src/components/LiveChat/ChatSend/ChatSend.tsx
@@ -399,7 +399,7 @@ const ChatSendInner = ({
                         endAdornment: (
                             <InputAdornment position="end">
                                 <ChatSettings primaryColor={primaryColor} faction_id={faction_id} />
-                                <TooltipHelper placement="top" text="Keyboard shortcut ':'">
+                                <TooltipHelper placement="top" text="Use keyboard shortcut ' : '">
                                     <IconButton
                                         ref={popoverRef}
                                         onClick={() => toggleIsEmojiOpen()}
