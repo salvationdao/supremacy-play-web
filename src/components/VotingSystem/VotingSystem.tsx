0f5ddc6f374bab60cd537e48d0a9d8034cd99e75
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-26 13:02:19 +0800

    Working battle and player ability tabs

--- src/components/VotingSystem/VotingSystem.tsx
+++ src/components/VotingSystem/VotingSystem.tsx
@@ -1,11 +1,14 @@
-import { Box, Slide, Stack } from "@mui/material"
+import { Box, Slide, Stack, Tab, Tabs } from "@mui/material"
 import { Theme } from "@mui/material/styles"
+import { TabProps } from "@mui/material/Tab"
 import { useTheme } from "@mui/styles"
 import { useCallback, useMemo, useState } from "react"
-import { BattleAbilityItem, ClipThing, FactionAbilities, ResizeBox } from ".."
+import { ClipThing, FactionAbilities, ResizeBox } from ".."
 import { BribeStageResponse, useDimension, useGame, useGameServerAuth } from "../../containers"
 import { parseString } from "../../helpers"
+import { colors } from "../../theme/theme"
 import { Dimension } from "../../types"
+import { BattleAbilityItem } from "./BattleAbilityItem"
 
 export const VotingSystem = () => {
     const { bribeStage } = useGame()
@@ -23,6 +26,7 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
         pxToRemRatio,
         streamDimensions: { height },
     } = useDimension()
+    const [currentTab, setCurrentTab] = useState(0)
 
     const isBattleStarted = useMemo(() => bribeStage && bribeStage.phase !== "HOLD", [bribeStage])
 
@@ -51,7 +55,7 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
                 filter: "drop-shadow(0 3px 3px #00000050)",
             }}
         >
-            <Slide in={isBattleStarted} direction="right">
+            <Slide in={true} direction="right">
                 <Box sx={{ position: "relative" }}>
                     <ResizeBox
                         color={theme.factionTheme.primary}
@@ -77,9 +81,27 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
                         )}
                     />
 
+                    <Tabs
+                        defaultValue={0}
+                        value={currentTab}
+                        onChange={(_, value) => setCurrentTab(value)}
+                        TabIndicatorProps={{
+                            hidden: true,
+                        }}
+                        sx={{
+                            minHeight: 0,
+                        }}
+                    >
+                        <TabButton label="Battle Abilities" backgroundColor={theme.factionTheme.background} borderColor={theme.factionTheme.primary} />
+                        <TabButton label="Player Abilities" backgroundColor={theme.factionTheme.background} borderColor={theme.factionTheme.primary} />
+                    </Tabs>
                     <ClipThing
+                        sx={{
+                            zIndex: -1,
+                            position: "relative",
+                            top: "-1px",
+                        }}
                         border={{
-                            isFancy: true,
                             borderThickness: ".15rem",
                             borderColor: theme.factionTheme.primary,
                         }}
@@ -95,39 +117,89 @@ const VotingSystemInner = ({ bribeStage }: { bribeStage?: BribeStageResponse }) 
                                 pb: "1.6rem",
                             }}
                         >
-                            <Box
-                                sx={{
-                                    flex: 1,
-                                    // 100vh, 160px gap bottom, 10px gap above, 56px for the title
-                                    maxHeight: `calc(${height}px - 140px - 10px)`,
-                                    overflowY: "auto",
-                                    overflowX: "hidden",
-                                    pl: ".88rem",
-                                    py: ".16rem",
-                                    direction: "rtl",
-                                    scrollbarWidth: "none",
-                                    "::-webkit-scrollbar": {
-                                        width: ".4rem",
-                                    },
-                                    "::-webkit-scrollbar-track": {
-                                        background: "#FFFFFF15",
-                                        borderRadius: 3,
-                                    },
-                                    "::-webkit-scrollbar-thumb": {
-                                        background: theme.factionTheme.primary,
-                                        borderRadius: 3,
-                                    },
-                                }}
-                            >
-                                <Stack spacing="2rem" sx={{ direction: "ltr" }}>
-                                    <BattleAbilityItem />
-                                    <FactionAbilities />
-                                </Stack>
-                            </Box>
+                            <TabPanel value={currentTab} index={0}>
+                                <Box
+                                    sx={{
+                                        flex: 1,
+                                        // 100vh, 160px gap bottom, 10px gap above, 56px for the title
+                                        maxHeight: `calc(${height}px - 140px - 10px)`,
+                                        overflowY: "auto",
+                                        overflowX: "hidden",
+                                        pl: ".88rem",
+                                        py: ".16rem",
+                                        direction: "rtl",
+                                        scrollbarWidth: "none",
+                                        "::-webkit-scrollbar": {
+                                            width: ".4rem",
+                                        },
+                                        "::-webkit-scrollbar-track": {
+                                            background: "#FFFFFF15",
+                                            borderRadius: 3,
+                                        },
+                                        "::-webkit-scrollbar-thumb": {
+                                            background: theme.factionTheme.primary,
+                                            borderRadius: 3,
+                                        },
+                                    }}
+                                >
+                                    <Stack spacing="2rem" sx={{ direction: "ltr" }}>
+                                        <BattleAbilityItem />
+                                        <FactionAbilities />
+                                    </Stack>
+                                </Box>
+                            </TabPanel>
+                            <TabPanel value={currentTab} index={1}>
+                                testing
+                            </TabPanel>
                         </Box>
                     </ClipThing>
                 </Box>
             </Slide>
         </Stack>
+    )
+}
+
+interface TabButtonProps extends TabProps {
+    backgroundColor: string
+    borderColor: string
+}
+
+const TabButton = ({ backgroundColor, borderColor, ...props }: TabButtonProps) => {
+    return (
+        <Tab
+            sx={{
+                color: colors.grey,
+                "&.MuiButtonBase-root": {
+                    minHeight: 0,
+                    padding: ".5rem 1rem",
+                    border: `1px solid ${borderColor}`,
+                    backgroundColor,
+                },
+                "&.Mui-selected": {
+                    color: borderColor,
+                    borderBottom: `1px solid ${backgroundColor}`,
+                },
+                "&.Mui-focusVisible": {
+                    backgroundColor: "orangered",
+                },
+            }}
+            {...props}
+        />
+    )
+}
+
+interface TabPanelProps {
+    children?: React.ReactNode
+    index: number
+    value: number
+}
+
+const TabPanel = (props: TabPanelProps) => {
+    const { children, value, index, ...other } = props
+
+    return (
+        <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+            {value === index && children}
+        </div>
     )
 }
