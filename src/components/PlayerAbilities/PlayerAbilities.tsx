41c2f67cadb61fcaf1e6091e6a3ef945dd44ee29
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-26 17:52:51 +0800

    Pagination player abilities

--- src/components/PlayerAbilities/PlayerAbilities.tsx
+++ src/components/PlayerAbilities/PlayerAbilities.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, Pagination, Stack, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { SocketState, useGameServerAuth, useGameServerWebsocket } from "../../containers"
 import { GameServerKeys } from "../../keys"
@@ -10,11 +10,18 @@ export const PlayerAbilities = () => {
     const { state, send, subscribe } = useGameServerWebsocket()
     const [playerAbilityIDs, setPlayerAbilityIDs] = useState<string[]>([])
 
+    // Pagination
+    const [currentPage, setCurrentPage] = useState(1)
+    const [pageSize, setPageSize] = useState(8)
+    const [totalPages, setTotalPages] = useState(0)
+
     useEffect(() => {
         if (state !== SocketState.OPEN || !send || !subscribe || !user) return
 
         const fetchSaleAbilities = async () => {
             const resp = await send<{ total: number; ability_ids: string[] }>(GameServerKeys.PlayerAbilitiesList, {
+                page_size: pageSize,
+                page: currentPage - 1,
                 filter: {
                     items: [
                         {
@@ -26,12 +33,13 @@ export const PlayerAbilities = () => {
                 },
             })
             setPlayerAbilityIDs(resp.ability_ids)
+            setTotalPages(Math.ceil(resp.total / pageSize))
         }
 
         fetchSaleAbilities()
 
         return subscribe(GameServerKeys.TriggerSaleAbilitiesListUpdated, () => fetchSaleAbilities())
-    }, [state, send, subscribe, user])
+    }, [state, send, subscribe, user, currentPage, pageSize])
 
     return (
         <Box>
@@ -47,32 +55,43 @@ export const PlayerAbilities = () => {
                     Player Abilities
                 </Typography>
             </Stack>
-            {playerAbilityIDs.length > 0 ? (
-                <Box
-                    sx={{
-                        display: "grid",
-                        gridTemplateColumns: "repeat(4, 1fr)",
-                        gap: ".5rem",
-                    }}
-                >
-                    {playerAbilityIDs.map((s) => (
-                        <PlayerAbilityCard key={s} abilityID={s} />
-                    ))}
-                </Box>
-            ) : (
-                <Typography
-                    sx={{
-                        px: "1.28rem",
-                        pt: "1.28rem",
-                        mb: ".56rem",
-                        color: colors.grey,
-                        userSelect: "text !important",
-                        opacity: 0.8,
-                    }}
-                >
-                    There are currently no abilities on sale.
-                </Typography>
-            )}
+            <Box marginBottom="1rem">
+                {playerAbilityIDs.length > 0 ? (
+                    <Box
+                        sx={{
+                            display: "grid",
+                            gridTemplateColumns: "repeat(4, 1fr)",
+                            gridTemplateRows: "repeat(2, 1fr)",
+                            gap: ".5rem",
+                        }}
+                    >
+                        {playerAbilityIDs.map((s) => (
+                            <PlayerAbilityCard key={s} abilityID={s} />
+                        ))}
+                    </Box>
+                ) : (
+                    <Typography
+                        sx={{
+                            px: "1.28rem",
+                            pt: "1.28rem",
+                            mb: ".56rem",
+                            color: colors.grey,
+                            userSelect: "text !important",
+                            opacity: 0.8,
+                        }}
+                    >
+                        There are currently no abilities on sale.
+                    </Typography>
+                )}
+            </Box>
+            <Pagination
+                count={totalPages}
+                color="primary"
+                page={currentPage}
+                onChange={(_, p) => {
+                    setCurrentPage(p)
+                }}
+            />
         </Box>
     )
 }
