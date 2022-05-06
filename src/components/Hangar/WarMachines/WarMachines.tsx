d0be3b0608c634d10b24d5e13cfa0063c68d7012
James Li <jayli3n@gmail.com>
2022-05-06 18:42:45 +0800

    WIP

--- src/components/Hangar/WarMachines/WarMachines.tsx
+++ src/components/Hangar/WarMachines/WarMachines.tsx
@@ -1,6 +1,7 @@
 import { Box, useTheme, Theme, Stack, Typography, IconButton, Pagination, Button, CircularProgress } from "@mui/material"
 import { useEffect, useMemo, useRef, useState } from "react"
 import { ClipThing, FancyButton } from "../.."
+import { HangarBg } from "../../../assets"
 import { PASSPORT_WEB } from "../../../constants"
 import { useGameServerAuth, useGameServerWebsocket, useSnackbar, useSupremacy } from "../../../containers"
 import { useDebounce, usePagination, useToggle } from "../../../hooks"
@@ -105,7 +106,7 @@ export const WarMachines = () => {
         if (isLoading || !assetsQueue || !queueFeed) {
             return (
                 <Stack alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
-                    <CircularProgress size="1.8rem" sx={{ color: colors.neonBlue }} />
+                    <CircularProgress size="2rem" sx={{ color: colors.neonBlue }} />
                 </Stack>
             )
         }
@@ -141,7 +142,7 @@ export const WarMachines = () => {
                         opacity: 0.8,
                     }}
                 >
-                    {"You don't own any assets yet."}
+                    {"You don't own any war machines yet."}
                 </Typography>
                 <FancyButton
                     href={`${PASSPORT_WEB}stores`}
@@ -171,108 +172,118 @@ export const WarMachines = () => {
 
     return (
         <>
-            <ClipThing
-                clipSize="10px"
-                border={{
-                    isFancy: true,
-                    borderColor: theme.factionTheme.primary,
-                    borderThickness: ".15rem",
+            <Stack
+                sx={{
+                    height: "100%",
+                    backgroundImage: `url(${HangarBg})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "contain",
                 }}
-                opacity={0.7}
-                backgroundColor={theme.factionTheme.background}
-                sx={{ height: "100%", minWidth: "95rem", maxWidth: "65%" }}
             >
-                <Stack sx={{ height: "100%" }}>
-                    <Stack
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        sx={{
-                            px: "1.5rem",
-                            pt: ".5rem",
-                            pb: ".3rem",
-                            backgroundColor: "#00000070",
-                            borderBottom: (theme) => `${theme.factionTheme.primary}70 1px solid`,
-                            span: {
-                                fontFamily: fonts.nostromoBold,
-                            },
-                        }}
-                    >
-                        <Typography variant="caption">
-                            <strong>DISPLAYING:</strong> {assetsQueue?.length || 0} of {totalItems}
-                        </Typography>
-                        <Stack direction="row" alignItems="center">
-                            <IconButton
-                                size="small"
-                                onClick={() => {
-                                    setPageSize(12)
-                                    changePage(1)
-                                }}
-                            >
-                                <Typography variant="caption" sx={{ opacity: pageSize === 12 ? 1 : 0.3 }}>
-                                    12
-                                </Typography>
-                            </IconButton>
-                            <IconButton
-                                size="small"
-                                onClick={() => {
-                                    setPageSize(24)
-                                    changePage(1)
-                                }}
-                            >
-                                <Typography variant="caption" sx={{ opacity: pageSize === 24 ? 1 : 0.3 }}>
-                                    24
-                                </Typography>
-                            </IconButton>
-                            <IconButton
-                                size="small"
-                                onClick={() => {
-                                    setPageSize(36)
-                                    changePage(1)
-                                }}
-                            >
-                                <Typography variant="caption" sx={{ opacity: pageSize === 36 ? 1 : 0.3 }}>
-                                    36
-                                </Typography>
-                            </IconButton>
+                <ClipThing
+                    clipSize="10px"
+                    border={{
+                        isFancy: true,
+                        borderColor: theme.factionTheme.primary,
+                        borderThickness: ".15rem",
+                    }}
+                    opacity={0.7}
+                    backgroundColor={theme.factionTheme.background}
+                    sx={{ height: "100%", minWidth: "95rem", maxWidth: "65%" }}
+                >
+                    <Stack sx={{ height: "100%" }}>
+                        <Stack
+                            direction="row"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            sx={{
+                                px: "1.5rem",
+                                pt: ".5rem",
+                                pb: ".3rem",
+                                backgroundColor: "#00000070",
+                                borderBottom: (theme) => `${theme.factionTheme.primary}70 1px solid`,
+                                span: {
+                                    fontFamily: fonts.nostromoBold,
+                                },
+                            }}
+                        >
+                            <Typography variant="caption">
+                                <strong>DISPLAYING:</strong> {assetsQueue?.length || 0} of {totalItems}
+                            </Typography>
+                            <Stack direction="row" alignItems="center">
+                                <IconButton
+                                    size="small"
+                                    onClick={() => {
+                                        setPageSize(12)
+                                        changePage(1)
+                                    }}
+                                >
+                                    <Typography variant="caption" sx={{ opacity: pageSize === 12 ? 1 : 0.3 }}>
+                                        12
+                                    </Typography>
+                                </IconButton>
+                                <IconButton
+                                    size="small"
+                                    onClick={() => {
+                                        setPageSize(24)
+                                        changePage(1)
+                                    }}
+                                >
+                                    <Typography variant="caption" sx={{ opacity: pageSize === 24 ? 1 : 0.3 }}>
+                                        24
+                                    </Typography>
+                                </IconButton>
+                                <IconButton
+                                    size="small"
+                                    onClick={() => {
+                                        setPageSize(36)
+                                        changePage(1)
+                                    }}
+                                >
+                                    <Typography variant="caption" sx={{ opacity: pageSize === 36 ? 1 : 0.3 }}>
+                                        36
+                                    </Typography>
+                                </IconButton>
+                            </Stack>
                         </Stack>
-                    </Stack>
 
-                    <Box
-                        sx={{
-                            my: ".8rem",
-                            ml: ".3rem",
-                            pl: ".5rem",
-                            mr: ".3rem",
-                            pr: ".5rem",
-                            flex: 1,
-                            overflowY: "auto",
-                            overflowX: "hidden",
-                            direction: "ltr",
-                            scrollbarWidth: "none",
-                            "::-webkit-scrollbar": {
-                                width: ".4rem",
-                            },
-                            "::-webkit-scrollbar-track": {
-                                background: "#FFFFFF15",
-                                borderRadius: 3,
-                            },
-                            "::-webkit-scrollbar-thumb": {
-                                background: colors.assetsBanner,
-                                borderRadius: 3,
-                            },
-                        }}
-                    >
-                        {content}
-                    </Box>
-
-                    {assetsQueue && totalPages > 1 && (
-                        <Box sx={{ px: "1rem", py: ".5rem", backgroundColor: "#00000050" }}>
-                            <Pagination size="small" count={totalPages} page={page} onChange={(e, p) => changePage(p)} showFirstButton showLastButton />
+                        <Box
+                            sx={{
+                                my: ".8rem",
+                                ml: ".3rem",
+                                pl: ".5rem",
+                                mr: ".3rem",
+                                pr: ".5rem",
+                                flex: 1,
+                                overflowY: "auto",
+                                overflowX: "hidden",
+                                direction: "ltr",
+                                scrollbarWidth: "none",
+                                "::-webkit-scrollbar": {
+                                    width: ".4rem",
+                                },
+                                "::-webkit-scrollbar-track": {
+                                    background: "#FFFFFF15",
+                                    borderRadius: 3,
+                                },
+                                "::-webkit-scrollbar-thumb": {
+                                    background: colors.assetsBanner,
+                                    borderRadius: 3,
+                                },
+                            }}
+                        >
+                            {content}
                         </Box>
-                    )}
-                </Stack>
-            </ClipThing>
+
+                        {assetsQueue && totalPages > 1 && (
+                            <Box sx={{ px: "1rem", py: ".5rem", backgroundColor: "#00000050" }}>
+                                <Pagination size="small" count={totalPages} page={page} onChange={(e, p) => changePage(p)} showFirstButton showLastButton />
+                            </Box>
+                        )}
+                    </Stack>
+                </ClipThing>
+            </Stack>
 
             <TelegramShortcodeModal code={telegramShortcode} onClose={() => setTelegramShortcode("")} open={!!telegramShortcode} />
         </>
