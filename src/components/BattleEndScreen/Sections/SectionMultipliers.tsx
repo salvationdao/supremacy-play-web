2eb7fc9a19e068028fbe1f191a1570dbf60c9b79
felix t <41817193+o2bomb@users.noreply.github.com>
2022-05-06 14:45:52 +0800

    Fix duplicate keys on multiplier badges

--- src/components/BattleEndScreen/Sections/SectionMultipliers.tsx
+++ src/components/BattleEndScreen/Sections/SectionMultipliers.tsx
@@ -61,10 +61,10 @@ export const SectionMultipliers = ({ battleEndDetail }: { battleEndDetail: Battl
                                 MULTIPLIERS
                             </Typography>
 
-                            {multiplierList.map((m) => {
+                            {multiplierList.map((m, index) => {
                                 const deets = getMutiplierDeets(m.key)
                                 return (
-                                    <TooltipHelper key={m.key} placement="right" text={m.description}>
+                                    <TooltipHelper key={`${m.key}-${index}`} placement="right" text={m.description}>
                                         <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ px: ".8rem" }}>
                                             <StyledImageText
                                                 color={colors.text}
