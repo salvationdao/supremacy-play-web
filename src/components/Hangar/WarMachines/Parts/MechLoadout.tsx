0528decaee7147cbae5e9d349833f479d1a0b11f
James Li <jayli3n@gmail.com>
2022-05-20 19:35:52 +0800

    Added loadout icons

--- src/components/Hangar/WarMachines/Parts/MechLoadout.tsx
+++ src/components/Hangar/WarMachines/Parts/MechLoadout.tsx
@@ -1,6 +1,7 @@
 import { Box, CircularProgress, Stack } from "@mui/material"
+import { ReactNode } from "react"
 import { FancyButton, TooltipHelper } from "../../.."
-import { SvgPlus } from "../../../../assets"
+import { SvgPlus, SvgPowerCore, SvgSkin, SvgIntroAnimation, SvgOutroAnimation, SvgWeapons, SvgUtilities } from "../../../../assets"
 import { useTheme } from "../../../../containers/theme"
 import { MechBasic, MechDetails } from "../../../../types"
 
@@ -44,15 +45,55 @@ export const MechLoadout = ({ mech, mechDetails }: { mech: MechBasic; mechDetail
             }}
         >
             <Stack sx={{ flexWrap: "wrap", height: "100%", width: "fit-content" }}>
-                {power_core_id && <LoadoutItem imageUrl={powerCore?.image_url} primaryColor={primaryColor} tooltipText={powerCore?.label} />}
-                {chassis_skin_id && <LoadoutItem imageUrl={chassisSkin?.image_url} primaryColor={primaryColor} tooltipText={chassisSkin?.label} />}
-                {intro_animation_id && <LoadoutItem imageUrl={introAnimation?.image_url} primaryColor={primaryColor} tooltipText={introAnimation?.label} />}
-                {outro_animation_id && <LoadoutItem imageUrl={outroAnimation?.image_url} primaryColor={primaryColor} tooltipText={outroAnimation?.label} />}
+                {power_core_id && (
+                    <LoadoutItem
+                        imageUrl={powerCore?.image_url}
+                        Icon={<SvgPowerCore size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={powerCore?.label}
+                    />
+                )}
+                {chassis_skin_id && (
+                    <LoadoutItem
+                        imageUrl={chassisSkin?.image_url}
+                        Icon={<SvgSkin size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={chassisSkin?.label}
+                    />
+                )}
+                {intro_animation_id && (
+                    <LoadoutItem
+                        imageUrl={introAnimation?.image_url}
+                        Icon={<SvgIntroAnimation size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={introAnimation?.label}
+                    />
+                )}
+                {outro_animation_id && (
+                    <LoadoutItem
+                        imageUrl={outroAnimation?.image_url}
+                        Icon={<SvgOutroAnimation size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={outroAnimation?.label}
+                    />
+                )}
                 {weapons?.map((w) => (
-                    <LoadoutItem key={`mech-loadout-weapon-${w.id}`} imageUrl={w?.image_url} primaryColor={primaryColor} tooltipText={w.label} />
+                    <LoadoutItem
+                        key={`mech-loadout-weapon-${w.id}`}
+                        imageUrl={w?.image_url}
+                        Icon={<SvgWeapons size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={w.label}
+                    />
                 ))}
                 {utilities?.map((u) => (
-                    <LoadoutItem key={`mech-loadout-utility-${u.id}`} imageUrl={u?.image_url} primaryColor={primaryColor} tooltipText={u.label} />
+                    <LoadoutItem
+                        key={`mech-loadout-utility-${u.id}`}
+                        imageUrl={u?.image_url}
+                        Icon={<SvgUtilities size="1.1rem" />}
+                        primaryColor={primaryColor}
+                        tooltipText={u.label}
+                    />
                 ))}
                 <AddLoadoutItem primaryColor={primaryColor} />
             </Stack>
@@ -60,9 +101,9 @@ export const MechLoadout = ({ mech, mechDetails }: { mech: MechBasic; mechDetail
     )
 }
 
-const LoadoutItem = ({ imageUrl, primaryColor, tooltipText }: { imageUrl?: string; primaryColor: string; tooltipText?: string }) => {
+const LoadoutItem = ({ imageUrl, primaryColor, tooltipText, Icon }: { imageUrl?: string; primaryColor: string; tooltipText?: string; Icon: ReactNode }) => {
     return (
-        <Box sx={{ flexBasis: "50%", width: `${ITEM_WIDTH}rem`, p: ".3rem" }}>
+        <Box sx={{ position: "relative", flexBasis: "50%", width: `${ITEM_WIDTH}rem`, p: ".2rem" }}>
             <FancyButton
                 excludeCaret
                 clipThingsProps={{
@@ -100,13 +141,28 @@ const LoadoutItem = ({ imageUrl, primaryColor, tooltipText }: { imageUrl?: strin
                     </Box>
                 </TooltipHelper>
             </FancyButton>
+
+            <Box
+                sx={{
+                    position: "absolute",
+                    right: ".4rem",
+                    bottom: ".2rem",
+                    pl: ".4rem",
+                    pt: ".2rem",
+                    opacity: 0.82,
+                    borderTopLeftRadius: "50%",
+                    backgroundColor: "#000000DD",
+                }}
+            >
+                {Icon}
+            </Box>
         </Box>
     )
 }
 
 const AddLoadoutItem = ({ primaryColor }: { primaryColor: string }) => {
     return (
-        <Box sx={{ flexBasis: "50%", width: `${ITEM_WIDTH}rem`, p: ".3rem" }}>
+        <Box sx={{ flexBasis: "50%", width: `${ITEM_WIDTH}rem`, p: ".2rem" }}>
             <FancyButton
                 excludeCaret
                 clipThingsProps={{
