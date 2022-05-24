f4d58f1be343023642ce847bd33d4d3cef6772f3
James <jayli3n@gmail.com>
2022-05-24 21:09:33 +0800

    Template ws for rental

--- src/components/Hangar/WarMachines/MechRental/RentalModal.tsx
+++ src/components/Hangar/WarMachines/MechRental/RentalModal.tsx
@@ -6,6 +6,41 @@ import { MechModal } from "../Common/MechModal"
 export const RentalModal = () => {
     const { rentalMechDetails, setRentalMechDetails } = useHangarWarMachine()
 
+    // // Fetch
+    // const { send } = useGameServerCommandsYYY("xxxxxxxxx")
+
+    // useEffect(() => {
+    //     ;(async () => {
+    //         try {
+    //             const resp = await send<RESPONSE_TYPE>(GameServerKeys.XXXXXX, {
+    //                 payload: something,
+    //             })
+
+    //             if (!resp) return
+    //             setFactionsData(resp)
+    //         } catch (e) {
+    //             console.error(e)
+    //         }
+    //     })()
+    // }, [send])
+
+    // // Subscription
+    // const payload = useGameServerSubscriptionYYY<RESPONSE_TYPE>({
+    //     URI: "/xxxxxxxxx",
+    //     key: GameServerKeys.SomeKey,
+    // })
+
+    // useGameServerSubscriptionYYY<RESPONSE_TYPE>(
+    //     {
+    //         URI: "/xxxxxxxxx",
+    //         key: GameServerKeys.SomeKey,
+    //     },
+    //     (payload) => {
+    //         if (!payload) return
+    //         setState(payload)
+    //     },
+    // )
+
     const onClose = useCallback(() => {
         setRentalMechDetails(undefined)
     }, [setRentalMechDetails])
