dc7c509e1eac5d52f7f891a8fae3ebe51c20d154
James Li <jayli3n@gmail.com>
2022-05-27 18:03:58 +0800

    DONEW

--- src/components/RightDrawer/LiveChat/BanProposal/BanProposal.tsx
+++ src/components/RightDrawer/LiveChat/BanProposal/BanProposal.tsx
@@ -1,13 +1,14 @@
 import { Box, Divider, Grow, Stack, Typography } from "@mui/material"
 import { ReactNode, useCallback, useEffect, useMemo, useState } from "react"
-import { FancyButton, TooltipHelper } from "../.."
-import { SvgCooldown, SvgInfoCircular, SvgSupToken } from "../../../assets"
-import { useChat, useGameServerAuth, useGameServerWebsocket } from "../../../containers"
-import { getUserRankDeets, snakeToTitle, supFormatterNoFixed } from "../../../helpers"
-import { useTimer, useToggle } from "../../../hooks"
-import { GameServerKeys } from "../../../keys"
-import { colors } from "../../../theme/theme"
-import { BanProposalStruct } from "../../../types/chat"
+import { FancyButton, TooltipHelper } from "../../.."
+import { SvgCooldown, SvgInfoCircular, SvgSupToken } from "../../../../assets"
+import { useChat, useGameServerAuth, useGameServerWebsocket } from "../../../../containers"
+import { getUserRankDeets, snakeToTitle, supFormatterNoFixed } from "../../../../helpers"
+import { useTimer, useToggle } from "../../../../hooks"
+import { GameServerKeys } from "../../../../keys"
+import { colors } from "../../../../theme/theme"
+import { BanProposalStruct } from "../../../../types/chat"
+import { InstantPunishConfirmModal } from "./InstantPunishConfirmModal"
 
 export const BanProposal = () => {
     const { banProposal } = useChat()
@@ -55,8 +56,9 @@ const BanProposalInner = ({
     const [submitted, setSubmitted] = useState(!!banProposal.decision)
     const [submittedVote, setSubmittedVote] = useState(banProposal.decision?.is_agreed)
     const [error, setError] = useState("")
+    const [instantPunishModalOpen, toggleInstantPunishModalOpen] = useToggle()
 
-    const rankDeets = useMemo(() => (userRank ? getUserRankDeets(userRank, "1rem", "1.2rem") : undefined), [userRank])
+    const rankDeets = useMemo(() => getUserRankDeets("GENERAL", "1rem", "1.2rem"), [])
 
     const submitVote = useCallback(
         async (isAgree: boolean) => {
@@ -79,25 +81,22 @@ const BanProposalInner = ({
         [state, send, banProposal],
     )
 
-    const submitInstantBan = useCallback(
-        async (isAgree: boolean) => {
-            if (state !== WebSocket.OPEN || !send) return
-            try {
-                const resp = await send<boolean, { punish_vote_id: string }>(GameServerKeys.SubmitInstantBan, {
-                    punish_vote_id: banProposal.id,
-                })
+    const submitInstantPunish = useCallback(async () => {
+        if (state !== WebSocket.OPEN || !send) return
+        try {
+            const resp = await send<boolean, { punish_vote_id: string }>(GameServerKeys.SubmitInstantBan, {
+                punish_vote_id: banProposal.id,
+            })
 
-                if (resp) {
-                    setSubmitted(true)
-                    setSubmittedVote(isAgree)
-                    setError("")
-                }
-            } catch (e) {
-                setError(typeof e === "string" ? e : "Failed to submit your vote.")
+            if (resp) {
+                setSubmitted(true)
+                setSubmittedVote(true)
+                setError("")
             }
-        },
-        [banProposal.id, send, state],
-    )
+        } catch (e) {
+            setError(typeof e === "string" ? e : "Failed to submit your vote.")
+        }
+    }, [banProposal.id, send, state])
 
     const bottomSection = useMemo(() => {
         if (!userStat || (userStat.last_seven_days_kills < 5 && userStat.ability_kill_count < 100 && userRank !== "GENERAL")) {
@@ -130,20 +129,15 @@ const BanProposalInner = ({
                             clipSize: "4px",
                             backgroundColor: colors.darkNavyBlue,
                             border: { borderColor: userRank !== "GENERAL" ? "#FFFFFF90" : "#FFFFFF", borderThickness: "2px" },
-                            sx: { flex: 2.8, position: "relative" },
+                            sx: { flex: 1.6, position: "relative" },
                         }}
                         sx={{ pt: ".2rem", pb: 0, minWidth: "5rem" }}
-                        onClick={() => submitInstantBan(true)}
+                        onClick={() => toggleInstantPunishModalOpen(true)}
                         disabled={userRank !== "GENERAL"}
                     >
                         {rankDeets?.icon}
                         <Typography variant="body2" sx={{ ml: ".5rem", fontWeight: "fontWeightBold" }}>
                             INSTANT PUNISH
-                        </Typography>
-
-                        <SvgSupToken size="1.2rem" sx={{ ml: ".5rem" }} />
-                        <Typography variant="body2" sx={{ fontWeight: "fontWeightBold" }}>
-                            ({supFormatterNoFixed(banProposal.instant_pass_fee, 0)})
                         </Typography>
                     </FancyButton>
 
@@ -183,69 +177,79 @@ const BanProposalInner = ({
                 )}
             </>
         )
-    }, [userStat, userRank, submitted, rankDeets?.icon, banProposal.instant_pass_fee, error, submittedVote, submitInstantBan, submitVote])
+    }, [userStat, userRank, submitted, rankDeets?.icon, error, submittedVote, toggleInstantPunishModalOpen, submitVote])
 
     return (
-        <Grow in={!outOfTime} timeout={250}>
-            <Box sx={{ m: ".5rem", border: `${colors.red} 2px solid` }}>
-                <Stack
-                    sx={{
-                        px: "1rem",
-                        py: ".2rem",
-                        backgroundColor: colors.red,
-                    }}
-                    direction="row"
-                    justifyContent="space-between"
-                >
-                    <Typography sx={{ fontWeight: "fontWeightBold" }}>PUNISHMENT PROPOSAL</Typography>
-                    <Typography sx={{ fontWeight: "fontWeightBold", px: "1rem", backgroundColor: "#00000090" }}>
-                        <Countdown endTime={banProposal.ended_at} toggleOutOfTime={toggleOutOfTime} />s
-                    </Typography>
-                </Stack>
-
-                <Box sx={{ px: "1.2rem", py: ".9rem" }}>
-                    <Stack spacing=".3rem">
-                        <LineItem title="INITIATOR" color={colors.green}>
-                            <Typography sx={{ lineHeight: 1 }}>
-                                {`${banProposal.issued_by_username}`}
-                                <span style={{ marginLeft: ".2rem", opacity: 0.7 }}>{`#${banProposal.issued_by_gid}`}</span>
-                            </Typography>
-                        </LineItem>
-
-                        <LineItem title="AGAINST">
-                            <Typography sx={{ lineHeight: 1 }}>
-                                {`${banProposal.reported_player_username}`}
-                                <span style={{ marginLeft: ".2rem", opacity: 0.7 }}>{`#${banProposal.reported_player_gid}`}</span>
-                            </Typography>
-                        </LineItem>
-
-                        <LineItem title="PUNISH">
-                            <Typography sx={{ lineHeight: 1 }}>{snakeToTitle(banProposal.punish_option.key)}</Typography>
-                            <TooltipHelper placement="bottom" text={banProposal.punish_option.description}>
-                                <Box>
-                                    <SvgInfoCircular size="1.1rem" sx={{ pt: 0, pb: 0, opacity: 0.4, ":hover": { opacity: 1 } }} />
-                                </Box>
-                            </TooltipHelper>
-                        </LineItem>
-
-                        <LineItem title="DURATION">
-                            <Stack spacing=".24rem" direction="row" alignItems="center" justifyContent="center">
-                                <SvgCooldown component="span" size="1.4rem" sx={{ pb: ".25rem" }} />
-                                <Typography sx={{ lineHeight: 1 }}>{banProposal.punish_option.punish_duration_hours} Hrs</Typography>
-                            </Stack>
-                        </LineItem>
-
-                        <LineItem title="REASON">
-                            <Typography sx={{ lineHeight: 1 }}>{banProposal.reason}</Typography>
-                        </LineItem>
+        <>
+            <Grow in={!outOfTime} timeout={250}>
+                <Box sx={{ m: ".5rem", border: `${colors.red} 2px solid` }}>
+                    <Stack
+                        sx={{
+                            px: "1rem",
+                            py: ".2rem",
+                            backgroundColor: colors.red,
+                        }}
+                        direction="row"
+                        justifyContent="space-between"
+                    >
+                        <Typography sx={{ fontWeight: "fontWeightBold" }}>PUNISHMENT PROPOSAL</Typography>
+                        <Typography sx={{ fontWeight: "fontWeightBold", px: "1rem", backgroundColor: "#00000090" }}>
+                            <Countdown endTime={banProposal.ended_at} toggleOutOfTime={toggleOutOfTime} />s
+                        </Typography>
                     </Stack>
 
-                    <Divider sx={{ mt: "1.1rem", mb: ".7rem" }} />
+                    <Box sx={{ px: "1.2rem", py: ".9rem" }}>
+                        <Stack spacing=".3rem">
+                            <LineItem title="INITIATOR" color={colors.green}>
+                                <Typography sx={{ lineHeight: 1 }}>
+                                    {`${banProposal.issued_by_username}`}
+                                    <span style={{ marginLeft: ".2rem", opacity: 0.7 }}>{`#${banProposal.issued_by_gid}`}</span>
+                                </Typography>
+                            </LineItem>
 
-                    <Stack spacing=".4rem">{bottomSection}</Stack>
+                            <LineItem title="AGAINST">
+                                <Typography sx={{ lineHeight: 1 }}>
+                                    {`${banProposal.reported_player_username}`}
+                                    <span style={{ marginLeft: ".2rem", opacity: 0.7 }}>{`#${banProposal.reported_player_gid}`}</span>
+                                </Typography>
+                            </LineItem>
+
+                            <LineItem title="PUNISH">
+                                <Typography sx={{ lineHeight: 1 }}>{snakeToTitle(banProposal.punish_option.key)}</Typography>
+                                <TooltipHelper placement="bottom" text={banProposal.punish_option.description}>
+                                    <Box>
+                                        <SvgInfoCircular size="1.1rem" sx={{ pt: 0, pb: 0, opacity: 0.4, ":hover": { opacity: 1 } }} />
+                                    </Box>
+                                </TooltipHelper>
+                            </LineItem>
+
+                            <LineItem title="DURATION">
+                                <Stack spacing=".24rem" direction="row" alignItems="center" justifyContent="center">
+                                    <SvgCooldown component="span" size="1.4rem" sx={{ pb: ".25rem" }} />
+                                    <Typography sx={{ lineHeight: 1 }}>{banProposal.punish_option.punish_duration_hours} Hrs</Typography>
+                                </Stack>
+                            </LineItem>
+
+                            <LineItem title="REASON">
+                                <Typography sx={{ lineHeight: 1 }}>{banProposal.reason}</Typography>
+                            </LineItem>
+                        </Stack>
+
+                        <Divider sx={{ mt: "1.1rem", mb: ".7rem" }} />
+
+                        <Stack spacing=".4rem">{bottomSection}</Stack>
+                    </Box>
                 </Box>
-            </Box>
-        </Grow>
+            </Grow>
+
+            {instantPunishModalOpen && (
+                <InstantPunishConfirmModal
+                    submitInstantPunish={submitInstantPunish}
+                    onClose={() => toggleInstantPunishModalOpen(false)}
+                    cost={banProposal.instant_pass_fee}
+                />
+            )}
+        </>
     )
 }
 
