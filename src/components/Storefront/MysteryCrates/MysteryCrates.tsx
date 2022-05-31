d170705f89bf0f3e0ee7af73f889b1fd58b2eb4e
James Li <jayli3n@gmail.com>
2022-05-31 15:23:55 +0800

    Style

--- src/components/Storefront/MysteryCrates/MysteryCrates.tsx
+++ src/components/Storefront/MysteryCrates/MysteryCrates.tsx
@@ -212,8 +212,7 @@ export const MysteryCrates = () => {
                         sx={{
                             my: ".8rem",
                             ml: ".8rem",
-                            mr: ".4rem",
-                            pr: "1.2rem",
+                            pr: "1.5rem",
                             flex: 1,
                             overflowY: "auto",
                             overflowX: "hidden",
