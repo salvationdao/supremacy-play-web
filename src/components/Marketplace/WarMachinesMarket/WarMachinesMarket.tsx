a27bca0575828b00d0a09e0237e87900f97a5a62
James Li <jayli3n@gmail.com>
2022-06-01 15:16:27 +0800

    Style

--- src/components/Marketplace/WarMachinesMarket/WarMachinesMarket.tsx
+++ src/components/Marketplace/WarMachinesMarket/WarMachinesMarket.tsx
@@ -102,11 +102,6 @@ export const WarMachinesMarket = () => {
                     borderColor: theme.factionTheme.primary,
                     borderThickness: ".3rem",
                 }}
-                corners={{
-                    topRight: true,
-                    bottomLeft: true,
-                    bottomRight: true,
-                }}
                 opacity={0.7}
                 backgroundColor={theme.factionTheme.background}
                 sx={{ height: "100%", flex: 1 }}
