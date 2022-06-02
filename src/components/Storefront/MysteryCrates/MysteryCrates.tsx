e01bb8ef73053505158bba18a80d5e5fe74088c5
James Li <jayli3n@gmail.com>
2022-06-02 13:11:07 +0800

    2 crates

--- src/components/Storefront/MysteryCrates/MysteryCrates.tsx
+++ src/components/Storefront/MysteryCrates/MysteryCrates.tsx
@@ -44,17 +44,17 @@ export const MysteryCrates = () => {
     useEffect(() => {
         // TODO: remove later
         setCrates([...placeholderCrates])
-        setCrates([
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-            ...placeholderCrates,
-        ])
+        // setCrates([
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        //     ...placeholderCrates,
+        // ])
         setIsLoading(false)
         return
         ;(async () => {
