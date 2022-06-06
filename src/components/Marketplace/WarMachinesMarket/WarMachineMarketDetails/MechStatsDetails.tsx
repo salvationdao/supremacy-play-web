195a6f58b8fc7f5f2faee6912c01cec41c3d5608
James Li <jayli3n@gmail.com>
2022-06-06 19:51:58 +0800

    WIP

--- src/components/Marketplace/WarMachinesMarket/WarMachineMarketDetails/MechStatsDetails.tsx
+++ src/components/Marketplace/WarMachinesMarket/WarMachineMarketDetails/MechStatsDetails.tsx
@@ -1,6 +1,21 @@
+import { Box } from "@mui/material"
+import { useTheme } from "../../../../containers/theme"
 import { MechDetails } from "../../../../types"
 
 export const MechStatsDetails = ({ mechDetails }: { mechDetails?: MechDetails }) => {
-    console.log(mechDetails)
-    return null
+    const theme = useTheme()
+    const primaryColor = theme.factionTheme.primary
+
+    if (!mechDetails) return null
+
+    const { chassis_skin_id, intro_animation_id, outro_animation_id, power_core_id } = mechDetails
+
+    const chassisSkin = mechDetails?.chassis_skin
+    const introAnimation = mechDetails?.intro_animation
+    const outroAnimation = mechDetails?.outro_animation
+    const powerCore = mechDetails?.power_core
+    const weapons = mechDetails?.weapons
+    const utilities = mechDetails?.utility
+
+    return <Box>TODO</Box>
 }
