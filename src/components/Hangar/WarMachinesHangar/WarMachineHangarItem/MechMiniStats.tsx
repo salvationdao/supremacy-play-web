e91eced9593ccd1aa387299eed1380b321972944
owengiri20 <owengiri20@gmail.com>
2022-06-10 11:18:17 +0800

    skin to sub model

--- src/components/Hangar/WarMachinesHangar/WarMachineHangarItem/MechMiniStats.tsx
+++ src/components/Hangar/WarMachinesHangar/WarMachineHangarItem/MechMiniStats.tsx
@@ -43,7 +43,7 @@ export const MechMiniStats = ({ mech, mechDetails }: { mech: MechBasic; mechDeta
                 }}
             >
                 <Stack alignItems="center" justifyContent="center" spacing=".4rem" sx={{ height: "100%", px: "1rem", py: ".8rem" }}>
-                    <SingleStat Icon={<SvgSkin size="1.5rem" />} current={chassis_skin_id ? 1 : 0} total={1} tooltipText="Chassis skin." />
+                    <SingleStat Icon={<SvgSkin size="1.5rem" />} current={chassis_skin_id ? 1 : 0} total={1} tooltipText="Submodel" />
                     <SingleStat Icon={<SvgIntroAnimation size="1.5rem" />} current={intro_animation_id ? 1 : 0} total={1} tooltipText="Intro animation." />
                     <SingleStat Icon={<SvgOutroAnimation size="1.5rem" />} current={outro_animation_id ? 1 : 0} total={1} tooltipText="Outro animation." />
                     <SingleStat Icon={<SvgPowerCore size="1.5rem" />} current={power_core_id ? 1 : 0} total={1} tooltipText="Power core." />
