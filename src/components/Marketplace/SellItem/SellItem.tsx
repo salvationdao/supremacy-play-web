7afbea2a0803213804f41eb9adcb7c5048e4fc87
James Li <jayli3n@gmail.com>
2022-06-11 23:13:08 +0800

    Skeleton ready for the sell item page

--- src/components/Marketplace/SellItem/SellItem.tsx
+++ src/components/Marketplace/SellItem/SellItem.tsx
@@ -1,19 +1,8 @@
-import { Box, CircularProgress, Stack, Typography } from "@mui/material"
-import { useEffect, useMemo, useState } from "react"
+import { Box, Stack, Typography } from "@mui/material"
 import { useTheme } from "../../../containers/theme"
-import { consolidateMarketItemDeets, getRarityDeets, MarketItemDeets } from "../../../helpers"
 import { useGameServerCommandsFaction } from "../../../hooks/useGameServer"
-import { GameServerKeys } from "../../../keys"
-import { colors, fonts } from "../../../theme/theme"
-import { MechDetails } from "../../../types"
-import { MarketplaceBuyAuctionItem } from "../../../types/marketplace"
+import { fonts } from "../../../theme/theme"
 import { ClipThing } from "../../Common/ClipThing"
-import { AuctionDetails } from "../Common/MarketDetails/AuctionDetails"
-import { BuyNowDetails } from "../Common/MarketDetails/BuyNowDetails"
-import { Dates } from "../Common/MarketDetails/Dates"
-import { ImagesPreview, MarketMedia } from "../Common/MarketDetails/ImagesPreview"
-import { ListingType } from "../Common/MarketDetails/ListingType"
-import { Owner } from "../Common/MarketDetails/Owner"
 
 export const SellItem = () => {
     const theme = useTheme()
@@ -63,46 +52,29 @@ export const SellItem = () => {
                     }}
                 >
                     <Box sx={{ direction: "ltr", height: 0 }}>
-                        <Box sx={{ pt: "2rem", pb: "3.8rem", px: "3rem" }}>
-                            <Box sx={{ height: "1000rem", width: "30rem", backgroundColor: "red" }}></Box>
+                        <Box sx={{ px: "3rem", py: "2rem" }}>
+                            <Typography variant="h4" sx={{ fontFamily: fonts.nostromoHeavy }}>
+                                SELL AN ITEM
+                            </Typography>
                         </Box>
                     </Box>
                 </Box>
             </Stack>
-
-            {/* <Box
-                sx={{
-                    height: "100%",
-                    overflowY: "auto",
-                    overflowX: "hidden",
-                    mr: "1rem",
-                    mt: "1.2rem",
-                    mb: "3rem",
-                    direction: "ltr",
-                    scrollbarWidth: "none",
-                    "::-webkit-scrollbar": {
-                        width: ".4rem",
-                    },
-                    "::-webkit-scrollbar-track": {
-                        background: "#FFFFFF15",
-                        borderRadius: 3,
-                    },
-                    "::-webkit-scrollbar-thumb": {
-                        background: primaryColor,
-                        borderRadius: 3,
-                    },
-                }}
-            >
-                <Box
-                    sx={{
-                        maxHeight: 0,
-                        px: "5rem",
-                        py: "2.8rem",
-                    }}
-                >
-                    <Box sx={{ height: "1000rem", width: "30rem", backgroundColor: "red" }}></Box>
-                </Box>
-            </Box> */}
         </ClipThing>
     )
 }
+
+/**
+ * What they're selling:
+ * - war machines
+ * - keycards
+ * - mystery crates
+ *
+ * List the items and they choose
+ *
+ * Listing type:
+ * - buyout					price
+ * - auction				reserve price
+ * - auction or buyout		price, reserve price
+ * - dutch auction			starting price, reserve price, drop rate
+ */
--- src/pages/MarketplaceSellPage.tsx
+++ src/pages/MarketplaceSellPage.tsx
@@ -3,57 +3,65 @@ import { useCallback } from "react"
 import { useHistory } from "react-router-dom"
 import { HangarBg, SvgBack } from "../assets"
 import { FancyButton } from "../components"
+import { SellItemModal } from "../components/Marketplace/Common/SellItemModal"
 import { SellItem } from "../components/Marketplace/SellItem/SellItem"
+import { useToggle } from "../hooks"
 import { fonts, siteZIndex } from "../theme/theme"
 
 export const MarketplaceSellPage = () => {
     const history = useHistory()
+    const [open, toggleOpen] = useToggle()
 
     const goBack = useCallback(() => {
         history.goBack()
     }, [history])
 
     return (
-        <Stack
-            alignItems="center"
-            sx={{
-                height: "100%",
-                zIndex: siteZIndex.RoutePage,
-                backgroundImage: `url(${HangarBg})`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                boxShadow: `inset 0 0 50px 60px #00000090`,
-            }}
-        >
-            <Stack spacing=".6rem" sx={{ mt: "1.5rem", mb: "2rem", height: "100%", width: "calc(100% - 3rem)", maxWidth: "132rem" }}>
-                <FancyButton
-                    excludeCaret
-                    clipThingsProps={{
-                        clipSize: "9px",
-                        corners: { topLeft: true },
-                        opacity: 1,
-                        sx: { position: "relative", alignSelf: "flex-start", opacity: 0.5, ":hover": { opacity: 1 } },
-                    }}
-                    sx={{ px: "1.6rem", py: ".6rem", color: "#FFFFFF" }}
-                    onClick={goBack}
-                >
-                    <Stack spacing=".6rem" direction="row" alignItems="center">
-                        <SvgBack size="1.4rem" fill={"#FFFFFF"} />
-                        <Typography
-                            variant="caption"
-                            sx={{
-                                color: "#FFFFFF",
-                                fontFamily: fonts.nostromoBlack,
-                            }}
-                        >
-                            GO BACK
-                        </Typography>
-                    </Stack>
-                </FancyButton>
+        <>
+            <Stack
+                alignItems="center"
+                sx={{
+                    height: "100%",
+                    zIndex: siteZIndex.RoutePage,
+                    backgroundImage: `url(${HangarBg})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    boxShadow: `inset 0 0 50px 60px #00000090`,
+                }}
+            >
+                <Stack spacing=".6rem" sx={{ mt: "1.5rem", mb: "2rem", height: "100%", width: "calc(100% - 3rem)", maxWidth: "132rem" }}>
+                    <FancyButton
+                        excludeCaret
+                        clipThingsProps={{
+                            clipSize: "9px",
+                            corners: { topLeft: true },
+                            opacity: 1,
+                            sx: { position: "relative", alignSelf: "flex-start", opacity: 0.5, ":hover": { opacity: 1 } },
+                        }}
+                        sx={{ px: "1.6rem", py: ".6rem", color: "#FFFFFF" }}
+                        // onClick={goBack}
+                        onClick={() => toggleOpen(true)}
+                    >
+                        <Stack spacing=".6rem" direction="row" alignItems="center">
+                            <SvgBack size="1.4rem" fill={"#FFFFFF"} />
+                            <Typography
+                                variant="caption"
+                                sx={{
+                                    color: "#FFFFFF",
+                                    fontFamily: fonts.nostromoBlack,
+                                }}
+                            >
+                                GO BACK
+                            </Typography>
+                        </Stack>
+                    </FancyButton>
 
-                <SellItem />
+                    <SellItem />
+                </Stack>
             </Stack>
-        </Stack>
+
+            {open && <SellItemModal onClose={() => toggleOpen(false)} />}
+        </>
     )
 }
