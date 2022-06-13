b40d6e0941addc135afa887805b154b8f188ded6
James Li <jayli3n@gmail.com>
2022-06-13 15:56:42 +0800

    Better skew

--- src/components/Marketplace/Common/MarketItem/BuyoutPrice.tsx
+++ src/components/Marketplace/Common/MarketItem/BuyoutPrice.tsx
@@ -37,7 +37,7 @@ export const BuyoutPrice = ({
                         <Typography variant="caption" sx={{ color: primaryColor, fontFamily: fonts.nostromoBold }}>
                             &nbsp;-&nbsp;
                         </Typography>
-                        <SvgSupToken size="1.5rem" fill={primaryColor} sx={{ transform: "rotate(15deg)" }} />
+                        <SvgSupToken size="1.5rem" fill={primaryColor} sx={{ transform: "skew(-20deg)" }} />
                         <Typography variant="caption" sx={{ color: primaryColor, fontFamily: fonts.nostromoBold }}>
                             {formattedDropPrice}/MIN
                         </Typography>
