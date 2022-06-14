776174ca045dbd8979ffe48973ccbcff48e56441
owengiri20 <owengiri20@gmail.com>
2022-06-14 18:13:11 +0800

    clean up

--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -10,38 +10,6 @@ import { Trailer } from "./Trailer"
 
 import OvenPlayer from "ovenplayer"
 
-interface OvenPlayerConfig {
-    aspectRatio?: string
-    title?: string
-    waterMark?: object
-    autoStart?: boolean
-    autoFallback?: boolean
-    controls?: boolean
-    loop?: boolean
-    showBigPlayButton?: boolean
-    disableSeekUI?: boolean
-    showSeekControl?: boolean
-    seekControlInterval?: number
-    expandFullScreenUI?: boolean
-    mute?: boolean
-    timecode?: boolean
-    playbackRate?: number
-    playbackRates?: number[]
-    currentProtocolOnly?: boolean
-    tracks?: object[]
-    volume?: number
-    adTagUrl?: string
-    adClient?: "googleima" | "vast"
-    playlist?: OvenPlayerPlayList
-    hidePlaylistIcon?: boolean
-    webrtcConfig?: object
-    hlsConfig?: any
-    dashConfig?: any
-    sources: OvenPlayerSource[]
-}
-
-type OvenPlayerPlayList = OvenPlayerSource[][]
-
 interface OvenPlayerSource {
     type: "webrtc" | "llhls" | "hls" | "lldash" | "dash" | "mp4"
     file: string
@@ -57,17 +25,6 @@ export const Stream = () => {
     const { currentStream, isMute, streamResolutions, vidRefCallback } = useStream()
     const { isOpen } = useTour()
 
-    const options: OvenPlayerConfig = {
-        sources: [
-            {
-                label: "label_for_webrtc",
-                // Set the type to 'webrtc'
-                type: "mp4",
-                file: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
-            },
-        ],
-    }
-
     if (!watchedTrailer) {
         return <Trailer watchedTrailer={watchedTrailer} setWatchedTrailer={setWatchedTrailer} />
     }
@@ -78,16 +35,8 @@ export const Stream = () => {
 
     return (
         <>
-            {currentStream?.name === "newthing" ? (
-                <OutputPlayerOven
-                    playerConfig="file"
-                    link="test"
-                    conenctMetaSocket={() => {}}
-                    handlePlayEnd={(s: string) => {}}
-                    setCurrentPlayBack={(s: string) => {}}
-                    handleVideoSeek={(s: string) => {}}
-                    sources={options.sources}
-                />
+            {currentStream?.name === "stream2" ? (
+                <OutputPlayerOven playerConfig="file" link="test" />
             ) : (
                 <Stack sx={{ width: "100%", height: "100%", zIndex: siteZIndex.Stream }}>
                     {!isPlaying && <NoStreamScreen />}
@@ -197,163 +146,51 @@ const NoStreamScreen = () => {
     )
 }
 
-const OutputPlayerOven = (props: {
-    playerConfig: string
-    link: string
-    conenctMetaSocket: () => void
-
-    handlePlayEnd: (s: string) => void
-    setCurrentPlayBack: (arg: any) => void
-    handleVideoSeek: (arg: any) => void
-
-    sources: OvenPlayerSource[]
-}) => {
-    const [loading, setLoading] = useState(true)
-    const [showGoLiveButton, setShowGoLiveButton] = useState(false)
-    const [sourceFile, setSourceFile] = useState(true)
-    const [userSeek, setUserSeek] = useState(true)
-
-    let ovenPlayer: any = null
-    let reloadTimeout: any
-    let posTimeout: any
-
-    // useEffect(() => {
-    //     // OvenPlayer.debug(true)
-
-    //     return () => {
-    //         // cleaned up
-    //         ovenPlayer = null
-    //         clearTimeout(posTimeout)
-    //     }
-    // }, [])
-
-    // useEffect(() => {
-    //     if (props.playerConfig === "file") setSourceFile(true)
-    //     else setSourceFile(false)
-    // }, [props.playerConfig])
-
+const OutputPlayerOven = (props: { playerConfig: string; link: string }) => {
     const loadOvenPlayer = () => {
-        console.log("in loadOvenPlayer")
-
-        // if (ovenPlayer) {
-        //   ovenPlayer.remove()
-        // }
-
-        // let source: OvenPlayerSource[] = [
-        //     {
-        //         label: `testStream`,
-        //         type: "webrtc",
-        //         file: "path",
-        //     },
-        // ]
-
         if (document.getElementById("player")) {
-            ovenPlayer = OvenPlayer.create("player", {
-                // autoStart: true,
-                // controls: true,
-                // mute: true,
-                // autoFallback: true,
-                // currentProtocolOnly: false,
-                // showBigPlayButton: false,
-                // showSeekControl: false,
-                // disableSeekUI: false,
-                // hidePlaylistIcon: true,
-                // timecode: true,
-                // playbackRate: 1,
-                // playbackRates: [1],
-                sources: props.sources,
+            // load oven player
+            const source: OvenPlayerSource = {
+                label: "label_for_webrtc",
+                // Set the type to 'webrtc'
+                type: "webrtc",
+                file: "wss://stream2.supremacy.game:3334/app/stream2",
+            }
+            const ovenPlayer = OvenPlayer.create("player", {
+                autoStart: true,
+                controls: false,
+                mute: false,
+                sources: [source],
+                autoFallback: true,
             })
 
             // Reload OvenPlayer when error occured.
             ovenPlayer.on("error", (error: any) => {
-                // console.log('error =>', error)
-
-                // Wait 2 sec and relaod.
-                reloadTimeout = setTimeout(() => {
-                    loadOvenPlayer()
-                }, 2000)
+                console.log("error =>", error)
             })
 
             ovenPlayer.on("ready", () => {
-                getPosition()
+                console.log("oven ready")
             })
+
             ovenPlayer.on("metaChanged", (event: any) => {
                 console.log("metaChanged =>", event)
-                props.conenctMetaSocket()
             })
+
             ovenPlayer.on("stateChanged", (event: any) => {
                 if (event.newstate === "playing") {
-                    clearTimeout(reloadTimeout)
-                    console.log("Play time=>", new Date())
-                    setLoading(false)
                 } else if (event.newstate === "complete") {
-                    props.handlePlayEnd("ended")
-                    clearTimeout(posTimeout)
                 }
             })
-            ovenPlayer.on("seeked", () => {
-                setLoading(false)
-            })
-            ovenPlayer.on("time", (event: any) => {
-                console.log("time update =>", event)
-                props.setCurrentPlayBack(event.position)
-                // seekHandler(event: any)
-            })
         }
-    }
-
-    const getPosition = () => {
-        // if (ovenPlayer) {
-        //     let position = ovenPlayer.getPosition()
-        //     console.log("position before if =>", position)
-        //     if (position) {
-        //         console.log("position =>", position)
-        //         props.setCurrentPlayBack(position)
-        //     }
-        // }
-        // posTimeout = setTimeout(() => {
-        //     getPosition()
-        // }, 1000)
     }
 
     useEffect(() => {
-        if ((props.link || props.sources.length) && props.playerConfig) loadOvenPlayer()
+        loadOvenPlayer()
     }, [])
-
-    const handleLiveClick = () => {
-        if (!sourceFile) {
-            setShowGoLiveButton(false)
-        }
-        const player = document.getElementById("player")
-        // player.currentTime = player.duration
-    }
-
-    const seekHandler = (event: any) => {
-        if (!sourceFile) {
-            if (userSeek) {
-                setUserSeek(false)
-            } else {
-                setShowGoLiveButton(true)
-                console.log("handleVideoSeek =>", event.newstate, event.position, event.newstate - event.position)
-                if (event.newstate - event.position < 60) {
-                    props.handleVideoSeek(undefined)
-                } else {
-                    props.handleVideoSeek(event.position)
-                }
-            }
-        }
-    }
-
     return (
-        <div className="outputPlayerWrapper">
-            {loading ? "loading" : null}
-            <div id="player" style={{ opacity: loading ? 0 : 1 }} />
-            {showGoLiveButton ? (
-                <div className="resumeLive" onClick={handleLiveClick}>
-                    <div className="resumeLiveIcon"></div>
-                    <span className="resumeLiveLabel">Go Live</span>
-                </div>
-            ) : null}
-        </div>
+        <Stack sx={{ width: "100%", height: "90%", zIndex: siteZIndex.Stream }}>
+            <div id="player" />
+        </Stack>
     )
 }
