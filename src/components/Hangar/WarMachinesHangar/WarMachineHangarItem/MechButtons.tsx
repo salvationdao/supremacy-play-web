4e6ff5aa3382cd2f132124a4bc3d21f57c50a768
ivanngyb <ivanng2@hotmail.com>
2022-06-16 23:02:33 +0800

    Lint

--- src/components/Hangar/WarMachinesHangar/WarMachineHangarItem/MechButtons.tsx
+++ src/components/Hangar/WarMachinesHangar/WarMachineHangarItem/MechButtons.tsx
@@ -1,4 +1,4 @@
-import { Box, Tooltip, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { useState } from "react"
 import { useLocation } from "react-router-dom"
 import { FancyButton, TooltipHelper } from "../../.."
