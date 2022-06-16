52ea6f6dd6580de90a41c694a3fb400b40e9aa99
James Li <jayli3n@gmail.com>
2022-06-16 17:01:48 +0800

    Moveable

--- src/components/MechDeployListModal/MechDeployListButton.tsx
+++ src/components/MechDeployListModal/MechDeployListButton.tsx
@@ -1,12 +1,21 @@
 import { Stack, IconButton, colors } from "@mui/material"
+import { useLocation } from "react-router"
 import { SvgRobot } from "../../assets"
 import { useSupremacy } from "../../containers"
 
 export const MechDeployListButton = () => {
     const { mechDeployModalOpen, toggleMechDeployModalOpen } = useSupremacy()
+    const location = useLocation()
+
     return (
         <Stack alignItems="center" sx={{ backgroundColor: mechDeployModalOpen ? colors.purple : `${colors.purple}60`, opacity: 0.9, ":hover": { opacity: 1 } }}>
-            <IconButton size="small" onClick={() => toggleMechDeployModalOpen()}>
+            <IconButton
+                size="small"
+                onClick={() => {
+                    if (location.pathname !== "/") return
+                    toggleMechDeployModalOpen(true)
+                }}
+            >
                 <SvgRobot size="1.6rem" />
             </IconButton>
         </Stack>
