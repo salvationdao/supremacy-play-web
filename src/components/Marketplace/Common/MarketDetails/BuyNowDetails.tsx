bb0ba5ff50cbded0a3c5100a36bdae207563b267
Yong Tan <ultimatoonmaster@gmail.com>
2022-06-29 12:02:12 +0800

    Passing in amount when buying item

--- src/components/Marketplace/Common/MarketDetails/BuyNowDetails.tsx
+++ src/components/Marketplace/Common/MarketDetails/BuyNowDetails.tsx
@@ -78,6 +78,7 @@ export const BuyNowDetails = ({
 
             const resp = await send(isKeycard ? GameServerKeys.MarketplaceSalesKeycardBuy : GameServerKeys.MarketplaceSalesBuy, {
                 id,
+                amount: currentPrice,
             })
 
             if (!resp) return
