79b0e30051f16fce4dc29c2c3a0ad334695243fd
James Li <jayli3n@gmail.com>
2022-07-07 13:56:53 +0800

    Fix game ui moveable boxes stuck when NaN

--- src/components/VotingSystem/PlayerAbilities/PlayerAbilities.tsx
+++ src/components/VotingSystem/PlayerAbilities/PlayerAbilities.tsx
@@ -154,7 +154,7 @@ export const PlayerAbilities = () => {
                             <Box
                                 sx={{
                                     display: "grid",
-                                    gridTemplateColumns: `repeat(${COLUMNS}, minmax(0, 1fr))`,
+                                    gridTemplateColumns: `repeat(${COLUMNS}, minmax(0, 25%))`,
                                     gridTemplateRows: `repeat(${ROWS}, 1fr)`,
                                     gap: ".6rem",
                                 }}
