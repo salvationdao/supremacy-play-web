fbbbe99466815fb38d2bc6684f02fa720b8f5663
James Li <jayli3n@gmail.com>
2022-07-11 10:57:45 +0800

    Style

--- src/components/RightDrawer/DrawerButtons.tsx
+++ src/components/RightDrawer/DrawerButtons.tsx
@@ -1,6 +1,5 @@
 import { Box, Tabs } from "@mui/material"
 import { useHistory, useLocation } from "react-router-dom"
-import { useMobile } from "../../containers"
 import { useTheme } from "../../containers/theme"
 import { HASH_ROUTES_ARRAY, RightDrawerHashes } from "../../routes"
 import { siteZIndex } from "../../theme/theme"
@@ -9,7 +8,6 @@ import { TabButton } from "../LeftDrawer/DrawerButtons"
 const DRAWER_BAR_WIDTH = 3 // rem
 
 export const DrawerButtons = () => {
-    const { isMobile } = useMobile()
     const theme = useTheme()
     const location = useLocation()
     const history = useHistory()
@@ -36,30 +34,28 @@ export const DrawerButtons = () => {
                 },
             }}
         >
-            {!isMobile && (
-                <Tabs value={0} orientation="vertical" variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile sx={{ flex: 1 }}>
-                    {HASH_ROUTES_ARRAY.map((r) => {
-                        return (
-                            <TabButton
-                                key={r.id}
-                                label={r.label}
-                                enable={true}
-                                icon={r.icon}
-                                onClick={() => {
-                                    if (location.hash === r.hash) {
-                                        history.replace(`${location.pathname}${RightDrawerHashes.None}`)
-                                        return
-                                    }
-                                    history.replace(`${location.pathname}${r.hash}`)
-                                }}
-                                isActive={location.hash === r.hash}
-                                primaryColor={theme.factionTheme.primary}
-                                secondaryColor={theme.factionTheme.secondary}
-                            />
-                        )
-                    })}
-                </Tabs>
-            )}
+            <Tabs value={0} orientation="vertical" variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile sx={{ flex: 1 }}>
+                {HASH_ROUTES_ARRAY.map((r) => {
+                    return (
+                        <TabButton
+                            key={r.id}
+                            label={r.label}
+                            enable={true}
+                            icon={r.icon}
+                            onClick={() => {
+                                if (location.hash === r.hash) {
+                                    history.replace(`${location.pathname}${RightDrawerHashes.None}`)
+                                    return
+                                }
+                                history.replace(`${location.pathname}${r.hash}`)
+                            }}
+                            isActive={location.hash === r.hash}
+                            primaryColor={theme.factionTheme.primary}
+                            secondaryColor={theme.factionTheme.secondary}
+                        />
+                    )
+                })}
+            </Tabs>
         </Box>
     )
 }
