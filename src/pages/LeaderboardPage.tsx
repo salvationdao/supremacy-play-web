3d2d7aef5e91d5056f047e87303b665a8b35edb9
James Li <jayli3n@gmail.com>
2022-07-19 21:57:20 +0800

    Add route and page

---
+++ src/pages/LeaderboardPage.tsx
@@ -0,0 +1,123 @@
+import { Box, Fade, Stack, Tab, Tabs } from "@mui/material"
+import { SyntheticEvent, useCallback, useEffect, useState } from "react"
+import { useHistory, useLocation, useParams } from "react-router-dom"
+import { HangarBg } from "../assets"
+import { ClipThing } from "../components"
+import { MysteryCrateBanner } from "../components/Common/PageHeaderBanners/MysteryCrateBanner"
+import { GlobalStats } from "../components/Leaderboard/GlobalStats/GlobalStats"
+import { useTheme } from "../containers/theme"
+import { ROUTES_MAP } from "../routes"
+import { siteZIndex } from "../theme/theme"
+
+export enum LEADERBOARD_TABS {
+    Global = "global",
+}
+
+export const LeaderboardPage = () => {
+    const theme = useTheme()
+    const location = useLocation()
+    const history = useHistory()
+    const { type } = useParams<{ type: LEADERBOARD_TABS }>()
+    const [currentValue, setCurrentValue] = useState<LEADERBOARD_TABS>()
+
+    // Make sure that the param route is correct, fix it if invalid
+    useEffect(() => {
+        if (Object.values(LEADERBOARD_TABS).includes(type)) return setCurrentValue(type)
+        history.replace(`${ROUTES_MAP.leaderboard.path.replace(":type", LEADERBOARD_TABS.Global)}${location.hash}`)
+    }, [history, location.hash, location.pathname, type])
+
+    const handleChange = useCallback(
+        (event: SyntheticEvent, newValue: LEADERBOARD_TABS) => {
+            setCurrentValue(newValue)
+            history.push(`${ROUTES_MAP.marketplace.path.replace(":type", newValue)}${location.hash}`)
+        },
+        [history, location.hash],
+    )
+
+    if (!currentValue) return null
+
+    const primaryColor = theme.factionTheme.primary
+    const secondaryColor = theme.factionTheme.secondary
+    const backgroundColor = theme.factionTheme.background
+
+    return (
+        <Stack
+            alignItems="center"
+            sx={{
+                height: "100%",
+                zIndex: siteZIndex.RoutePage,
+                backgroundImage: `url(${HangarBg})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                boxShadow: `inset 0 0 50px 60px #00000090`,
+            }}
+        >
+            <Stack sx={{ mt: "1.5rem", mb: "2rem", height: "100%", width: "calc(100% - 3rem)", maxWidth: "160rem" }}>
+                <Stack direction="row" alignItems="center" flexWrap="wrap" sx={{ mb: "1.1rem", gap: "1.2rem" }}>
+                    <ClipThing
+                        clipSize="10px"
+                        border={{
+                            borderColor: primaryColor,
+                            borderThickness: ".3rem",
+                        }}
+                        corners={{ topRight: true, bottomRight: true }}
+                        backgroundColor={backgroundColor}
+                        sx={{ maxWidth: "fit-content" }}
+                    >
+                        <Box sx={{ height: "100%" }}>
+                            <Tabs
+                                value={currentValue}
+                                onChange={handleChange}
+                                variant="scrollable"
+                                scrollButtons="auto"
+                                sx={{
+                                    flexShrink: 0,
+                                    color: primaryColor,
+                                    minHeight: 0,
+                                    ".MuiTab-root": { minHeight: 0, fontSize: "1.3rem", height: "6rem", width: "10rem" },
+                                    ".Mui-selected": {
+                                        color: `${secondaryColor} !important`,
+                                        background: `linear-gradient(${primaryColor} 26%, ${primaryColor}BB)`,
+                                    },
+                                    ".MuiTabs-indicator": { display: "none" },
+                                    ".MuiTabScrollButton-root": { display: "none" },
+                                }}
+                            >
+                                <Tab label="GLOBAL" value={LEADERBOARD_TABS.Global} />
+                            </Tabs>
+                        </Box>
+                    </ClipThing>
+
+                    <MysteryCrateBanner />
+                </Stack>
+
+                <TabPanel currentValue={currentValue} value={LEADERBOARD_TABS.Global}>
+                    <GlobalStats />
+                </TabPanel>
+            </Stack>
+        </Stack>
+    )
+}
+
+interface TabPanelProps {
+    children?: React.ReactNode
+    value: LEADERBOARD_TABS
+    currentValue: LEADERBOARD_TABS
+}
+
+const TabPanel = (props: TabPanelProps) => {
+    const { children, currentValue, value } = props
+
+    if (currentValue === value) {
+        return (
+            <Fade in>
+                <Box id={`marketplace-tabpanel-${value}`} sx={{ flex: 1 }}>
+                    {children}
+                </Box>
+            </Fade>
+        )
+    }
+
+    return null
+}
