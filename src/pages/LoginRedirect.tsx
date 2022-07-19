9b7f4ab71725be38ffc8f87f1073c51122e98ec5
James Li <jayli3n@gmail.com>
2022-07-19 12:56:36 +0800

    Styles

--- src/pages/LoginRedirect.tsx
+++ src/pages/LoginRedirect.tsx
@@ -1,12 +1,7 @@
 import { Stack, Typography } from "@mui/material"
 import { useEffect } from "react"
-import { colors } from "../theme/theme"
+import { colors, fonts } from "../theme/theme"
 
-/**
- * This componeny will be rendered as a page.
- * Back end opens this page with a url param "token" in an iframe.
- * This page will pass this token to the parent window (play-web) via postMessage.
- */
 export const LoginRedirect = () => {
     // Receives token from the url param and passes it to the parent via postMessage
     useEffect(() => {
@@ -18,7 +13,9 @@ export const LoginRedirect = () => {
 
     return (
         <Stack alignItems="center" justifyContent="center" sx={{ height: "100vh", p: "3.8rem", backgroundColor: colors.darkNavy }}>
-            <Typography sx={{ textAlign: "center", fontSize: "32px" }}>Loading...</Typography>
+            <Typography variant="h5" sx={{ fontFamily: fonts.nostromoBlack, textAlign: "center" }}>
+                LOADING...
+            </Typography>
         </Stack>
     )
 }
