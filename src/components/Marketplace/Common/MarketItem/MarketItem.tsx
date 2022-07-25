c96091166ec0d72943d59816f5cef84bbb7a1e93
James Li <jayli3n@gmail.com>
2022-07-25 10:57:58 +0800

    Styles

--- src/components/Marketplace/Common/MarketItem/MarketItem.tsx
+++ src/components/Marketplace/Common/MarketItem/MarketItem.tsx
@@ -14,14 +14,13 @@ import { Timeframe } from "../../Common/MarketItem/Timeframe"
 import { SoldPrice } from "./SoldPrice"
 
 interface MarketItemProps {
-    backgroundImageUrl?: string
     item: MarketplaceBuyAuctionItem
     isGridView: boolean
     children: ReactNode
     linkSubPath: MARKETPLACE_TABS
 }
 
-export const MarketItem = ({ backgroundImageUrl, item, isGridView, children, linkSubPath }: MarketItemProps) => {
+export const MarketItem = ({ item, isGridView, children, linkSubPath }: MarketItemProps) => {
     const theme = useTheme()
 
     const formattedBuyoutPrice = useMemo(() => {
@@ -111,22 +110,6 @@ export const MarketItem = ({ backgroundImageUrl, item, isGridView, children, lin
                         </>
                     )}
                 </Box>
-
-                <Box
-                    sx={{
-                        position: "absolute",
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        bottom: 0,
-                        background: `url(${backgroundImageUrl})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "top",
-                        backgroundSize: "cover",
-                        opacity: 0.06,
-                        zIndex: -2,
-                    }}
-                />
 
                 <Box
                     sx={{
