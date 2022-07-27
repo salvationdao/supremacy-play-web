bba85e0f58869b2191a243c6dffe310e057c07be
James Li <jayli3n@gmail.com>
2022-07-27 21:02:45 +0800

    Styles

--- src/components/Hangar/WarMachinesHangar/WarMachineDetails/Modals/RepairModal/SelfRepairCard.tsx
+++ src/components/Hangar/WarMachinesHangar/WarMachineDetails/Modals/RepairModal/SelfRepairCard.tsx
@@ -4,8 +4,8 @@ import { colors, fonts } from "../../../../../../theme/theme"
 
 export const SelfRepairCard = () => {
     return (
-        <Stack spacing="1rem" sx={{ p: "2rem", pt: "1.6rem", backgroundColor: "#FFFFFF20", border: `${colors.green}30 1px solid` }}>
-            <Typography variant="h6" sx={{ fontFamily: fonts.nostromoBlack, color: colors.green }}>
+        <Stack spacing="1rem" sx={{ p: "2rem", pt: "1.6rem", backgroundColor: "#FFFFFF20", border: `${colors.orange}30 1px solid` }}>
+            <Typography variant="h6" sx={{ fontFamily: fonts.nostromoBlack, color: colors.orange }}>
                 SELF REPAIR
             </Typography>
 
@@ -14,8 +14,8 @@ export const SelfRepairCard = () => {
             <FancyButton
                 clipThingsProps={{
                     clipSize: "5px",
-                    backgroundColor: colors.green,
-                    border: { isFancy: true, borderColor: colors.green },
+                    backgroundColor: colors.orange,
+                    border: { isFancy: true, borderColor: colors.orange },
                     sx: { position: "relative", width: "100%" },
                 }}
                 sx={{ px: "1.6rem", py: ".8rem", color: "#FFFFFF" }}
