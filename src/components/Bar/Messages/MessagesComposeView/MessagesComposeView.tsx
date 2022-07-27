d2c30a52fc0e1151119f861331204757d66d708c
felix t <41817193+o2bomb@users.noreply.github.com>
2022-07-27 13:42:45 +0800

    Display character limit

--- src/components/Bar/Messages/MessagesComposeView/MessagesComposeView.tsx
+++ src/components/Bar/Messages/MessagesComposeView/MessagesComposeView.tsx
@@ -1,7 +1,9 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, InputAdornment, Stack, TextField, Typography } from "@mui/material"
 import MDEditor, { commands } from "@uiw/react-md-editor"
 import { useEffect, useRef, useState } from "react"
+import { SvgSend } from "../../../../assets"
 import { useTheme } from "../../../../containers/theme"
+import { scaleUpKeyframes } from "../../../../theme/keyframes"
 import { colors, fonts } from "../../../../theme/theme"
 import { FancyButton } from "../../../Common/FancyButton"
 import MessageRenderer from "../MessageRenderer"
@@ -10,11 +12,17 @@ interface MessageComposeViewProps {
     onBack: () => void
 }
 
+const MESSAGE_LIMIT = 1000
+
 export const MessagesComposeView = ({ onBack }: MessageComposeViewProps) => {
     const theme = useTheme()
+
     const editorContainerEl = useRef<HTMLDivElement>()
-    const [message, setMessage] = useState<string>()
     const [height, setHeight] = useState<number>()
+
+    const [to, setTo] = useState("")
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
 
     useEffect(() => {
         if (!editorContainerEl.current) return
@@ -24,33 +32,103 @@ export const MessagesComposeView = ({ onBack }: MessageComposeViewProps) => {
     }, [])
 
     return (
-        <Stack p="2rem" height="100%">
-            <Stack direction="row">
-                <FancyButton
-                    clipThingsProps={{
-                        clipSize: "7px",
+        <Stack p="2rem" height="100%" spacing="1rem">
+            <Stack
+                direction="row"
+                sx={{
+                    pb: "1rem",
+                    borderBottom: `${theme.factionTheme.primary}70 1.5px solid`,
+                }}
+            >
+                <Typography variant="h6" sx={{ fontFamily: fonts.nostromoBlack }}>
+                    COMPOSE MESSAGE
+                </Typography>
+            </Stack>
+            <Stack spacing=".5rem">
+                <TextField
+                    variant="outlined"
+                    hiddenLabel
+                    fullWidth
+                    placeholder="Recipient ID"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <Typography
+                                    variant="h6"
+                                    sx={{
+                                        fontWeight: "fontWeightBold",
+                                    }}
+                                >
+                                    To
+                                </Typography>
+                            </InputAdornment>
+                        ),
                     }}
                     sx={{
-                        fontSize: "2rem",
-                        px: "1.2rem",
-                        backgroundColor: colors.grey,
+                        backgroundColor: "#00000090",
+                        ".MuiOutlinedInput-input": {
+                            px: "1.5rem",
+                            py: "1.5rem",
+                            fontSize: "2rem",
+                            height: "unset",
+                            "::-webkit-outer-spin-button, ::-webkit-inner-spin-button": {
+                                "-webkit-appearance": "none",
+                            },
+                        },
+                        ".MuiOutlinedInput-notchedOutline": { border: "unset" },
                     }}
-                    onClick={() => onBack()}
-                >
-                    Back
-                </FancyButton>
+                    type="text"
+                    value={to}
+                    onChange={(e) => {
+                        setTo(e.target.value)
+                    }}
+                />
+                <TextField
+                    variant="outlined"
+                    hiddenLabel
+                    fullWidth
+                    placeholder="Message subject"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <Typography
+                                    variant="h6"
+                                    sx={{
+                                        fontWeight: "fontWeightBold",
+                                    }}
+                                >
+                                    Subject
+                                </Typography>
+                            </InputAdornment>
+                        ),
+                    }}
+                    sx={{
+                        backgroundColor: "#00000090",
+                        ".MuiOutlinedInput-input": {
+                            px: "1.5rem",
+                            py: "1.5rem",
+                            fontSize: "2rem",
+                            height: "unset",
+                            "::-webkit-outer-spin-button, ::-webkit-inner-spin-button": {
+                                "-webkit-appearance": "none",
+                            },
+                        },
+                        ".MuiOutlinedInput-notchedOutline": { border: "unset" },
+                    }}
+                    type="text"
+                    value={subject}
+                    onChange={(e) => {
+                        setSubject(e.target.value)
+                    }}
+                />
             </Stack>
-            <Box>
-                <Typography>To</Typography>
-            </Box>
-            <Box>
-                <Typography>Subject</Typography>
-            </Box>
-
             <Box
                 ref={editorContainerEl}
                 flex={1}
                 sx={{
+                    "& .w-md-editor-toolbar": {
+                        borderRadius: 0,
+                    },
                     "& .w-md-editor-text": {
                         fontFamily: fonts.shareTechMono,
                         "& *": {
@@ -69,6 +147,7 @@ export const MessagesComposeView = ({ onBack }: MessageComposeViewProps) => {
                     previewOptions={MessageRenderer.generateOptions(theme.factionTheme.background)}
                     style={{
                         backgroundColor: theme.factionTheme.background,
+                        boxShadow: "none",
                     }}
                     height={height}
                     commands={[
@@ -95,9 +174,66 @@ export const MessagesComposeView = ({ onBack }: MessageComposeViewProps) => {
                     extraCommands={[commands.codeEdit, commands.codeLive, commands.codePreview]}
                     visibleDragbar={false}
                     value={message}
-                    onChange={setMessage}
+                    onChange={(value) => {
+                        setMessage((value || "").substring(0, MESSAGE_LIMIT))
+                    }}
                 />
             </Box>
+            <Stack direction="row" alignItems="start">
+                <FancyButton
+                    clipThingsProps={{
+                        clipSize: "7px",
+                    }}
+                    sx={{
+                        px: "1.2rem",
+                        fontSize: "1.6rem",
+                        backgroundColor: colors.grey,
+                        "&:hover": {
+                            backgroundColor: colors.red,
+                            color: "white",
+                        },
+                    }}
+                    onClick={() => onBack()}
+                >
+                    Discard / Back
+                </FancyButton>
+                <Box flex={1} />
+                <Stack direction="row">
+                    <Typography
+                        sx={{
+                            color: colors.grey,
+                        }}
+                    >
+                        <Box
+                            key={message ? message.length : 0}
+                            component="span"
+                            sx={{
+                                animate: `${scaleUpKeyframes} .2s ease-out`,
+                            }}
+                        >
+                            {message ? message.length : 0}
+                        </Box>{" "}
+                        / {MESSAGE_LIMIT}
+                    </Typography>
+                </Stack>
+                <FancyButton
+                    clipThingsProps={{
+                        clipSize: "7px",
+                        sx: {
+                            ml: "1rem",
+                        },
+                    }}
+                    sx={{
+                        px: "1.2rem",
+                        fontSize: "1.6rem",
+                        backgroundColor: colors.green,
+                    }}
+                    endIcon={<SvgSend />}
+                    disabled={!message}
+                >
+                    Submit Message
+                </FancyButton>
+            </Stack>
         </Stack>
     )
 }
