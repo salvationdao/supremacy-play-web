72ebf80ba8da0ac8100f2fa011586a95027021d9
Torrunt <me@torrunt.net>
2022-07-29 16:27:51 +0800

    Change battle zone transition delay

--- src/components/MiniMap/MapInsideItems/BattleZone.tsx
+++ src/components/MiniMap/MapInsideItems/BattleZone.tsx
@@ -1,5 +1,5 @@
 import { useGame } from "../../../containers"
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useMemo } from "react"
 import { Box } from "@mui/material"
 import { colors } from "../../../theme/theme"
 import { Map } from "../../../types"
@@ -13,40 +13,18 @@ const borderThickness = 3000
 export const BattleZone = ({ map }: BattleZoneProps) => {
     const { battleZone } = useGame()
 
-    // Update battle zone after warn time
-    const [currentBattleZone, setCurrentBattleZone] = useState(battleZone)
-    useEffect(() => {
-        const t = setTimeout(
-            () => {
-                setCurrentBattleZone(battleZone)
-            },
-            battleZone ? battleZone.warnTime * 1000 : 10000,
-        )
-        return () => clearTimeout(t)
-    }, [battleZone])
-
     const mapScale = useMemo(() => map.width / (map.cells_x * 2000), [map])
 
     const locationX = useMemo(
-        () => ((currentBattleZone?.location.x || map.left_pixels + map.width / 2) - map.left_pixels) * mapScale,
-        [map.left_pixels, map.width, mapScale, currentBattleZone?.location.x],
-    )
-    const locationY = useMemo(
-        () => ((currentBattleZone?.location.y || map.top_pixels + map.height / 2) - map.top_pixels) * mapScale,
-        [map.top_pixels, map.height, mapScale, currentBattleZone?.location.y],
-    )
-    const radius = useMemo(() => (currentBattleZone?.radius || 0) * mapScale, [mapScale, currentBattleZone?.radius])
-    const adjustedRadius = (radius + borderThickness) * 2
-
-    const targetLocationX = useMemo(
         () => ((battleZone?.location.x || map.left_pixels + map.width / 2) - map.left_pixels) * mapScale,
         [map.left_pixels, map.width, mapScale, battleZone?.location.x],
     )
-    const targetLocationY = useMemo(
+    const locationY = useMemo(
         () => ((battleZone?.location.y || map.top_pixels + map.height / 2) - map.top_pixels) * mapScale,
         [map.top_pixels, map.height, mapScale, battleZone?.location.y],
     )
-    const targetRadius = useMemo(() => (battleZone?.radius || 0) * mapScale, [mapScale, battleZone?.radius])
+    const radius = useMemo(() => (battleZone?.radius || 0) * mapScale, [mapScale, battleZone?.radius])
+    const adjustedRadius = (radius + borderThickness) * 2
 
     const battleZoneCircle = (overlay: boolean = false) => (
         <Box
@@ -57,10 +35,11 @@ export const BattleZone = ({ map }: BattleZoneProps) => {
                 width: adjustedRadius,
                 height: adjustedRadius,
                 transform: `translate(-50%, -50%) translate3d(${locationX}px, ${locationY}px, 0)`,
-                pointerEvents: "none",
-                transition: `all ${currentBattleZone?.shrinkTime || 0.5}s ease-in-out`,
                 borderRadius: "50%",
                 border: `${borderThickness}px solid #0F0202${overlay ? "66" : "BB"}`,
+                pointerEvents: "none",
+                transition: `all ${battleZone?.shrinkTime || 0.5}s ease-in-out`,
+                transitionDelay: `${battleZone?.warnTime || 0}s`,
             }}
         />
     )
@@ -77,9 +56,9 @@ export const BattleZone = ({ map }: BattleZoneProps) => {
                 sx={{
                     zIndex: 1,
                     position: "absolute",
-                    width: targetRadius * 2,
-                    height: targetRadius * 2,
-                    transform: `translate(-50%, -50%) translate3d(${targetLocationX}px, ${targetLocationY}px, 0)`,
+                    width: radius * 2,
+                    height: radius * 2,
+                    transform: `translate(-50%, -50%) translate3d(${locationX}px, ${locationY}px, 0)`,
                     borderRadius: "50%",
                     borderColor: `${colors.red}77`,
                     borderStyle: "solid",
