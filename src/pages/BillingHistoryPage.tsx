ac30fc32b2956e5731396c19974a82550ab7a2eb
Yong Tan <ultimatoonmaster@gmail.com>
2022-08-01 14:46:21 +0800

    Saving current work on billing history page

--- src/pages/BillingHistoryPage.tsx
+++ src/pages/BillingHistoryPage.tsx
@@ -1,23 +1,278 @@
-import { useEffect } from "react"
+import { useState, useEffect, useCallback, useMemo } from "react"
+import { useUrlQuery, usePagination } from "../hooks"
 import { useGameServerCommandsUser } from "../hooks/useGameServer"
+import { parseString } from "../helpers"
 import { GameServerKeys } from "../keys"
+import { BillingHistory } from "../types/fiat"
+import { Box, CircularProgress, Pagination, Stack, Typography } from "@mui/material"
+import { useTheme } from "../containers/theme"
+import { ClipThing, FancyButton } from "../components"
+import { PageHeader } from "../components/Common/PageHeader"
+import { EmptyWarMachinesPNG, WarMachineIconPNG } from "../assets"
+import { colors, fonts } from "../theme/theme"
 
 export const BillingHistoryPage = () => {
+    const theme = useTheme()
+    const [query, updateQuery] = useUrlQuery()
     const { send } = useGameServerCommandsUser("/user_commander")
 
-    useEffect(() => {
-        ;(async () => {
-            try {
-                const resp = await send(GameServerKeys.BillingHistoryList, {
-                    page: 0,
-                    page_size: 10,
-                })
-                console.log("Hmm", resp)
-            } catch (err) {
-                console.error(err)
-            }
-        })()
-    }, [send])
+    const { page, changePage, totalItems, setTotalItems, totalPages, pageSize, changePageSize } = usePagination({
+        pageSize: parseString(query.get("pageSize"), 10),
+        page: parseString(query.get("page"), 1),
+    })
 
-    return <div>Test Cakes</div>
+    // Items
+    const [isLoading, setIsLoading] = useState(true)
+    const [loadError, setLoadError] = useState<string>()
+    const [billingHistoryItems, setBillingHistoryItems] = useState<BillingHistory[]>()
+
+    const getItems = useCallback(async () => {
+        try {
+            setIsLoading(true)
+            const resp = await send<{ total: number; records: BillingHistory[] }>(GameServerKeys.BillingHistoryList, {
+                page: page - 1,
+                page_size: pageSize,
+            })
+            console.log("Hmm", resp)
+
+            updateQuery({
+                page: page.toString(),
+                pageSize: pageSize.toString(),
+            })
+
+            if (!resp) return
+
+            setTotalItems(resp.total)
+            setBillingHistoryItems(resp.records)
+            setLoadError(undefined)
+        } catch (err) {
+            const message = typeof err === "string" ? err : "Failed to get billing history listings."
+            setLoadError(message)
+            console.error(err)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [page, pageSize, send, updateQuery, setTotalItems])
+
+    useEffect(() => {
+        getItems()
+    }, [getItems])
+
+    const content = useMemo(() => {
+        if (loadError) {
+            return (
+                <Stack alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
+                    <Stack
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ height: "100%", maxWidth: "100%", width: "75rem", px: "3rem", pt: "1.28rem" }}
+                        spacing="1.5rem"
+                    >
+                        <Typography
+                            sx={{
+                                color: colors.red,
+                                fontFamily: fonts.nostromoBold,
+                                textAlign: "center",
+                            }}
+                        >
+                            {loadError}
+                        </Typography>
+                    </Stack>
+                </Stack>
+            )
+        }
+
+        if (!billingHistoryItems || isLoading) {
+            return (
+                <Stack alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
+                    <Stack alignItems="center" justifyContent="center" sx={{ height: "100%", px: "3rem", pt: "1.28rem" }}>
+                        <CircularProgress size="3rem" sx={{ color: theme.factionTheme.primary }} />
+                    </Stack>
+                </Stack>
+            )
+        }
+
+        if (billingHistoryItems && billingHistoryItems.length > 0) {
+            return (
+                <Box sx={{ direction: "ltr", height: 0 }}>
+                    <Box
+                        sx={{
+                            width: "100%",
+                            py: "1rem",
+                            display: "grid",
+                            gridTemplateColumns: "100%",
+                            gap: "1.3rem",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            overflow: "visible",
+                        }}
+                    >
+                        {billingHistoryItems.map((item) => (
+                            <BillingHistoryItem key={`billing-history-${item.id}`} item={item} />
+                        ))}
+                    </Box>
+                </Box>
+            )
+        }
+
+        return (
+            <Stack alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
+                <Stack alignItems="center" justifyContent="center" sx={{ height: "100%", maxWidth: "40rem" }}>
+                    <Box
+                        sx={{
+                            width: "80%",
+                            height: "16rem",
+                            opacity: 0.7,
+                            filter: "grayscale(100%)",
+                            background: `url(${EmptyWarMachinesPNG})`,
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "bottom center",
+                            backgroundSize: "contain",
+                        }}
+                    />
+                    <Typography
+                        sx={{
+                            px: "1.28rem",
+                            pt: "1.28rem",
+                            color: colors.grey,
+                            fontFamily: fonts.nostromoBold,
+                            userSelect: "text !important",
+                            opacity: 0.9,
+                            textAlign: "center",
+                        }}
+                    >
+                        {"There are no past purchases found."}
+                    </Typography>
+                </Stack>
+            </Stack>
+        )
+    }, [loadError, billingHistoryItems, isLoading])
+
+    return (
+        <Stack direction="row" spacing="1rem" sx={{ height: "100%" }}>
+            <ClipThing
+                clipSize="10px"
+                border={{
+                    borderColor: theme.factionTheme.primary,
+                    borderThickness: ".3rem",
+                }}
+                opacity={0.7}
+                backgroundColor={theme.factionTheme.background}
+                sx={{ height: "100%", flex: 1 }}
+            >
+                <Stack sx={{ position: "relative", height: "100%" }}>
+                    <Stack sx={{ flex: 1 }}>
+                        <PageHeader title="BILLING HISTORY" description="View your past payments." imageUrl={WarMachineIconPNG} />
+
+                        <Stack sx={{ px: "1rem", py: "1rem", flex: 1 }}>
+                            <Box
+                                sx={{
+                                    ml: "1.9rem",
+                                    mr: ".5rem",
+                                    pr: "1.4rem",
+                                    my: "1rem",
+                                    flex: 1,
+                                    overflowY: "auto",
+                                    overflowX: "hidden",
+                                    direction: "ltr",
+
+                                    "::-webkit-scrollbar": {
+                                        width: ".4rem",
+                                    },
+                                    "::-webkit-scrollbar-track": {
+                                        background: "#FFFFFF15",
+                                        borderRadius: 3,
+                                    },
+                                    "::-webkit-scrollbar-thumb": {
+                                        background: theme.factionTheme.primary,
+                                        borderRadius: 3,
+                                    },
+                                }}
+                            >
+                                {content}
+                            </Box>
+                        </Stack>
+                    </Stack>
+
+                    {totalPages > 1 && (
+                        <Box
+                            sx={{
+                                px: "1rem",
+                                py: ".7rem",
+                                borderTop: (theme) => `${theme.factionTheme.primary}70 1.5px solid`,
+                                backgroundColor: "#00000070",
+                            }}
+                        >
+                            <Pagination
+                                size="medium"
+                                count={totalPages}
+                                page={page}
+                                sx={{
+                                    ".MuiButtonBase-root": { borderRadius: 0.8, fontFamily: fonts.nostromoBold },
+                                    ".Mui-selected": {
+                                        color: (theme) => theme.factionTheme.secondary,
+                                        backgroundColor: `${theme.factionTheme.primary} !important`,
+                                    },
+                                }}
+                                onChange={(e, p) => changePage(p)}
+                                showFirstButton
+                                showLastButton
+                            />
+                        </Box>
+                    )}
+                </Stack>
+            </ClipThing>
+        </Stack>
+    )
+}
+
+interface BillingHistoryItemProps {
+    item: BillingHistory
+}
+
+export const BillingHistoryItem = ({ item }: BillingHistoryItemProps) => {
+    const theme = useTheme()
+
+    const primaryColor = theme.factionTheme.primary
+    const backgroundColor = theme.factionTheme.background
+
+    return (
+        <Box sx={{ position: "relative", overflow: "visible", height: "100%" }}>
+            <FancyButton
+                disableRipple
+                clipThingsProps={{
+                    clipSize: "7px",
+                    clipSlantSize: "0px",
+                    corners: {
+                        topLeft: true,
+                        topRight: true,
+                        bottomLeft: true,
+                        bottomRight: true,
+                    },
+                    backgroundColor: backgroundColor,
+                    opacity: 0.9,
+                    border: { isFancy: true, borderColor: primaryColor, borderThickness: ".25rem" },
+                    sx: { position: "relative", height: "100%" },
+                }}
+                sx={{ color: primaryColor, textAlign: "start", height: "100%", ":hover": { opacity: 1 } }}
+                to={`/billing-history`}
+            >
+                <Box
+                    sx={{
+                        position: "relative",
+                        height: "100%",
+                        p: ".1rem .3rem",
+                        display: "grid",
+                        gridTemplateRows: "7rem",
+                        gridTemplateColumns: `minmax(36rem, auto) 23rem repeat(3, 17rem)`, // hard-coded to have 6 columns, adjust as required
+                        gap: "1.4rem",
+                    }}
+                >
+                    <Stack direction="column" alignItems="center" spacing="1.4rem" sx={{ position: "relative" }}>
+                        Test Cakes
+                    </Stack>
+                </Box>
+            </FancyButton>
+        </Box>
+    )
 }
