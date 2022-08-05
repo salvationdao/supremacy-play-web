df9d3978cade4be8036362931c1273a74c01cf30
James Li <jayli3n@gmail.com>
2022-08-05 17:41:31 +0800

    Colow

--- src/components/MiniMap/MapInsideItems/MapGrid.tsx
+++ src/components/MiniMap/MapInsideItems/MapGrid.tsx
@@ -2,6 +2,7 @@ import { Box, Stack, Typography } from "@mui/material"
 import React, { useMemo } from "react"
 import { Crosshair } from "../../../assets"
 import { intToLetter } from "../../../helpers"
+import { colors } from "../../../theme/theme"
 import { LineSelect } from "./LineSelect"
 
 export const MapGrid = React.memo(function MapGrid({
@@ -31,10 +32,11 @@ export const MapGrid = React.memo(function MapGrid({
                 {new Array(Math.floor(mapHeight / gridHeight / 3)).fill(0).map((_, i) => (
                     <Stack key={i} direction="row" sx={{ position: "relative", flex: 1 }}>
                         {new Array(Math.floor(mapWidth / gridWidth / 3)).fill(0).map((_, j) => (
-                            <Box key={j} sx={{ flex: 1, position: "relative", border: "#FFFFFF30 1px solid", ":hover p": { opacity: 1 } }}>
+                            <Box key={j} sx={{ flex: 1, position: "relative", border: `${colors.offWhite}30 1px solid`, ":hover p": { opacity: 1 } }}>
                                 <Typography
                                     variant="caption"
                                     sx={{
+                                        color: colors.offWhite,
                                         position: "absolute",
                                         fontSize: gridHeight / 1.2,
                                         top: 0,
