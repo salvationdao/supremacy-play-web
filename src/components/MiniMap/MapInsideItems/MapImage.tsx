14b6721984ad2d2938a6d1b76c8c93e6407a146c
James Li <jayli3n@gmail.com>
2022-08-05 15:31:38 +0800

    Map grid

--- src/components/MiniMap/MapInsideItems/MapImage.tsx
+++ src/components/MiniMap/MapInsideItems/MapImage.tsx
@@ -2,7 +2,6 @@ import { Box, Stack, Typography } from "@mui/material"
 import React from "react"
 import { Crosshair } from "../../../assets"
 import { intToLetter } from "../../../helpers"
-import { fonts } from "../../../theme/theme"
 import { Map } from "../../../types"
 import { LineSelect } from "./LineSelect"
 
@@ -24,71 +23,53 @@ export const MapImage = ({
     gridHeight: number
 }) => {
     return (
-        <>
-            <Box
-                ref={mapElement}
-                onClick={onClick}
-                sx={{
-                    position: "absolute",
-                    width: `${map.width}px`,
-                    height: `${map.height}px`,
-                    backgroundImage: `url(${map.image_url})`,
-                    cursor: isLocationSelection || isLineSelection ? `url(${Crosshair}) 14.5 14.5, auto` : "move",
-                    borderSpacing: 0,
-                }}
-            >
-                {isLineSelection && <LineSelect mapScale={mapScale} />}
-            </Box>
-
-            <MapGrid mapWidth={map.width} mapHeight={map.height} gridHeight={gridHeight} />
-        </>
+        <Box
+            ref={mapElement}
+            onClick={onClick}
+            sx={{
+                position: "absolute",
+                width: `${map.width}px`,
+                height: `${map.height}px`,
+                backgroundImage: `url(${map.image_url})`,
+                cursor: isLocationSelection || isLineSelection ? `url(${Crosshair}) 14.5 14.5, auto` : "move",
+                borderSpacing: 0,
+            }}
+        >
+            {isLineSelection && <LineSelect mapScale={mapScale} />}
+            <MapGrid gridHeight={gridHeight} />
+        </Box>
     )
 }
 
-const MapGrid = React.memo(function MapGrid({ mapWidth, mapHeight, gridHeight }: { mapWidth: number; mapHeight: number; gridHeight: number }) {
+const MapGrid = React.memo(function MapGrid({ gridHeight }: { gridHeight: number }) {
+    return null
     return (
         <Stack
             sx={{
-                position: "absolute",
-                width: `${mapWidth}px`,
-                height: `${mapHeight}px`,
-                pointerEvents: "none",
+                position: "relative",
+                width: `100%`,
+                height: `100%`,
             }}
         >
             {new Array(10).fill(0).map((_, i) => (
                 <Stack key={i} direction="row" sx={{ position: "relative", flex: 1 }}>
-                    <Typography
-                        variant="caption"
-                        sx={{
-                            position: "absolute",
-                            fontSize: gridHeight / 1.8,
-                            fontFamily: fonts.nostromoBlack,
-                            left: 0,
-                            top: "50%",
-                            transform: "translateY(-50%)",
-                        }}
-                    >
-                        {i + 1}
-                    </Typography>
-
                     {new Array(10).fill(0).map((_, j) => (
-                        <div key={j} style={{ flex: 1, position: "relative", border: "#FFFFFF40 1px solid" }}>
-                            {i === 0 && (
-                                <Typography
-                                    variant="caption"
-                                    sx={{
-                                        position: "absolute",
-                                        fontSize: gridHeight / 1.8,
-                                        fontFamily: fonts.nostromoBlack,
-                                        top: 0,
-                                        left: "50%",
-                                        transform: "translateX(-50%)",
-                                    }}
-                                >
-                                    {intToLetter(j)}
-                                </Typography>
-                            )}
-                        </div>
+                        <Box key={j} sx={{ flex: 1, position: "relative", border: "#FFFFFF 1px solid", opacity: 0.12, ":hover": { opacity: 1 } }}>
+                            <Typography
+                                variant="caption"
+                                sx={{
+                                    position: "absolute",
+                                    fontSize: gridHeight / 1.6,
+                                    top: 0,
+                                    left: "10%",
+                                    textTransform: "uppercase",
+                                    fontWeight: "fontWeightBold",
+                                }}
+                            >
+                                {intToLetter(j)}
+                                {i + 1}
+                            </Typography>
+                        </Box>
                     ))}
                 </Stack>
             ))}
