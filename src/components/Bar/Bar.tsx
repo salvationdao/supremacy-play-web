2d97fc2b3a11b37a72712ba0cf511b64467b33cf
James Li <jayli3n@gmail.com>
2022-08-05 12:38:35 +0800

    Add testing env border and banner

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,9 +1,9 @@
 import { Box, Stack, Typography } from "@mui/material"
 import { Logo, ProfileCard, WalletDetails } from ".."
 import { SvgDisconnected } from "../../assets"
-import { DRAWER_TRANSITION_DURATION, GAME_BAR_HEIGHT } from "../../constants"
+import { DRAWER_TRANSITION_DURATION, GAME_BAR_HEIGHT, STAGING_OR_DEV_ONLY } from "../../constants"
 import { useAuth, useSupremacy } from "../../containers"
-import { fonts, siteZIndex } from "../../theme/theme"
+import { colors, fonts, siteZIndex } from "../../theme/theme"
 import { User } from "../../types"
 import { Messages } from "./Messages/Messages"
 import { NavLinks } from "./NavLinks/NavLinks"
@@ -12,36 +12,68 @@ export const Bar = () => {
     const { userID, user } = useAuth()
 
     return (
-        <Stack
-            direction="row"
-            alignItems="center"
-            sx={{
-                position: "relative",
-                pl: ".8rem",
-                pr: "1.6rem",
-                flexShrink: 0,
-                height: `${GAME_BAR_HEIGHT}rem`,
-                color: "#FFFFFF",
-                background: (theme) => `linear-gradient(#FFFFFF10 26%, ${theme.factionTheme.background})`,
+        <>
+            {STAGING_OR_DEV_ONLY && (
+                <>
+                    <Box
+                        sx={{
+                            position: "relative",
+                            flexShrink: 0,
+                            p: ".6rem",
+                            width: "100vw",
+                            backgroundColor: colors.lightRed,
+                            zIndex: siteZIndex.Popover,
+                        }}
+                    >
+                        <Typography variant="body2" sx={{ fontFamily: fonts.nostromoBlack, lineHeight: 1, textAlign: "center" }}>
+                            THIS IS A TESTING ENVIRONMENT
+                        </Typography>
+                    </Box>
 
-                zIndex: siteZIndex.Popover,
-                "::-webkit-scrollbar": {
-                    height: ".3rem",
-                },
-                "::-webkit-scrollbar-track": {
-                    background: "#FFFFFF15",
-                    borderRadius: 3,
-                },
-                "::-webkit-scrollbar-thumb": {
-                    background: "#FFFFFF50",
-                    borderRadius: 3,
-                },
-                width: "100vw",
-                transition: `all ${DRAWER_TRANSITION_DURATION / 1000}s`,
-            }}
-        >
-            <BarContent userID={userID} user={user} />
-        </Stack>
+                    <Box
+                        sx={{
+                            position: "fixed",
+                            height: "100%",
+                            width: "100%",
+                            border: STAGING_OR_DEV_ONLY ? `${colors.lightRed} 3px solid` : "unset",
+                            zIndex: siteZIndex.Modal * 99,
+                            pointerEvents: "none",
+                        }}
+                    />
+                </>
+            )}
+
+            <Stack
+                direction="row"
+                alignItems="center"
+                sx={{
+                    position: "relative",
+                    pl: ".8rem",
+                    pr: "1.6rem",
+                    flexShrink: 0,
+                    height: `${GAME_BAR_HEIGHT}rem`,
+                    width: "100vw",
+                    color: "#FFFFFF",
+                    background: (theme) => `linear-gradient(#FFFFFF10 26%, ${theme.factionTheme.background})`,
+                    transition: `all ${DRAWER_TRANSITION_DURATION / 1000}s`,
+
+                    zIndex: siteZIndex.Popover,
+                    "::-webkit-scrollbar": {
+                        height: ".3rem",
+                    },
+                    "::-webkit-scrollbar-track": {
+                        background: "#FFFFFF15",
+                        borderRadius: 3,
+                    },
+                    "::-webkit-scrollbar-thumb": {
+                        background: "#FFFFFF50",
+                        borderRadius: 3,
+                    },
+                }}
+            >
+                <BarContent userID={userID} user={user} />
+            </Stack>
+        </>
     )
 }
 
--- src/components/Bar/WalletDetails/BuySupsButton.tsx
+++ src/components/Bar/WalletDetails/BuySupsButton.tsx
@@ -1,34 +1,9 @@
 import { Button } from "@mui/material"
-import { useCallback, useMemo, useState } from "react"
-import { TooltipHelper } from "../.."
+import { useCallback } from "react"
 import { STAGING_OR_DEV_ONLY, TOKEN_SALE_PAGE } from "../../../constants"
-import { dateFormatter } from "../../../helpers"
 import { colors, fonts } from "../../../theme/theme"
 
 export const BuySupsButton = () => {
-    // const { send } = usePassportCommandsUser("xxxxxxxxx")
-    const [timeTilNextClaim] = useState<Date>()
-
-    const getFreeSups = useCallback(async () => {
-        // try {
-        //     const resp = await send<Date | boolean>(PassportServerKeys.GetFreeSups)
-        //     if (resp instanceof Date) setTimeTilNextClaim(resp)
-        // } catch (e) {
-        //     console.error(e)
-        // }
-    }, [])
-
-    const tooltipText = useMemo(() => {
-        if (STAGING_OR_DEV_ONLY) {
-            if (timeTilNextClaim && timeTilNextClaim < new Date()) {
-                return `Time until next claim: ${dateFormatter(timeTilNextClaim)}`
-            }
-
-            return "Claim free SUPs!"
-        }
-        return ""
-    }, [timeTilNextClaim])
-
     const openBuySupsPage = useCallback(() => {
         const width = 520
         const height = 690
@@ -37,28 +12,27 @@ export const BuySupsButton = () => {
         window.open(TOKEN_SALE_PAGE, "SUPS Token Sale", `width=${width},height=${height},left=${left},top=${top},popup=1`)
     }, [])
 
+    if (STAGING_OR_DEV_ONLY) return null
+
     return (
-        <TooltipHelper placement="bottom" text={tooltipText}>
-            <Button
-                id="tutorial-purchase"
-                sx={{
-                    px: "1.2rem",
-                    pt: ".32rem",
-                    pb: ".16rem",
-                    flexShrink: 0,
-                    justifyContent: "flex-start",
-                    color: STAGING_OR_DEV_ONLY ? colors.gold : colors.neonBlue,
-                    whiteSpace: "nowrap",
-                    borderRadius: 0.2,
-                    border: `1px solid ${STAGING_OR_DEV_ONLY ? colors.gold : colors.neonBlue}`,
-                    overflow: "hidden",
-                    fontFamily: fonts.nostromoBold,
-                }}
-                onClick={STAGING_OR_DEV_ONLY ? getFreeSups : openBuySupsPage}
-                disabled={STAGING_OR_DEV_ONLY && timeTilNextClaim && timeTilNextClaim < new Date()}
-            >
-                {STAGING_OR_DEV_ONLY ? "GET FREE SUPS" : "GET SUPS"}
-            </Button>
-        </TooltipHelper>
+        <Button
+            id="tutorial-purchase"
+            sx={{
+                px: "1.2rem",
+                pt: ".32rem",
+                pb: ".16rem",
+                flexShrink: 0,
+                justifyContent: "flex-start",
+                color: colors.neonBlue,
+                whiteSpace: "nowrap",
+                borderRadius: 0.2,
+                border: `1px solid ${colors.neonBlue}`,
+                overflow: "hidden",
+                fontFamily: fonts.nostromoBold,
+            }}
+            onClick={openBuySupsPage}
+        >
+            GET SUPS
+        </Button>
     )
 }
