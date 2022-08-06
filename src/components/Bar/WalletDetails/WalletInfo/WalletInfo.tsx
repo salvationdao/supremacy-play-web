fb7333d02486fcb9530c2148ab1801380d6a85e7
James Li <jayli3n@gmail.com>
2022-08-06 13:23:12 +0800

    Done

--- src/components/Bar/WalletDetails/WalletInfo/WalletInfo.tsx
+++ src/components/Bar/WalletDetails/WalletInfo/WalletInfo.tsx
@@ -2,6 +2,7 @@ import { CircularProgress, IconButton, Stack, Typography } from "@mui/material"
 import BigNumber from "bignumber.js"
 import { MutableRefObject, useRef, useState } from "react"
 import { SvgHide, SvgSupToken, SvgUnhide } from "../../../../assets"
+import { STAGING_OR_DEV_ONLY } from "../../../../constants"
 import { useAuth, useWallet } from "../../../../containers"
 import { supFormatterNoFixed } from "../../../../helpers"
 import { useToggle } from "../../../../hooks"
@@ -117,7 +118,7 @@ const WalletInfoInner = ({
                     },
                 }}
             >
-                <SvgSupToken size="1.9rem" fill={colors.red} sx={{ mr: ".2rem", pb: ".4rem" }} />
+                <SvgSupToken size="1.9rem" fill={STAGING_OR_DEV_ONLY ? colors.red : colors.yellow} sx={{ mr: ".2rem", pb: ".4rem" }} />
                 <Typography sx={{ fontFamily: fonts.nostromoBold, lineHeight: 1, whiteSpace: "nowrap" }}>
                     {!isHideValue && <>{onWorldSupsRaw ? supFormatterNoFixed(onWorldSupsRaw, 2) : "0.00"}</>}
                     {isHideValue && "---"}
