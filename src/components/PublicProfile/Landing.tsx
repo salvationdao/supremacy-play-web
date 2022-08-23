182bc3f40a73ecbce6b1c1e1428000f2ca29f055
owengiri20 <owengiri20@gmail.com>
2022-08-23 20:18:10 +0800

    stylez

--- src/components/PublicProfile/Landing.tsx
+++ src/components/PublicProfile/Landing.tsx
@@ -1,5 +1,10 @@
 import { Box, Typography } from "@mui/material"
 
+import RmBorder from "../../assets/landing/RmBorder.svg"
+import RmBottom from "../../assets/landing/RmBottom.svg"
+import RmBorderBottom from "../../assets/landing/RmBorderBottom.svg"
+import RmBack from "../../assets/landing/RmBack.png"
+
 export const Landing = () => {
     return (
         <Box sx={{ height: "100%", width: "100%" }}>
@@ -16,12 +21,12 @@ export const Landing = () => {
                     }}
                 >
                     {/* left */}
-                    <Box sx={{ top: "10rem", position: "absolute", left: 0 }}>
+                    <Box sx={{ top: "15rem", position: "absolute", left: 0 }}>
                         <BoxThing />
                     </Box>
 
                     {/* right */}
-                    <Box sx={{ top: "10rem", position: "absolute", right: 0 }}>
+                    <Box sx={{ top: "15rem", position: "absolute", right: 0 }}>
                         <BoxThing />
                     </Box>
 
@@ -30,7 +35,7 @@ export const Landing = () => {
                         <BoxThing />
                     </Box>
 
-                    <Box sx={{ justifySelf: "flex-end" }}>
+                    <Box sx={{ position: "absolute", bottom: 0 }}>
                         <Typography variant="h4">Next Battle</Typography>
                         <Typography variant="h4">Battle name</Typography>
                     </Box>
@@ -41,48 +46,54 @@ export const Landing = () => {
 }
 
 const BoxThing = () => {
+    return (
+        <Box position="relative" width="50rem" height="45rem" margin="1rem">
+            <Box sx={{ position: "absolute", top: "35%", left: 0, margin: "1rem" }}>
+                <Card />
+            </Box>
+
+            <Box sx={{ position: "absolute", top: "35%", right: 0, margin: "1rem" }}>
+                <Card />
+            </Box>
+
+            <Box sx={{ position: "absolute", top: 0, left: "34%", margin: "1rem" }}>
+                <Card />
+            </Box>
+
+            <Box sx={{ position: "absolute", bottom: 0, left: "34%", margin: "1rem" }}>
+                <Card />
+            </Box>
+        </Box>
+    )
+}
+
+const Card = () => {
     const h = "18rem"
     const w = "14rem"
     return (
-        <Box position="relative" border="1px solid white" width="50rem" height="45rem" margin="1rem">
-            <Box position="absolute" height={h} width={w} sx={{ top: "35%", left: 0, margin: "1rem", background: "blue" }}>
-                <img
-                    height={"80%"}
-                    width={"100%"}
-                    src="https://afiles.ninja-cdn.com/passport/genesis/avatar/boston-cybernetics_law-enforcer-x-1000_biohazard_avatar.png"
-                    alt=""
-                />
-                <Typography variant="h4">left middle</Typography>
-            </Box>
-            <Box position="absolute" height={h} width={w} sx={{ top: "35%", right: 0, margin: "1rem", background: "blue" }}>
-                <img
-                    height={"80%"}
-                    width={"100%"}
-                    src="https://afiles.ninja-cdn.com/passport/genesis/avatar/boston-cybernetics_law-enforcer-x-1000_crystal-blue_avatar.png"
-                    alt=""
-                />
-                <Typography variant="h4"> right middle</Typography>
-            </Box>
+        <Box position="relative" height={h} width={w}>
+            <img style={{ position: "absolute", top: "-6px", zIndex: 4 }} width={"100%"} src={RmBorder} alt="" />
+            <div
+                style={{
+                    position: "absolute",
+                    top: "-6px",
+                    left: "5px",
+                    backgroundImage: `url(${"https://afiles.ninja-cdn.com/passport/genesis/avatar/boston-cybernetics_law-enforcer-x-1000_crystal-blue_avatar.png"})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "contain",
+                    height: "80%",
+                    width: "92%",
+                    zIndex: 3,
+                    clipPath: "polygon(10% 0%, 90% 0%, 100% 10%, 100% 90%, 90% 100%, 10% 100%, 0% 90%, 0% 10%)",
+                }}
+            />
 
-            <Box position="absolute" height={h} width={w} sx={{ top: 0, left: "34%", margin: "1rem", background: "red" }}>
-                <img
-                    height={"80%"}
-                    width={"100%"}
-                    src="https://afiles.ninja-cdn.com/passport/genesis/avatar/red-mountain_olympus-mons-ly07_evo_avatar.png"
-                    alt=""
-                />
-                <Typography variant="h4">top</Typography>
-            </Box>
-
-            <Box position="absolute" height={h} width={w} sx={{ bottom: 0, left: "34%", margin: "1rem", background: "red" }}>
-                <img
-                    height={"80%"}
-                    width={"100%"}
-                    src="https://afiles.ninja-cdn.com/passport/genesis/avatar/red-mountain_olympus-mons-ly07_red-blue_avatar.png"
-                    alt=""
-                />
-                <Typography variant="h4">bottom</Typography>
-            </Box>
+            <img style={{ bottom: 0, zIndex: 3, left: "1.3px", position: "absolute" }} width={"97%"} src={RmBorderBottom} alt="" />
+            <img style={{ bottom: 0, zIndex: 2, left: "1.3px", position: "absolute" }} width={"97%"} src={RmBottom} alt="" />
+            <Typography sx={{ position: "absolute", bottom: ".7rem", zIndex: 3, left: "19%" }} variant="caption">
+                Mech name here
+            </Typography>
         </Box>
     )
 }
