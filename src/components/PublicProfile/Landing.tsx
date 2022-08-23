e18682fdcbc03b2727f1cdd3489b398372e532af
owengiri20 <owengiri20@gmail.com>
2022-08-23 21:18:44 +0800

    faction colors

--- src/components/PublicProfile/Landing.tsx
+++ src/components/PublicProfile/Landing.tsx
@@ -1,13 +1,33 @@
 import { Box, Typography } from "@mui/material"
 
-import RmBorder from "../../assets/landing/RmBorder.svg"
-import RmBottom from "../../assets/landing/RmBottom.svg"
-import RmBorderBottom from "../../assets/landing/RmBorderBottom.svg"
-import RmBack from "../../assets/landing/RmBack.png"
+// rm
+import RmBorder from "../../assets/landing/rm/RmBorder.svg"
+import RmBottom from "../../assets/landing/rm/RmBottom.svg"
+import RmBorderBottom from "../../assets/landing/rm/RmBorderBottom.svg"
+
+// zhi
+import ZhiBorder from "../../assets/landing/zhi/ZHIBorder.svg"
+import ZhiBottom from "../../assets/landing/zhi/ZHIBottom.svg"
+import ZhiBorderBottom from "../../assets/landing/zhi/ZHIBorderBottom.svg"
+
+// bc
+import BcBorder from "../../assets/landing/bc/BCBorder.svg"
+import BcBottom from "../../assets/landing/bc/BCBottom.svg"
+import BcBorderBottom from "../../assets/landing/bc/BCBorderBottom.svg"
 
 export const Landing = () => {
     return (
-        <Box sx={{ height: "100%", width: "100%" }}>
+        <Box
+            sx={{
+                height: "100%",
+                width: "100%",
+
+                backgroundImage: `url(${"https://afiles.ninja-cdn.com/supremacy-stream-site/assets/img/factions/zai-wall.png"})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "contain",
+            }}
+        >
             <Box sx={{ display: "flex", width: "100%", height: "100%", justifyContent: "center" }}>
                 <Box
                     sx={{
@@ -17,22 +37,21 @@ export const Landing = () => {
                         height: "80%",
                         alignItems: "center",
                         flexDirection: "column",
-                        border: "2px solid orange",
                     }}
                 >
                     {/* left */}
                     <Box sx={{ top: "15rem", position: "absolute", left: 0 }}>
-                        <BoxThing />
+                        <BoxThing faction="bc" />
                     </Box>
 
                     {/* right */}
                     <Box sx={{ top: "15rem", position: "absolute", right: 0 }}>
-                        <BoxThing />
+                        <BoxThing faction="zhi" />
                     </Box>
 
                     {/* center */}
                     <Box>
-                        <BoxThing />
+                        <BoxThing faction="rm" />
                     </Box>
 
                     <Box sx={{ position: "absolute", bottom: 0 }}>
@@ -45,34 +64,69 @@ export const Landing = () => {
     )
 }
 
-const BoxThing = () => {
+const BoxThing = ({ faction }: { faction: string }) => {
     return (
         <Box position="relative" width="50rem" height="45rem" margin="1rem">
             <Box sx={{ position: "absolute", top: "35%", left: 0, margin: "1rem" }}>
-                <Card />
+                <Card faction={faction} />
             </Box>
 
             <Box sx={{ position: "absolute", top: "35%", right: 0, margin: "1rem" }}>
-                <Card />
+                <Card faction={faction} />
             </Box>
 
             <Box sx={{ position: "absolute", top: 0, left: "34%", margin: "1rem" }}>
-                <Card />
+                <Card faction={faction} />
             </Box>
 
             <Box sx={{ position: "absolute", bottom: 0, left: "34%", margin: "1rem" }}>
-                <Card />
+                <Card faction={faction} />
             </Box>
         </Box>
     )
 }
 
-const Card = () => {
+const getCardInfo = (faction: string) => {
+    if (faction === "bc") {
+        return {
+            border: BcBorder,
+            borderBottom: BcBorderBottom,
+            bottom: BcBottom,
+        }
+    }
+
+    if (faction === "zhi") {
+        return {
+            border: ZhiBorder,
+            borderBottom: ZhiBorderBottom,
+            bottom: ZhiBottom,
+        }
+    }
+
+    if (faction === "rm") {
+        return {
+            border: RmBorder,
+            borderBottom: RmBorderBottom,
+            bottom: RmBottom,
+        }
+    }
+
+    return {
+        border: RmBorder,
+        borderBottom: RmBorderBottom,
+        bottom: RmBottom,
+    }
+}
+const Card = ({ faction }: { faction: string }) => {
+    const border = getCardInfo(faction)?.border || ""
+    const borderBottom = getCardInfo(faction)?.borderBottom || ""
+    const bottom = getCardInfo(faction)?.bottom || ""
+
     const h = "18rem"
     const w = "14rem"
     return (
         <Box position="relative" height={h} width={w}>
-            <img style={{ position: "absolute", top: "-6px", zIndex: 4 }} width={"100%"} src={RmBorder} alt="" />
+            <img style={{ position: "absolute", top: "-6px", zIndex: 4 }} width={"100%"} src={border} alt="" />
             <div
                 style={{
                     position: "absolute",
@@ -89,8 +143,8 @@ const Card = () => {
                 }}
             />
 
-            <img style={{ bottom: 0, zIndex: 3, left: "1.3px", position: "absolute" }} width={"97%"} src={RmBorderBottom} alt="" />
-            <img style={{ bottom: 0, zIndex: 2, left: "1.3px", position: "absolute" }} width={"97%"} src={RmBottom} alt="" />
+            <img style={{ bottom: 0, zIndex: 3, left: "1.3px", position: "absolute" }} width={"97%"} src={borderBottom} alt="" />
+            <img style={{ bottom: 0, zIndex: 2, left: "1.3px", position: "absolute" }} width={"97%"} src={bottom} alt="" />
             <Typography sx={{ position: "absolute", bottom: ".7rem", zIndex: 3, left: "19%" }} variant="caption">
                 Mech name here
             </Typography>
