24cea5df1cc7d53578465a2dd55a5f6953589910
James Li <jayli3n@gmail.com>
2022-08-24 14:04:56 +0800

    Fix trailer render

--- src/components/BigDisplay/Stream/Trailer.tsx
+++ src/components/BigDisplay/Stream/Trailer.tsx
@@ -32,7 +32,7 @@ export const Trailer = () => {
     }, [isMute])
 
     return (
-        <Box sx={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}>
+        <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
             <Stack
                 onClick={onPlayClick}
                 alignItems="center"
