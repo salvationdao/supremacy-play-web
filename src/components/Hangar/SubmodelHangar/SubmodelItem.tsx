3143597f09f8a51bf969e394c7f16a7dd49d39ac
alyssa <aruefenacht14@gmail.com>
2022-08-25 16:30:47 +0800

    rarity deets

--- src/components/Hangar/SubmodelHangar/SubmodelItem.tsx
+++ src/components/Hangar/SubmodelHangar/SubmodelItem.tsx
@@ -4,6 +4,9 @@ import { fonts } from "../../../theme/theme"
 import { Submodel } from "../../../types"
 import { ClipThing } from "../../Common/ClipThing"
 import { MediaPreview } from "../../Common/MediaPreview/MediaPreview"
+import { useMemo } from "react"
+import { getRarityDeets } from "../../../helpers"
+import { SvgSkin } from "../../../assets"
 
 interface SubmodelItemProps {
     submodel: Submodel
@@ -16,6 +19,7 @@ export const SubmodelItem = ({ submodel, isGridView }: SubmodelItemProps) => {
 
 export const KeycardHangarItemInner = ({ submodel, isGridView }: SubmodelItemProps) => {
     const theme = useTheme()
+    const rarityDeets = useMemo(() => getRarityDeets(submodel.tier), [submodel.tier])
 
     const primaryColor = theme.factionTheme.primary
     const backgroundColor = theme.factionTheme.background
@@ -49,12 +53,30 @@ export const KeycardHangarItemInner = ({ submodel, isGridView }: SubmodelItemPro
                         }}
                     >
                         <MediaPreview
-                            imageUrl={submodel.images.image_url ?? ""}
+                            imageUrl={submodel.images.image_url ?? submodel.images.avatar_url ?? ""}
                             videoUrls={[submodel.images.animation_url, submodel.images.card_animation_url]}
                         />
                     </Box>
 
-                    <Stack spacing=".4rem" sx={{ flex: 1, px: ".4rem", py: ".3rem" }}>
+                    <Stack spacing="0" sx={{ flex: 1, px: ".4rem", py: ".3rem" }}>
+                        <Stack spacing=".4rem" direction="row" alignItems="center">
+                            <Typography
+                                variant="body2"
+                                sx={{
+                                    color: rarityDeets.color,
+                                    fontFamily: fonts.nostromoBold,
+                                    display: "-webkit-box",
+                                    overflow: "hidden",
+                                    overflowWrap: "anywhere",
+                                    textOverflow: "ellipsis",
+                                    WebkitLineClamp: 1, // change to max number of lines
+                                    WebkitBoxOrient: "vertical",
+                                }}
+                            >
+                                {rarityDeets.label}
+                            </Typography>
+                            <SvgSkin fill={rarityDeets.color} size="1.7rem" />
+                        </Stack>
                         <Typography variant="h6" sx={{ color: primaryColor, fontFamily: fonts.nostromoBlack }}>
                             {submodel.label}
                         </Typography>
