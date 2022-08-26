5ad2ef9a1f6987a25d23b2ff3881a45e697c03a9
James Li <jayli3n@gmail.com>
2022-08-26 11:59:35 +0800

    Quick fix

--- src/components/Hangar/SubmodelHangar/SubmodelsHangar.tsx
+++ src/components/Hangar/SubmodelHangar/SubmodelsHangar.tsx
@@ -151,36 +151,21 @@ export const SubmodelsHangar = () => {
                     sortBy = "rarity"
             }
 
-            const resp =
-                submodelType === SubmodelType.warMachine
-                    ? await send<GetSubmodelsResponse, GetSubmodelsRequest>(GameServerKeys.GetMechSubmodels, {
-                          search: search,
-                          sort_by: sortBy,
-                          sort_dir: sortDir,
-                          page_size: pageSize,
-                          page: page,
-                          display_xsyn: false,
-                          exclude_market_locked: false,
-                          include_market_listed: false,
-                          display_genesis_and_limited: false,
-                          rarities: rarities,
-                          skin_compatibility: modelFilter,
-                          equipped_statuses: equippedStatus,
-                      })
-                    : await send<GetSubmodelsResponse, GetSubmodelsRequest>(GameServerKeys.GetWeaponSubmodels, {
-                          search: search,
-                          sort_by: sortBy,
-                          sort_dir: sortDir,
-                          page_size: pageSize,
-                          page: page,
-                          display_xsyn: false,
-                          exclude_market_locked: false,
-                          include_market_listed: false,
-                          display_genesis_and_limited: false,
-                          rarities: rarities,
-                          skin_compatibility: modelFilter,
-                          equipped_statuses: equippedStatus,
-                      })
+            const key = submodelType === SubmodelType.warMachine ? GameServerKeys.GetMechSubmodels : GameServerKeys.GetWeaponSubmodels
+            const resp = await send<GetSubmodelsResponse, GetSubmodelsRequest>(key, {
+                search: search,
+                sort_by: sortBy,
+                sort_dir: sortDir,
+                page_size: pageSize,
+                page: page,
+                display_xsyn: false,
+                exclude_market_locked: false,
+                include_market_listed: false,
+                display_genesis_and_limited: false,
+                rarities: rarities,
+                skin_compatibility: modelFilter,
+                equipped_statuses: equippedStatus,
+            })
 
             updateQuery({
                 sort,
@@ -189,6 +174,7 @@ export const SubmodelsHangar = () => {
                 statuses: equippedStatus.join("||"),
                 page: page.toString(),
                 pageSize: pageSize.toString(),
+                models: modelFilter.join("||"),
             })
 
             if (!resp) return
@@ -209,45 +195,28 @@ export const SubmodelsHangar = () => {
     }, [getItems])
 
     useEffect(() => {
-        submodelType === SubmodelType.warMachine
-            ? (async () => {
-                  try {
-                      setIsLoading(true)
-                      const resp = await send<MechModel[]>(GameServerKeys.GetMechModels)
+        ;(async () => {
+            try {
+                setIsLoading(true)
+                const resp =
+                    submodelType === SubmodelType.warMachine
+                        ? await send<MechModel[]>(GameServerKeys.GetMechModels)
+                        : await send<WeaponModel[]>(GameServerKeys.GetWeaponModels)
 
-                      if (!resp) return
+                if (!resp) return
 
-                      modelFilterSection.current.options = resp.map((r) => {
-                          return { value: r.id, label: r.label, color: colors.blue2 }
-                      })
+                modelFilterSection.current.options = resp.map((r) => {
+                    return { value: r.id, label: r.label, color: colors.blue2 }
+                })
 
-                      toggleSortFilterReRender()
-                  } catch (e) {
-                      setLoadError(typeof e === "string" ? e : `Failed to get war machines models.`)
-                      console.error(e)
-                  } finally {
-                      setIsLoading(false)
-                  }
-              })()
-            : (async () => {
-                  try {
-                      setIsLoading(true)
-                      const resp = await send<WeaponModel[]>(GameServerKeys.GetWeaponModels)
-
-                      if (!resp) return
-
-                      modelFilterSection.current.options = resp.map((r) => {
-                          return { value: r.id, label: r.label, color: colors.blue2 }
-                      })
-
-                      toggleSortFilterReRender()
-                  } catch (e) {
-                      setLoadError(typeof e === "string" ? e : "Failed to get weapon models.")
-                      console.error(e)
-                  } finally {
-                      setIsLoading(false)
-                  }
-              })()
+                toggleSortFilterReRender()
+            } catch (e) {
+                setLoadError(typeof e === "string" ? e : `Failed to get ${submodelType} submodels.`)
+                console.error(e)
+            } finally {
+                setIsLoading(false)
+            }
+        })()
     }, [send, submodelType, toggleSortFilterReRender])
 
     const content = useMemo(() => {
@@ -331,7 +300,7 @@ export const SubmodelsHangar = () => {
                             textAlign: "center",
                         }}
                     >
-                        {`There are no ${submodelType} Submodels found, please try again.`}
+                        {`There are no ${submodelType} submodels found, please try again.`}
                     </Typography>
 
                     <FancyButton
@@ -426,6 +395,7 @@ export const SubmodelsHangar = () => {
                             isFiltersExpanded={isFiltersExpanded}
                             toggleIsFiltersExpanded={toggleIsFiltersExpanded}
                         />
+
                         <Stack direction={"row"} sx={{ borderBottom: `${theme.factionTheme.primary}70 1.5px solid` }}>
                             <Tabs
                                 value={submodelType}
