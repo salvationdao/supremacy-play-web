e5651b337979b158080572566a5df0d2591b3cc0
James Li <jayli3n@gmail.com>
2022-09-02 14:04:59 +0800

    Done

--- src/components/Bar/Messages/MessagesMainView/MessageDisplay/Common/SystemMessageMech.tsx
+++ src/components/Bar/Messages/MessagesMainView/MessageDisplay/Common/SystemMessageMech.tsx
@@ -1,6 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material"
 import { useMemo } from "react"
-import { SvgCrown, SvgSkull } from "../../../../../../assets"
+import { SvgCrown, SvgDeath, SvgSkull, SvgSkull2 } from "../../../../../../assets"
 import { useSupremacy } from "../../../../../../containers"
 import { useTheme } from "../../../../../../containers/theme"
 import { getRarityDeets } from "../../../../../../helpers"
@@ -17,153 +17,160 @@ export const SystemMessageMech = ({ mech }: { mech: SystemMessageMechStruct }) =
     const rarityDeets = useMemo(() => getRarityDeets(tier || ""), [tier])
 
     return (
-        <ClipThing
-            clipSize="6px"
-            opacity={0.8}
-            border={{ borderColor: theme.factionTheme.primary, borderThickness: killed ? "0" : "1px" }}
-            backgroundColor={theme.factionTheme.background}
-        >
-            <Stack
-                spacing="1.2rem"
-                alignItems="stretch"
-                sx={{
-                    position: "relative",
-                    p: ".8rem 1rem",
-                    borderRadius: 0.8,
-                    width: "20rem",
-                }}
+        <Stack spacing="1rem" sx={{ width: "20rem" }}>
+            <ClipThing
+                clipSize="6px"
+                opacity={0.8}
+                border={{ borderColor: theme.factionTheme.primary, borderThickness: killed ? "0" : "1px" }}
+                backgroundColor={theme.factionTheme.background}
             >
-                {/* Mech image */}
-                <Box
+                <Stack
+                    spacing="1.2rem"
+                    alignItems="stretch"
                     sx={{
                         position: "relative",
-                        height: "16rem",
-                        width: "100%",
-                        borderRadius: 0.3,
-                        overflow: "hidden",
+                        p: ".8rem 1rem",
+                        borderRadius: 0.8,
                     }}
                 >
+                    {/* Mech image */}
                     <Box
                         sx={{
-                            height: "100%",
+                            position: "relative",
+                            height: "16rem",
                             width: "100%",
-                            background: `url(${image_url})`,
-                            backgroundRepeat: "no-repeat",
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
+                            borderRadius: 0.3,
+                            overflow: "hidden",
                         }}
-                    />
-
-                    {killed ? (
-                        <Stack
-                            alignItems="center"
-                            justifyContent="center"
+                    >
+                        <Box
                             sx={{
-                                px: "4rem",
-                                position: "absolute",
-                                top: 0,
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                background: "linear-gradient(#00000090, #000000)",
-                                zIndex: 2,
+                                height: "100%",
+                                width: "100%",
+                                background: `url(${image_url})`,
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                backgroundSize: "cover",
                             }}
-                        >
-                            <SvgSkull size="90%" />
-                        </Stack>
-                    ) : (
-                        <SvgCrown size="2rem" fill={colors.gold} sx={{ position: "absolute", top: ".4rem", left: ".4rem" }} />
-                    )}
-                </Box>
+                        />
 
-                {/* Info */}
-                <Stack sx={{ flex: 1, py: ".2rem" }}>
-                    <Box sx={{ py: ".2rem", flex: 1 }}>
-                        <Typography
-                            variant="caption"
-                            sx={{
-                                fontFamily: fonts.nostromoHeavy,
-                                color: rarityDeets.color,
-                            }}
-                        >
-                            {rarityDeets.label}
-                        </Typography>
+                        {killed ? (
+                            <Stack
+                                alignItems="center"
+                                justifyContent="center"
+                                sx={{
+                                    px: "4rem",
+                                    position: "absolute",
+                                    top: 0,
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    background: "linear-gradient(#00000090, #000000)",
+                                    zIndex: 2,
+                                }}
+                            >
+                                <SvgSkull size="90%" />
+                            </Stack>
+                        ) : (
+                            <SvgCrown size="2rem" fill={colors.gold} sx={{ position: "absolute", top: ".4rem", left: ".4rem" }} />
+                        )}
+                    </Box>
 
+                    {/* Info */}
+                    <Stack sx={{ flex: 1, py: ".2rem" }}>
+                        <Box sx={{ py: ".2rem", flex: 1 }}>
+                            <Typography
+                                variant="caption"
+                                sx={{
+                                    fontFamily: fonts.nostromoHeavy,
+                                    color: rarityDeets.color,
+                                }}
+                            >
+                                {rarityDeets.label}
+                            </Typography>
+
+                            <Typography
+                                variant="body2"
+                                sx={{
+                                    fontSize: "1.8rem",
+                                    display: "-webkit-box",
+                                    overflow: "hidden",
+                                    overflowWrap: "anywhere",
+                                    textOverflow: "ellipsis",
+                                    WebkitLineClamp: 1,
+                                    WebkitBoxOrient: "vertical",
+                                }}
+                            >
+                                {name}
+                            </Typography>
+                        </Box>
+
+                        {total_blocks && <RepairBlocks defaultBlocks={total_blocks} remainDamagedBlocks={damaged_blocks || 0} />}
+                    </Stack>
+                </Stack>
+            </ClipThing>
+
+            {killed && (
+                <Stack spacing=".3rem" sx={{ px: "1rem" }}>
+                    <Typography variant="body2" sx={{ fontFamily: fonts.nostromoBlack }}>
+                        KILLED BY:
+                    </Typography>
+
+                    <Stack spacing=".5rem" direction="row" alignItems="flex-start">
+                        <SvgSkull2 size="1.4rem" />
                         <Typography
-                            variant="body2"
                             sx={{
-                                fontSize: "1.8rem",
+                                fontWeight: "fontWeightBold",
+                                color: getFaction(killed.faction_id).primary_color,
+                                lineHeight: 1.2,
                                 display: "-webkit-box",
                                 overflow: "hidden",
                                 overflowWrap: "anywhere",
                                 textOverflow: "ellipsis",
-                                WebkitLineClamp: 1,
+                                WebkitLineClamp: 2, // change to max number of lines
                                 WebkitBoxOrient: "vertical",
                             }}
                         >
-                            {name}
+                            {killed.name}
                         </Typography>
-                    </Box>
-
-                    {total_blocks && <RepairBlocks defaultBlocks={total_blocks} remainDamagedBlocks={damaged_blocks || 0} />}
+                    </Stack>
                 </Stack>
-            </Stack>
-        </ClipThing>
+            )}
+
+            {kills && (
+                <Stack spacing=".3rem" sx={{ px: "1rem" }}>
+                    <Typography variant="body2" sx={{ fontFamily: fonts.nostromoBlack }}>
+                        KILLS:
+                    </Typography>
+
+                    <>
+                        {kills.map((kill, i) => {
+                            const killedFaction = getFaction(kill.faction_id)
+                            return (
+                                <Stack key={i} spacing=".5rem" direction="row" alignItems="flex-start">
+                                    <SvgDeath size="1.4rem" />
+                                    <Typography
+                                        sx={{
+                                            fontWeight: "fontWeightBold",
+                                            color: killedFaction.primary_color,
+                                            textDecoration: "line-through",
+                                            lineHeight: 1.2,
+                                            display: "-webkit-box",
+                                            overflow: "hidden",
+                                            overflowWrap: "anywhere",
+                                            textOverflow: "ellipsis",
+                                            WebkitLineClamp: 2, // change to max number of lines
+                                            WebkitBoxOrient: "vertical",
+                                        }}
+                                    >
+                                        {kill.name}
+                                    </Typography>
+                                </Stack>
+                            )
+                        })}
+                    </>
+                </Stack>
+            )}
+        </Stack>
     )
-
-    // return (
-    //     <Box>
-    //         <Stack direction="row" spacing=".6rem" alignItems="center">
-    //             {!battleBrief.killed && <SvgCrown size="1.7rem" fill={colors.gold} />}
-    //         </Stack>
-
-    //         {battleBrief.kills && battleBrief.kills.length > 0 && (
-    //             <>
-    //                 <Stack direction="column" spacing=".2rem">
-    //                     <Typography
-    //                         sx={{
-    //                             fontWeight: "fontWeightBold",
-    //                         }}
-    //                     >
-    //                         KILLS:
-    //                     </Typography>
-
-    //                     {battleBrief.kills.map((k, i) => (
-    //                         <Typography
-    //                             key={i}
-    //                             sx={{
-    //                                 fontWeight: "fontWeightBold",
-    //                                 color: getFaction(k.faction_id)?.primary_color,
-    //                                 pl: "4rem",
-    //                             }}
-    //                         >
-    //                             {k.name}
-    //                         </Typography>
-    //                     ))}
-    //                 </Stack>
-    //             </>
-    //         )}
-
-    //         {battleBrief.killed && (
-    //             <Stack direction="column" spacing=".2rem">
-    //                 <Typography
-    //                     sx={{
-    //                         fontWeight: "fontWeightBold",
-    //                     }}
-    //                 >
-    //                     KILLED BY:
-    //                 </Typography>
-    //                 <Typography
-    //                     sx={{
-    //                         fontWeight: "fontWeightBold",
-    //                         color: getFaction(battleBrief.killed.faction_id)?.primary_color,
-    //                         pl: "4rem",
-    //                     }}
-    //                 >
-    //                     {battleBrief.killed.name}
-    //                 </Typography>
-    //             </Stack>
-    //         )}
-    //     </Box>
-    // )
 }
