579e8c027d61ff8c02f07954cc2ff1fd7d08be9d
James Li <jayli3n@gmail.com>
2022-09-08 12:07:49 +0800

    Update styles

--- src/components/Controls/OverlayToggles.tsx
+++ src/components/Controls/OverlayToggles.tsx
@@ -1,10 +1,10 @@
 import { IconButton, Stack, Typography } from "@mui/material"
 import { TooltipHelper } from ".."
-import { SvgHistory } from "../../assets"
+import { SvgTrailer, SvgUpcomingBattle } from "../../assets"
 import { useUI } from "../../containers"
 
 export const OverlayToggles = () => {
-    const { isBattleHistoryOpen, toggleIsBattleHistoryOpen } = useUI()
+    const { showUpcomingBattle, toggleShowUpcomingBattle, showTrailer, toggleShowTrailer } = useUI()
 
     return (
         <Stack direction="row" alignItems="center" sx={{ height: "100%" }}>
@@ -12,8 +12,25 @@ export const OverlayToggles = () => {
                 OVERLAYS:{" "}
             </Typography>
 
+            {/* Upcoming battle */}
+            <TooltipHelper text="Toggle the upcoming battle screen.">
+                <IconButton
+                    size="small"
+                    onClick={() => toggleShowUpcomingBattle()}
+                    sx={{
+                        filter: showUpcomingBattle ? "grayscale(0)" : "grayscale(1)",
+                        opacity: showUpcomingBattle ? 1 : 0.4,
+                        transition: "all .2s",
+                        ":hover": { filter: "grayscale(0.2)" },
+                        ":active": { filter: "grayscale(.6)" },
+                    }}
+                >
+                    <SvgUpcomingBattle size="1.7rem" fill="#E8BB3F" />
+                </IconButton>
+            </TooltipHelper>
+
             {/* Battle history */}
-            <TooltipHelper text="Toggle the battle history page.">
+            {/* <TooltipHelper text="Toggle the battle history page.">
                 <IconButton
                     size="small"
                     onClick={() => toggleIsBattleHistoryOpen()}
@@ -26,6 +43,23 @@ export const OverlayToggles = () => {
                     }}
                 >
                     <SvgHistory size="1.7rem" fill="#E8BB3F" />
+                </IconButton>
+            </TooltipHelper> */}
+
+            {/* Watch trailer */}
+            <TooltipHelper text="Watch the Supremacy trailer video.">
+                <IconButton
+                    size="small"
+                    onClick={() => toggleShowTrailer()}
+                    sx={{
+                        filter: showTrailer ? "grayscale(0)" : "grayscale(1)",
+                        opacity: showTrailer ? 1 : 0.4,
+                        transition: "all .2s",
+                        ":hover": { filter: "grayscale(0.2)" },
+                        ":active": { filter: "grayscale(.6)" },
+                    }}
+                >
+                    <SvgTrailer size="1.7rem" fill="#E8BB3F" />
                 </IconButton>
             </TooltipHelper>
         </Stack>
