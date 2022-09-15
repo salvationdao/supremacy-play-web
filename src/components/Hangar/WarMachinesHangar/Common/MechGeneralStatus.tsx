21c9282ec6a0d70a7543c57d65dd74b4485a9718
lolivise <tom790924@gmail.com>
2022-09-15 16:47:50 +0800

    fix mech status update

--- src/components/Hangar/WarMachinesHangar/Common/MechGeneralStatus.tsx
+++ src/components/Hangar/WarMachinesHangar/Common/MechGeneralStatus.tsx
@@ -135,7 +135,7 @@ export const MechGeneralStatus = ({
     const triggerStatusUpdate = useCallback(
         async (currentStatus: string) => {
             try {
-                if (!currentStatus.includes("QUEUE") && !currentStatus.includes("BATTLING")) return
+                if (!currentStatus.includes("QUEUE") && !currentStatus.includes("POSITION") && !currentStatus.includes("BATTLING")) return
                 await send(GameServerKeys.TriggerMechStatusUpdate, {
                     mech_id: mechID,
                 })
@@ -154,6 +154,7 @@ export const MechGeneralStatus = ({
         },
         (payload) => {
             if (!payload) return
+            console.log(textValue.current)
             triggerStatusUpdate(textValue.current)
         },
     )
