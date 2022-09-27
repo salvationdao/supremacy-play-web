a7d26ab93190ee59663931db50409abfab54fc4b
James Li <jayli3n@gmail.com>
2022-09-27 11:54:58 +0800

    WIP

--- src/components/BigDisplay/MiniMapNew/OverlayItems/TargetSelect/TargetSelect.tsx
+++ src/components/BigDisplay/MiniMapNew/OverlayItems/TargetSelect/TargetSelect.tsx
@@ -162,7 +162,11 @@ const TargetHintInner = React.memo(function TargetHintInner({ ability, endTime, 
 
             // If ability has an end time and selection is placed, hide the icon
             if (endTime) {
-                pixiTargetHint.mouseIcon.showIcon(!selectedStartCoord.current)
+                if (isLocationSelection) {
+                    pixiTargetHint.mouseIcon.showIcon(!selectedStartCoord.current)
+                } else if (isLineSelection) {
+                    pixiTargetHint.mouseIcon.showIcon(!selectedStartCoord.current && !selectedEndCoord.current)
+                }
             }
         }
     }, [ability.location_select_type, endTime, gridCellToViewportPosition, onSelectMapPositionCallbacks, pixiTargetHint, selectMapPosition])
