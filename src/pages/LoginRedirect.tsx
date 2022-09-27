eb7fa28e343dbe5d921c86ecdf8cf97804ea8270
hjpunzalan <hj.punzalan@hotmail.com>
2022-09-27 14:26:04 +0800

    update text

--- src/pages/LoginRedirect.tsx
+++ src/pages/LoginRedirect.tsx
@@ -22,7 +22,8 @@ export const LoginRedirect = () => {
     return (
         <Stack alignItems="center" justifyContent="center" sx={{ height: "100vh", p: "3.8rem", backgroundColor: colors.darkNavy }}>
             <Typography variant="h5" sx={{ fontFamily: fonts.nostromoBlack, textAlign: "center" }}>
-                Please close this window to login. <br />
+                You can now close this window and navigate back to the Battle Arena.
+                <br />
                 <br /> <span style={{ fontSize: "1.7rem" }}>Automatically closing this window in </span>
                 <span style={{ fontSize: "3rem" }}> {parseInt(`${time / 1000}`)}...</span>
             </Typography>
