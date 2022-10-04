936915e89795d1697d3328b225a14f5518f313cb
James Li <jayli3n@gmail.com>
2022-10-04 11:41:12 +0800

    Add coord to bottom left

--- src/components/BigDisplay/MiniMapNew/OverlayItems/MapScale/pixiMapScale.ts
+++ src/components/BigDisplay/MiniMapNew/OverlayItems/MapScale/pixiMapScale.ts
@@ -1,29 +1,44 @@
 import { Viewport } from "pixi-viewport"
 import * as PIXI from "pixi.js"
 import { pixiStageZIndexes } from "../../../../../containers"
-import { HEXToVBColor } from "../../../../../helpers"
+import { HEXToVBColor, intToLetter } from "../../../../../helpers"
 import { fonts } from "../../../../../theme/theme"
-import { Dimension, GAME_CLIENT_TILE_SIZE } from "../../../../../types"
+import { Dimension, GAME_CLIENT_TILE_SIZE, Position } from "../../../../../types"
+import { MAX_COLS, MAX_ROWS } from "../../ViewportItems/Grid/pixiGrid"
+
+const GAP = 8
 
 export class PixiMapScale {
     root: PIXI.Container<PIXI.DisplayObject>
     private label: PIXI.Text
+    private coordLabel: PIXI.Text
     private line: PIXI.Graphics
     private viewport: Viewport
     private gridSizeRef: React.MutableRefObject<Dimension>
+    private mapMousePosition: React.MutableRefObject<Position | undefined>
     private animationFrame: number | undefined
 
-    constructor(viewport: Viewport, gridSizeRef: React.MutableRefObject<Dimension>) {
+    constructor(viewport: Viewport, gridSizeRef: React.MutableRefObject<Dimension>, mapMousePosition: React.MutableRefObject<Position | undefined>) {
         this.viewport = viewport
         this.gridSizeRef = gridSizeRef
+        this.mapMousePosition = mapMousePosition
 
         // Create container for everything
         this.root = new PIXI.Container()
         this.root.zIndex = pixiStageZIndexes.mapScale
 
+        // Co-ordinate text
+        this.coordLabel = new PIXI.Text("", {
+            fontFamily: fonts.shareTech,
+            fontSize: 12,
+            fill: "#FFFFFF",
+            lineHeight: 1,
+        })
+        this.coordLabel.resolution = 4
+        this.coordLabel.zIndex = 5
+
         // Line
         this.line = new PIXI.Graphics()
-        this.line.position.set(0, 5)
 
         // Label
         this.label = new PIXI.Text(`${Math.round(GAME_CLIENT_TILE_SIZE / 100)}m`, {
@@ -37,8 +52,9 @@ export class PixiMapScale {
         this.label.zIndex = 5
 
         // Add everything to container
-        this.root.addChild(this.label)
+        this.root.addChild(this.coordLabel)
         this.root.addChild(this.line)
+        this.root.addChild(this.label)
 
         this.render()
     }
@@ -50,20 +66,34 @@ export class PixiMapScale {
 
     render() {
         const step = () => {
-            const width = this.gridSizeRef.current.width * this.viewport.scale.x
+            // Get the grid row and column that the mouse is on
+            const mousePos = this.mapMousePosition.current
+            let highlightColIndex = -1
+            let highlightRowIndex = -1
+            if (mousePos) {
+                const rowCount = Math.round(Math.min(this.viewport.worldHeight / this.gridSizeRef.current.height, MAX_ROWS))
+                const colCount = Math.round(Math.min(this.viewport.worldWidth / this.gridSizeRef.current.width, MAX_COLS))
+                const cellWidth = this.viewport.worldWidth / colCount
+                const cellHeight = this.viewport.worldHeight / rowCount
+                highlightColIndex = Math.floor(mousePos.x / cellWidth)
+                highlightRowIndex = Math.floor(mousePos.y / cellHeight)
+                this.coordLabel.text = `[${intToLetter(highlightColIndex + 1)}${highlightRowIndex}]`
+            }
 
             // Rect
+            const width = this.gridSizeRef.current.width * this.viewport.scale.x
             this.line.clear()
             this.line.lineStyle(1, HEXToVBColor("#FFFFFF"))
             this.line.moveTo(0, 0)
             this.line.lineTo(0, 4)
             this.line.lineTo(width, 4)
             this.line.lineTo(width, 0)
-            this.label.position.set(this.line.width + 8, 0)
+            this.line.position.set(this.coordLabel.width + GAP, 5)
+            this.label.position.set(this.line.x + this.line.width + GAP, 0)
 
             this.root.pivot.set(this.root.width, this.root.height)
-            this.root.x = this.viewport.screenWidth - 11
-            this.root.y = this.viewport.screenHeight - 8
+            this.root.x = this.viewport.screenWidth - GAP * 1.4
+            this.root.y = this.viewport.screenHeight - GAP
 
             this.animationFrame = requestAnimationFrame(step)
         }
