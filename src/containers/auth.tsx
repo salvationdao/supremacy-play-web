9c6bea47e4a3f7b75d35acc459fd813b4621f4d4
felix t <41817193+o2bomb@users.noreply.github.com>
2022-10-17 14:05:22 +0800

    Get GID from route instead of route parameter

--- src/containers/auth.tsx
+++ src/containers/auth.tsx
@@ -44,6 +44,7 @@ export interface AuthState {
     user: User
     userID: string
     factionID: string
+    roleType: RoleType
     setUser: Dispatch<React.SetStateAction<User>>
     userStat: UserStat
     setUserStat: Dispatch<React.SetStateAction<UserStat>>
@@ -73,6 +74,7 @@ const initialState: AuthState = {
     user: FallbackUser,
     userID: FallbackUser.id,
     factionID: FallbackUser.faction_id,
+    roleType: FallbackUser.role_type,
     setUser: () => {
         return
     },
@@ -111,6 +113,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User>(initialState.user)
     const userID = user.id
     const factionID = user.faction_id
+    const roleType = user.role_type
     const [isActive, setIsActive] = useState(true)
     const [userStat, setUserStat] = useState<UserStat>(initialState.userStat)
     const [userRank, setUserRank] = useState<UserRank>(initialState.userRank)
@@ -261,6 +264,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
                 user,
                 userID,
                 factionID,
+                roleType,
                 setUser,
                 punishments,
                 setPunishments,
