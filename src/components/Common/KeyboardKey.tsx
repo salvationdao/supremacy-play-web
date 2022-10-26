f1a124ab4fdb2690bf50b141e1e6e9653e632a12
James Li <jayli3n@gmail.com>
2022-10-26 13:18:47 +0800

    Add cards

--- src/components/Common/KeyboardKey.tsx
+++ src/components/Common/KeyboardKey.tsx
@@ -1,13 +1,13 @@
-import { Stack, Typography } from "@mui/material"
+import { Stack, SxProps, Typography } from "@mui/material"
 import { Variant } from "@mui/material/styles/createTypography"
 import { fonts } from "../../theme/theme"
 
-export const KeyboardKey = ({ label, variant = "subtitle2" }: { label: string; variant?: Variant }) => {
+export const KeyboardKey = ({ sx, label, variant = "subtitle2" }: { sx?: SxProps; label: string; variant?: Variant }) => {
     return (
         <Stack
             alignItems="center"
             justifyContent="center"
-            sx={{ p: ".3rem .3rem", pt: ".4rem", border: `#FFFFFF 1px solid`, borderRadius: ".4rem", minWidth: "20px" }}
+            sx={{ p: ".3rem .3rem", pt: ".4rem", border: `#FFFFFF 1px solid`, borderRadius: ".4rem", minWidth: "20px", ...sx }}
         >
             <Typography variant={variant} sx={{ lineHeight: 1, fontFamily: fonts.nostromoBold }}>
                 {label}
--- src/components/MainMenuNav/TabContent/TabContentItem.tsx
+++ src/components/MainMenuNav/TabContent/TabContentItem.tsx
@@ -1,6 +1,13 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { RouteSingle } from "../../../routes"
+import { fonts } from "../../../theme/theme"
+import { KeyboardKey } from "../../Common/KeyboardKey"
 
 export const TabContentItem = ({ route, index }: { route: RouteSingle; index: number }) => {
-    return <Box sx={{ width: "100%", height: "100%", backgroundColor: "#FFFFFF30" }}>{index}</Box>
+    return (
+        <Box sx={{ position: "relative", width: "100%", height: "100%", backgroundColor: "#FFFFFF30" }}>
+            <Typography sx={{ position: "absolute", top: "1.2rem", left: "1.3rem", fontFamily: fonts.nostromoBlack }}>{route.showInMainMenu?.label}</Typography>
+            <KeyboardKey variant="body2" sx={{ position: "absolute", top: "1.1rem", right: "1.3rem" }} label={`${index}`} />
+        </Box>
+    )
 }
