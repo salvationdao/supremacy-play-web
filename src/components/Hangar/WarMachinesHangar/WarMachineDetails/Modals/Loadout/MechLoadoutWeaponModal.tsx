689264eecd7be4d64f20c53f815672bc1226ae09
James Li <jayli3n@gmail.com>
2022-11-07 14:12:59 +0800

    Refactor css, simplify style

--- src/components/Hangar/WarMachinesHangar/WarMachineDetails/Modals/Loadout/MechLoadoutWeaponModal.tsx
+++ src/components/Hangar/WarMachinesHangar/WarMachineDetails/Modals/Loadout/MechLoadoutWeaponModal.tsx
@@ -451,7 +451,7 @@ export const MechLoadoutWeaponModal = ({
             return (
                 <Stack alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
                     <Stack alignItems="center" justifyContent="center" sx={{ height: "100%", px: "3rem", pt: "1.28rem" }}>
-                        <CircularProgress size="3rem" sx={{ color: theme.factionTheme.primary }} />
+                        <CircularProgress />
                     </Stack>
                 </Stack>
             )
