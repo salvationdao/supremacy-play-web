8ae07dc581c68090e2d91b6c2978ba1cd46efd1c
lolivise <tom790924@gmail.com>
2022-11-14 18:02:59 +0800

    add war machines

--- src/components/Lobbies/Createlobby/WarMachineForm.tsx
+++ src/components/Lobbies/Createlobby/WarMachineForm.tsx
@@ -2,6 +2,7 @@ import { Stack } from "@mui/material"
 import { NiceButton } from "../../Common/Nice/NiceButton"
 import React from "react"
 import { useTheme } from "../../../containers/theme"
+import { Section } from "./RoomSettingForm"
 
 interface WarMachineFormProps {
     prevPage: () => void
@@ -11,7 +12,9 @@ export const WarMachineForm = ({ prevPage }: WarMachineFormProps) => {
     const { factionTheme } = useTheme()
     return (
         <Stack direction="column" flex={1} sx={{ px: "25rem", py: "4rem" }}>
-            <Stack direction="column" flex={1}></Stack>
+            <Stack direction="column" flex={1}>
+                <Section orderLabel="A" title="WAR MACHINES" description="Select your War machines to deploy for this lobby." />
+            </Stack>
             <Stack direction="row" alignItems="center" justifyContent="space-between">
                 <NiceButton
                     buttonColor={factionTheme.primary}
