ad9c85602201e7c7f3fbb24546063b76242b9574
James Li <jayli3n@gmail.com>
2022-11-18 11:43:10 +0800

    WIP

--- src/components/Common/Mech/MechCardWeaponAndStats.tsx
+++ src/components/Common/Mech/MechCardWeaponAndStats.tsx
@@ -1,5 +1,8 @@
+import { Stack } from "@mui/material"
 import React from "react"
+import { colors } from "../../../theme/theme"
 import { NewMechStruct } from "../../../types"
+import { NiceBoxThing } from "../Nice/NiceBoxThing"
 
 export const MechCardWeaponAndStats = React.memo(function MechCardWeaponAndStats({
     mech,
@@ -10,5 +13,16 @@ export const MechCardWeaponAndStats = React.memo(function MechCardWeaponAndStats
     isSelected?: boolean
     toggleSelected?: (mech: NewMechStruct) => void
 }) {
-    return null
+    return (
+        <NiceBoxThing
+            border={{
+                color: isSelected ? `${colors.neonBlue}80` : "#FFFFFF20",
+                thickness: isSelected ? "lean" : "very-lean",
+            }}
+            background={{ colors: ["#FFFFFF", "#FFFFFF"], opacity: 0.06 }}
+            sx={{ p: "1rem 1.5rem" }}
+        >
+            <Stack direction="row"></Stack>
+        </NiceBoxThing>
+    )
 })
