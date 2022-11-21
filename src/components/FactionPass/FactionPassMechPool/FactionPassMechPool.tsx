c32cc5e4deef4ceb447dc57356e73dcf63011773
James Li <jayli3n@gmail.com>
2022-11-21 15:19:31 +0800

    Styles

--- src/components/FactionPass/FactionPassMechPool/FactionPassMechPool.tsx
+++ src/components/FactionPass/FactionPassMechPool/FactionPassMechPool.tsx
@@ -304,11 +304,6 @@ export const FactionPassMechPool = () => {
                 mx: "auto",
                 position: "relative",
                 height: "100%",
-                backgroundColor: theme.factionTheme.background,
-                background: `url()`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
                 maxWidth: "190rem",
             }}
         >
