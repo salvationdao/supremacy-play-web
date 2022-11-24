5c9e55999b96f36418413b07ef875230fb808abe
lolivise <tom790924@gmail.com>
2022-11-24 13:14:49 +0800

    dashboard

--- src/components/FactionPass/FactionPassDashboard/FactionPassDashboard.tsx
+++ src/components/FactionPass/FactionPassDashboard/FactionPassDashboard.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material"
 import { useMemo } from "react"
 import { useAuth, useSupremacy } from "../../../containers"
 import { NavTabs } from "../../Common/NavTabs/NavTabs"
@@ -13,21 +13,35 @@ export const FactionPassDashboard = () => {
         return getFaction(factionID)
     }, [factionID, getFaction])
 
+    console.log(faction.logo_url)
+
     return (
         <Stack
             alignItems="center"
             sx={{
                 p: "4rem 5rem",
+                mx: "auto",
                 position: "relative",
                 height: "100%",
+                maxWidth: "190rem",
                 backgroundColor: faction.palette.background,
-                background: `url()`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
             }}
         >
             <NavTabs activeTabID={activeTabID} setActiveTabID={setActiveTabID} tabs={tabs} prevTab={prevTab} nextTab={nextTab} />
+            <Stack direction="row" alignItems="stretch" sx={{ flex: 1, width: "100%", overflow: "hidden" }}>
+                <Stack sx={{ width: "20rem" }}>
+                    <Box
+                        sx={{
+                            background: `url(${faction.logo_url})`,
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                        }}
+                    />
+                </Stack>
+                <Stack flex={1}></Stack>
+                <Stack sx={{ width: "20rem" }}></Stack>
+            </Stack>
         </Stack>
     )
 }
