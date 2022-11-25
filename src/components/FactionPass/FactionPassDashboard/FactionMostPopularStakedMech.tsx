d470fff9dce4c0997ef83d443c01b48eded6f679
lolivise <tom790924@gmail.com>
2022-11-25 15:58:43 +0800

    restyle popular mech box

--- src/components/FactionPass/FactionPassDashboard/FactionMostPopularStakedMech.tsx
+++ src/components/FactionPass/FactionPassDashboard/FactionMostPopularStakedMech.tsx
@@ -9,7 +9,7 @@ import { MediaPreview } from "../../Common/MediaPreview/MediaPreview"
 import { useSupremacy } from "../../../containers"
 import { RepairBlocks } from "../../Common/Mech/MechRepairBlocks"
 import { MechIdleStatus } from "../../Common/Mech/MechIdleStatus"
-import { getRarityDeets, numFormatter } from "../../../helpers"
+import { getMechStatusDeets, getRarityDeets, numFormatter } from "../../../helpers"
 import {
     SvgLoadoutArmour,
     SvgLoadoutPowerCoreCapacity,
@@ -46,11 +46,7 @@ export const FactionMostPopularStakedMech = () => {
         if (!mostPopularMech) return null
         const ownerFaction = getFaction(mostPopularMech.owner.faction_id)
         const rarity = getRarityDeets(mostPopularMech.tier)
-
-        console.log(mostPopularMech.weapon_slots)
-        console.log(mostPopularMech.utilities)
-        console.log(mostPopularMech.power_core)
-        console.log(mostPopularMech.skin_image_url)
+        const statusDeets = getMechStatusDeets(mostPopularMech.status)
         return (
             <>
                 {/* Mech image */}
@@ -63,116 +59,126 @@ export const FactionMostPopularStakedMech = () => {
                 </NiceBoxThing>
 
                 <Typography>{mostPopularMech.name || mostPopularMech.label}</Typography>
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    <Typography>{mostPopularMech.label}</Typography>
-                    <Typography color={rarity.color}>{mostPopularMech.tier}</Typography>
-                </Stack>
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    {/* Repair blocks */}
-                    <RepairBlocks
-                        defaultBlocks={mostPopularMech.repair_blocks}
-                        remainDamagedBlocks={mostPopularMech.damaged_blocks}
-                        sx={{
-                            width: "fit-content",
-                        }}
-                    />
+                <Stack spacing="1rem">
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <Typography>{mostPopularMech.label}</Typography>
+                        <Typography color={rarity.color}>{mostPopularMech.tier}</Typography>
+                    </Stack>
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        {/* Repair blocks */}
+                        <RepairBlocks
+                            defaultBlocks={mostPopularMech.repair_blocks}
+                            remainDamagedBlocks={mostPopularMech.damaged_blocks}
+                            sx={{
+                                width: "fit-content",
+                            }}
+                        />
 
-                    {/* Mech status */}
-                    <MechIdleStatus mech={mostPopularMech} />
-                </Stack>
-
-                {/* Mech Status*/}
-                <Stack direction="row" alignItems="center" justifyContent="space-between" spacing=".25rem">
-                    <MechStatus label={"Health"} icon={<SvgLoadoutArmour />} value={mostPopularMech.max_hitpoints} />
-                    <MechStatus label={"Speed"} icon={<SvgLoadoutSpeed />} value={mostPopularMech.speed} />
-                    <MechStatus label={"Power Core Recharge Rate"} icon={<SvgLoadoutPowerCoreRegen />} value={mostPopularMech.power_core?.recharge_rate || 0} />
-                    <MechStatus label={"Power Core Capacity"} icon={<SvgLoadoutPowerCoreCapacity />} value={mostPopularMech.power_core?.capacity || 0} />
-                </Stack>
-
-                {/* Mech battle stat*/}
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    <NiceTooltip placement="top-start" text="Total kills">
-                        <Typography whiteSpace="nowrap">
-                            <SvgMechKills inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_kills)}
-                        </Typography>
-                    </NiceTooltip>
-                    <NiceTooltip placement="top-start" text="Total deaths">
-                        <Typography whiteSpace="nowrap">
-                            <SvgMechDeaths inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_deaths)}
-                        </Typography>
-                    </NiceTooltip>
-
-                    <NiceTooltip placement="top-start" text="Total wins">
-                        <Typography whiteSpace="nowrap">
-                            <SvgMechWins inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_wins)}
-                        </Typography>
-                    </NiceTooltip>
-
-                    <NiceTooltip placement="top-start" text="Total losses">
-                        <Typography whiteSpace="nowrap">
-                            <SvgMechLosses inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_losses)}
-                        </Typography>
-                    </NiceTooltip>
-                </Stack>
-
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    <Stack spacing=".5rem">
-                        <SvgWeapons fill={colors.weapons} size="2.5rem" />
-                        <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
-                            {mostPopularMech.weapon_slots &&
-                                mostPopularMech.weapon_slots.map((ws, i) => (
-                                    <Box
-                                        key={i}
-                                        sx={{
-                                            width: "1rem",
-                                            height: ".3rem",
-                                            backgroundColor: ws.weapon ? colors.weapons : `${colors.offWhite}20`,
-                                        }}
-                                    />
-                                ))}
+                        {/* Mech status */}
+                        <Stack direction="row" alignItems="center">
+                            <Typography>Status:</Typography>
+                            <Typography color={statusDeets.color}>{statusDeets.label}</Typography>
                         </Stack>
                     </Stack>
 
-                    <Stack spacing=".5rem">
-                        <SvgUtilities fill={colors.utilities} size="2.5rem" />
-                        <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
-                            {mostPopularMech.utilities &&
-                                mostPopularMech.utilities.map((us, i) => (
-                                    <Box
-                                        key={i}
-                                        sx={{
-                                            width: "1rem",
-                                            height: ".3rem",
-                                            backgroundColor: us.utility ? colors.utilities : `${colors.offWhite}20`,
-                                        }}
-                                    />
-                                ))}
-                        </Stack>
+                    {/* Mech Status*/}
+                    <Stack direction="row" alignItems="center" justifyContent="space-between" spacing=".25rem">
+                        <MechStatus label={"Health"} icon={<SvgLoadoutArmour />} value={mostPopularMech.max_hitpoints} />
+                        <MechStatus label={"Speed"} icon={<SvgLoadoutSpeed />} value={mostPopularMech.speed} />
+                        <MechStatus
+                            label={"Power Core Recharge Rate"}
+                            icon={<SvgLoadoutPowerCoreRegen />}
+                            value={mostPopularMech.power_core?.recharge_rate || 0}
+                        />
+                        <MechStatus label={"Power Core Capacity"} icon={<SvgLoadoutPowerCoreCapacity />} value={mostPopularMech.power_core?.capacity || 0} />
                     </Stack>
 
-                    <Stack spacing=".5rem">
-                        <SvgPowerCore fill={colors.powerCore} size="2.5rem" />
-                        <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
-                            <Box
-                                sx={{
-                                    width: "1rem",
-                                    height: ".3rem",
-                                    backgroundColor: mostPopularMech.power_core ? colors.powerCore : `${colors.offWhite}20`,
-                                }}
-                            />
-                        </Stack>
+                    {/* Mech battle stat*/}
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <NiceTooltip placement="top-start" text="Total kills">
+                            <Typography whiteSpace="nowrap">
+                                <SvgMechKills inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_kills)}
+                            </Typography>
+                        </NiceTooltip>
+                        <NiceTooltip placement="top-start" text="Total deaths">
+                            <Typography whiteSpace="nowrap">
+                                <SvgMechDeaths inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_deaths)}
+                            </Typography>
+                        </NiceTooltip>
+
+                        <NiceTooltip placement="top-start" text="Total wins">
+                            <Typography whiteSpace="nowrap">
+                                <SvgMechWins inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_wins)}
+                            </Typography>
+                        </NiceTooltip>
+
+                        <NiceTooltip placement="top-start" text="Total losses">
+                            <Typography whiteSpace="nowrap">
+                                <SvgMechLosses inline size="1.6rem" /> {numFormatter(mostPopularMech.stats.total_losses)}
+                            </Typography>
+                        </NiceTooltip>
                     </Stack>
 
-                    <Stack spacing=".5rem">
-                        <SvgSkin fill={colors.chassisSkin} size="2.5rem" />
-                        <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
-                            <Box
-                                sx={{
-                                    width: "1rem",
-                                    height: ".3rem",
-                                    backgroundColor: mostPopularMech.chassis_skin_id ? colors.chassisSkin : `${colors.offWhite}20`,
-                                }}
-                            />
+                    {/* Mech Equipments*/}
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <Stack spacing=".5rem">
+                            <SvgWeapons fill={colors.weapons} size="2.5rem" />
+                            <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
+                                {mostPopularMech.weapon_slots &&
+                                    mostPopularMech.weapon_slots.map((ws, i) => (
+                                        <Box
+                                            key={i}
+                                            sx={{
+                                                width: "1rem",
+                                                height: ".3rem",
+                                                backgroundColor: ws.weapon ? colors.weapons : `${colors.offWhite}20`,
+                                            }}
+                                        />
+                                    ))}
+                            </Stack>
+                        </Stack>
+
+                        <Stack spacing=".5rem">
+                            <SvgUtilities fill={colors.utilities} size="2.5rem" />
+                            <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
+                                {mostPopularMech.utilities &&
+                                    mostPopularMech.utilities.map((us, i) => (
+                                        <Box
+                                            key={i}
+                                            sx={{
+                                                width: "1rem",
+                                                height: ".3rem",
+                                                backgroundColor: us.utility ? colors.utilities : `${colors.offWhite}20`,
+                                            }}
+                                        />
+                                    ))}
+                            </Stack>
+                        </Stack>
+
+                        <Stack spacing=".5rem">
+                            <SvgPowerCore fill={colors.powerCore} size="2.5rem" />
+                            <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
+                                <Box
+                                    sx={{
+                                        width: "1rem",
+                                        height: ".3rem",
+                                        backgroundColor: mostPopularMech.power_core ? colors.powerCore : `${colors.offWhite}20`,
+                                    }}
+                                />
+                            </Stack>
+                        </Stack>
+
+                        <Stack spacing=".5rem">
+                            <SvgSkin fill={colors.chassisSkin} size="2.5rem" />
+                            <Stack direction="row" alignItems="center" justifyContent="center" spacing=".25rem">
+                                <Box
+                                    sx={{
+                                        width: "1rem",
+                                        height: ".3rem",
+                                        backgroundColor: mostPopularMech.chassis_skin_id ? colors.chassisSkin : `${colors.offWhite}20`,
+                                    }}
+                                />
+                            </Stack>
                         </Stack>
                     </Stack>
                 </Stack>
