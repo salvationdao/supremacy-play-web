3ab86c4537d9082490eb76deaf93e3664376a3ba
James Li <jayli3n@gmail.com>
2022-11-28 12:38:57 +0800

    Syles

--- src/components/Common/Nice/NiceStepper.tsx
+++ src/components/Common/Nice/NiceStepper.tsx
@@ -1,43 +1,61 @@
-import { Stack, Step, Stepper, Typography } from "@mui/material"
+import { Stack, Step, Stepper, StepperProps, Typography } from "@mui/material"
 import { useTheme } from "../../../containers/theme"
+
+const ICON_SIZE = "2.5rem"
 
 export interface Step {
     label: string
 }
 
-export const NiceStepper = ({
-    steps,
-    activeStep,
-    completedSteps,
-    handleStep,
-}: {
+export interface NiceStepperProps extends StepperProps {
     steps: Step[]
     activeStep: number // Starts from 1, 2, 3 etc. not 0.
     handleStep: (step: number) => void
     completedSteps?: { [k: number]: boolean }
-}) => {
+}
+
+export const NiceStepper = ({ steps, activeStep, completedSteps, handleStep, sx, ...props }: NiceStepperProps) => {
     const theme = useTheme()
 
     return (
-        <Stepper activeStep={activeStep}>
+        <Stepper
+            activeStep={activeStep}
+            sx={{
+                ...(props.orientation === "vertical"
+                    ? {
+                          ".MuiStepConnector-root": {
+                              ml: `calc(${ICON_SIZE} / 2)`,
+                              my: ".6rem",
+                          },
+                      }
+                    : {}),
+                ...sx,
+            }}
+            {...props}
+        >
             {steps.map((step, index) => (
                 <Step key={`nice-stepper-${index}`} completed={completedSteps ? completedSteps[index] : false}>
-                    <Stack onClick={() => handleStep(index)} direction="row" alignItems="center" spacing=".8rem">
+                    <Stack
+                        onClick={() => handleStep(index)}
+                        direction="row"
+                        alignItems="center"
+                        spacing=".8rem"
+                        sx={{ opacity: index === activeStep ? 1 : 0.3 }}
+                    >
                         <Stack
                             alignItems="center"
                             justifyContent="center"
                             sx={{
                                 backgroundColor: theme.factionTheme.primary,
                                 borderRadius: "50%",
-                                width: "2.5rem",
-                                height: "2.5rem",
-                                opacity: index === activeStep ? 1 : 0.3,
+                                width: ICON_SIZE,
+                                height: ICON_SIZE,
                             }}
                         >
                             <Typography fontWeight="bold">{index + 1}</Typography>
                         </Stack>
 
-                        <Typography>{step.label}</Typography>
+                        <Typography lineHeight={1.2}>{step.label}</Typography>
                     </Stack>
                 </Step>
             ))}
--- src/components/Lobbies/CreateLobbyFormModal/CreateLobbyFormModal.tsx
+++ src/components/Lobbies/CreateLobbyFormModal/CreateLobbyFormModal.tsx
@@ -1,17 +1,79 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, Divider, Stack, Typography } from "@mui/material"
+import moment from "moment"
 import React, { useCallback, useMemo, useState } from "react"
+import { useForm } from "react-hook-form"
 import { useTheme } from "../../../containers/theme"
 import { colors, fonts } from "../../../theme/theme"
+import { NewMechStruct } from "../../../types"
 import { NiceButton } from "../../Common/Nice/NiceButton"
 import { NiceModal } from "../../Common/Nice/NiceModal"
 import { NiceStepper } from "../../Common/Nice/NiceStepper"
 import { FeesRewards } from "./FeesRewards"
 import { RoomSettings } from "./RoomSettings"
 
-const steps = [{ label: "Room Settings" }, { label: "Fees & Rewards" }, { label: "Deploy Mechs" }]
+const steps = [{ label: "Room Settings" }, { label: "Fees & Rewards" }, { label: "Deploy Mechs" }, { label: "Overview" }]
+
+export enum Accessibility {
+    Public = "PUBLIC",
+    Private = "PRIVATE",
+}
+
+export enum Scheduling {
+    SetTime = "SCHEDULED TIME & DATE",
+    OnReady = "ON FULL LOBBY",
+}
+
+export interface CreateLobbyFormFields {
+    // Step 1
+    name: string
+    accessibility: Accessibility
+    access_code: string
+    max_deploy_number: number
+    game_map_id: string
+    scheduling_type: Scheduling
+    wont_start_until_date: moment.Moment
+    wont_start_until_time: moment.Moment
+
+    // Step 2
+    entry_fee: number
+    first_faction_cut: number // Percentage (e.g. 75)
+    second_faction_cut: number // Percentage (e.g. 75)
+    third_faction_cut: number // Percentage (e.g. 75)
+    extra_reward: number
+
+    // Step 3
+    selected_mechs: NewMechStruct[]
+}
 
 export const CreateLobbyFormModal = React.memo(function CreateLobbyFormModal({ open, onClose }: { open: boolean; onClose: () => void }) {
     const theme = useTheme()
+
+    //  Form
+    const formMethods = useForm<CreateLobbyFormFields>({
+        defaultValues: {
+            // Step 1
+            name: "",
+            access_code: "",
+            accessibility: Accessibility.Public,
+            max_deploy_number: 3,
+            game_map_id: "",
+            scheduling_type: Scheduling.OnReady,
+            wont_start_until_date: moment(),
+            wont_start_until_time: moment(),
+
+            // Step 2
+            entry_fee: 0,
+            first_faction_cut: 75,
+            second_faction_cut: 25,
+            third_faction_cut: 0,
+            extra_reward: 0,
+
+            // Step 3
+            selected_mechs: [],
+        },
+    })
+
+    // Stepper
     const [activeStep, setActiveStep] = useState(0)
     const [completedSteps, setCompletedSteps] = React.useState<{
         [k: number]: boolean
@@ -57,32 +119,46 @@ export const CreateLobbyFormModal = React.memo(function CreateLobbyFormModal({ o
     }, [activeStep])
 
     return (
-        <NiceModal open={open} onClose={onClose} sx={{ p: "1.8rem 2.5rem", maxHeight: "calc(100vh - 20rem)", minWidth: "60rem" }}>
+        <NiceModal open={open} onClose={onClose} sx={{ p: "1.8rem 2.5rem", maxHeight: "calc(100vh - 20rem)", minWidth: "70rem" }}>
             <Stack spacing="1.5rem">
                 <Typography variant="h6" fontFamily={fonts.nostromoBlack}>
                     Create Lobby
                 </Typography>
 
-                {/* Stepper */}
-                <NiceStepper steps={steps} activeStep={activeStep} handleStep={handleStep} completedSteps={completedSteps} />
+                <Stack direction="row" alignItems="stretch" spacing="2rem">
+                    {/* Stepper */}
+                    <NiceStepper
+                        steps={steps}
+                        activeStep={activeStep}
+                        handleStep={handleStep}
+                        completedSteps={completedSteps}
+                        orientation="vertical"
+                        sx={{ ".MuiStepConnector-root": { ml: "1.25rem" } }}
+                    />
 
-                <Box sx={{ minHeight: "5rem" }}>{stepForm}</Box>
+                    <Divider flexItem orientation="vertical" />
 
-                {/* Bottom buttons */}
-                <Stack direction="row" alignItems="center" justifyContent="flex-end" spacing="1rem">
-                    <NiceButton buttonColor={theme.factionTheme.primary} disabled={activeStep === 0} onClick={handleBack}>
-                        Back
-                    </NiceButton>
+                    <Stack spacing="1rem" flex={1}>
+                        {/* The form questions */}
+                        <Box flex={1}>{stepForm}</Box>
 
-                    {isLastStep ? (
-                        <NiceButton buttonColor={colors.green} onClick={handleNext}>
-                            Create Lobby
-                        </NiceButton>
-                    ) : (
-                        <NiceButton buttonColor={theme.factionTheme.primary} onClick={handleNext}>
-                            Next
-                        </NiceButton>
-                    )}
+                        {/* Bottom buttons */}
+                        <Stack direction="row" alignItems="center" justifyContent="flex-end" spacing="1rem">
+                            <NiceButton buttonColor={theme.factionTheme.primary} disabled={activeStep === 0} onClick={handleBack}>
+                                Back
+                            </NiceButton>
+
+                            {isLastStep ? (
+                                <NiceButton buttonColor={colors.green} onClick={handleNext}>
+                                    Create Lobby
+                                </NiceButton>
+                            ) : (
+                                <NiceButton buttonColor={theme.factionTheme.primary} onClick={handleNext}>
+                                    Next
+                                </NiceButton>
+                            )}
+                        </Stack>
+                    </Stack>
                 </Stack>
             </Stack>
         </NiceModal>
