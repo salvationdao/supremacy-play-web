a00eb6b27f55787623dad960ebcd401e22583af5
felix t <41817193+o2bomb@users.noreply.github.com>
2022-11-29 15:54:31 +0800

    Good styles

--- src/components/BattleArena/BigDisplay/BattleIntro/BattleIntro.tsx
+++ src/components/BattleArena/BigDisplay/BattleIntro/BattleIntro.tsx
@@ -6,7 +6,7 @@ import { useAuth, useGlobalNotifications, useSupremacy } from "../../../../conta
 import { useGameServerCommandsFaction } from "../../../../hooks/useGameServer"
 import { GameServerKeys } from "../../../../keys"
 import { pulseEffect } from "../../../../theme/keyframes"
-import { colors } from "../../../../theme/theme"
+import { colors, fonts } from "../../../../theme/theme"
 import { BattleLobby } from "../../../../types/battle_queue"
 import { NiceBoxThing } from "../../../Common/Nice/NiceBoxThing"
 import { NiceButton } from "../../../Common/Nice/NiceButton"
@@ -122,6 +122,7 @@ const FactionRow = ({ index, lobby }: FactionRowProps) => {
 
     const supporterContent = useMemo(() => {
         const isMyFaction = factionID === lobby.faction.id
+        const boxSize = "4rem"
         if (!isMyFaction) {
             return new Array(MAX_SUPPORTER_SLOTS).fill(null).map((_, index) => (
                 <NiceBoxThing
@@ -132,6 +133,13 @@ const FactionRow = ({ index, lobby }: FactionRowProps) => {
                     }}
                     background={{
                         colors: [theme.background],
+                    }}
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: boxSize,
+                        width: boxSize,
                     }}
                 >
                     <SvgQuestionMark2 fill={theme.s700} />
@@ -151,8 +159,8 @@ const FactionRow = ({ index, lobby }: FactionRowProps) => {
                                 src={os.avatar_url}
                                 alt={`${os.username}'s avatar`}
                                 sx={{
-                                    height: "3rem",
-                                    width: "3rem",
+                                    height: boxSize,
+                                    width: boxSize,
                                     border: `1px solid ${theme.primary}`,
                                 }}
                             />
@@ -164,8 +172,8 @@ const FactionRow = ({ index, lobby }: FactionRowProps) => {
                             onClick={onOptIn}
                             buttonColor={theme.primary}
                             sx={{
-                                height: "3rem",
-                                width: "3rem",
+                                height: boxSize,
+                                width: boxSize,
                                 animation: `${pulseEffect} 1s infinite`,
                             }}
                         >
@@ -175,128 +183,194 @@ const FactionRow = ({ index, lobby }: FactionRowProps) => {
                     {optedInSupporterSlots.length - MAX_SUPPORTERS_TO_SHOW > 0 && (
                         <Typography
                             sx={{
-                                height: "3rem",
-                                width: "3rem",
+                                height: boxSize,
+                                width: boxSize,
                             }}
                         >
                             +{optedInSupporterSlots.length - MAX_SUPPORTERS_TO_SHOW} more
                         </Typography>
                     )}
-                    {
-                        optInError && <TypographyTruncated sx={{
-                            color: colors.red
-                        }}>{optInError}</TypographyTruncated>
-                    }
+                    {optInError && (
+                        <TypographyTruncated
+                            sx={{
+                                color: colors.red,
+                            }}
+                        >
+                            {optInError}
+                        </TypographyTruncated>
+                    )}
                 </>
             )
         }
-        return selectedSupporterSlots.map((ss, index) => (
-            <Box key={index} position="relative">
-                <Box
-                    component="img"
-                    src={ss.avatar_url}
-                    alt={`${ss.username}'s avatar`}
-                    sx={{
-                        height: "3rem",
-                        width: "3rem",
-                        border: `1px solid ${theme.primary}`,
-                    }}
-                />
-                <Stack
-                    sx={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        bottom: 0,
-                        right: 0,
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <SvgCheckMark fill={colors.green} />
-                </Stack>
-            </Box>
-        ))
+        return (
+            <>
+                {selectedSupporterSlots.map((ss, index) => (
+                    <NiceBoxThing
+                        key={index}
+                        border={{
+                            color: theme.primary,
+                            thickness: "lean",
+                        }}
+                        background={{
+                            colors: [theme.background],
+                        }}
+                        sx={{
+                            position: "relative",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: boxSize,
+                            width: boxSize,
+                        }}
+                    >
+                        {ss.avatar_url && (
+                            <Box
+                                component="img"
+                                src={ss.avatar_url}
+                                alt={`${ss.username}'s avatar`}
+                                sx={{
+                                    width: "100%",
+                                    objectFit: "cover",
+                                }}
+                            />
+                        )}
+                        <Stack
+                            sx={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                bottom: 0,
+                                right: 0,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <SvgCheckMark fill={colors.green} />
+                        </Stack>
+                    </NiceBoxThing>
+                ))}
+            </>
+        )
     }, [factionID, lobby.faction.id, onOptIn, optInError, optedInSupporterSlots, selectedSupporterSlots, theme.background, theme.primary, theme.s700])
 
     return (
         <Slide in direction={index % 2 === 0 ? "left" : "right"}>
             <Stack
-                direction="row"
                 sx={{
+                    flex: 1,
+                    width: "100%",
                     p: "2rem 4rem",
-                    justifyContent: "space-around",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: `${theme.s600}66`,
+                    "&:not(:last-child)": {
+                        borderBottom: `1px solid ${colors.black2}aa`,
+                    },
                 }}
             >
-                <Stack alignItems="center">
-                    {/* Faction logo */}
-                    <Box
-                        component="img"
-                        src={lobby.faction.logo_url}
+                <Stack
+                    direction="row"
+                    spacing="2rem"
+                    sx={{
+                        width: "100%",
+                        maxWidth: 1100,
+                        m: "0 auto",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Stack
+                        alignItems="center"
+                        spacing="1rem"
                         sx={{
-                            width: 80,
-                            height: 80,
-                            objectFit: "contain",
-                        }}
-                    />
-                    {/* Supporters */}
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexWrap: "wrap",
-                            justifyContent: "center",
+                            width: "100%",
+                            maxWidth: 150,
                         }}
                     >
-                        {supporterContent}
-                    </Box>
-                </Stack>
-                {lobby.mechSlots.map((ms, index) => (
-                    <Stack key={index}>
-                        <Stack direction="row" spacing=".5rem">
-                            {/* Mech avatar */}
-                            <Box
-                                component="img"
-                                src={ms.avatar_url}
-                                sx={{
-                                    width: 100,
-                                    objectFit: "cover",
-                                    border: `1px solid ${colors.black2}`,
-                                }}
-                            />
-                            {/* Weapon slots */}
-                            <Stack spacing=".5rem">
-                                {ms.weapon_slots &&
-                                    ms.weapon_slots.map((w, index) => (
-                                        <Box
-                                            key={index}
-                                            component="img"
-                                            src={w.weapon?.avatar_url}
-                                            sx={{
-                                                flex: 1,
-                                                flexBasis: "4rem",
-                                                backgroundColor: theme.background,
-                                                objectFit: "contain",
-                                            }}
-                                        />
-                                    ))}
-                            </Stack>
-                        </Stack>
-                        {/* Mech label */}
-                        <TypographyTruncated>{ms.name || ms.label}</TypographyTruncated>
-                        {/* Owner */}
-                        <TypographyTruncated
-                            variant="h6"
+                        {/* Faction logo */}
+                        <Box
+                            component="img"
+                            src={lobby.faction.logo_url}
                             sx={{
-                                color: ms.owner_id === userID ? colors.gold : theme.primary,
-                                fontWeight: "bold",
-                                mt: ".3rem !important",
+                                width: "100%",
+                                objectFit: "contain",
+                            }}
+                        />
+                        {/* Supporters */}
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                gap: "1rem",
                             }}
                         >
-                            <SvgUserDiamond size="2.5rem" inline fill={ms.owner_id === userID ? colors.gold : lobby.faction.palette.primary} />{" "}
-                            {ms.owner.username}#{ms.owner.gid}
-                        </TypographyTruncated>
+                            {supporterContent}
+                        </Box>
                     </Stack>
-                ))}
+                    {lobby.mechSlots.map((ms, index) => (
+                        <Stack
+                            key={index}
+                            sx={{
+                                width: "100%",
+                                maxWidth: 200,
+                            }}
+                        >
+                            <Stack direction="row" spacing=".5rem">
+                                {/* Mech avatar */}
+                                <Box
+                                    component="img"
+                                    src={ms.avatar_url}
+                                    sx={{
+                                        width: "100%",
+                                        maxWidth: 160,
+                                        objectFit: "cover",
+                                        border: `1px solid ${colors.black2}`,
+                                    }}
+                                />
+                                {/* Weapon slots */}
+                                <Stack spacing=".5rem" flex={1}>
+                                    {ms.weapon_slots &&
+                                        ms.weapon_slots.map((w, index) => (
+                                            <Box
+                                                key={index}
+                                                component="img"
+                                                src={w.weapon?.avatar_url}
+                                                sx={{
+                                                    width: "100%",
+                                                    objectFit: "contain",
+                                                    backgroundColor: theme.background,
+                                                    border: `1px solid ${colors.black2}`,
+                                                }}
+                                            />
+                                        ))}
+                                </Stack>
+                            </Stack>
+                            {/* Mech label */}
+                            <TypographyTruncated
+                                sx={{
+                                    fontFamily: fonts.nostromoBold,
+                                    fontSize: "2rem",
+                                }}
+                            >
+                                {ms.name || ms.label}
+                            </TypographyTruncated>
+                            {/* Owner */}
+                            <TypographyTruncated
+                                variant="h6"
+                                sx={{
+                                    color: ms.owner_id === userID ? colors.gold : "white",
+                                    fontWeight: "bold",
+                                    mt: ".3rem !important",
+                                }}
+                            >
+                                <SvgUserDiamond size="2.5rem" inline fill={ms.owner_id === userID ? colors.gold : lobby.faction.palette.primary} />{" "}
+                                {ms.owner.username}#{ms.owner.gid}
+                            </TypographyTruncated>
+                        </Stack>
+                    ))}
+                </Stack>
             </Stack>
         </Slide>
     )
