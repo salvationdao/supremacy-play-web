afb59cfa093dffdf6fdd93e18930846d2d3b2bba
James Li <jayli3n@gmail.com>
2022-12-05 13:38:36 +0800

    Done

--- src/components/Common/GlobalBrowserNotifications.tsx
+++ src/components/Common/GlobalBrowserNotifications.tsx
@@ -1,14 +1,49 @@
-import React, { useEffect } from "react"
+import React from "react"
+import { useHistory } from "react-router-dom"
 import { useGlobalNotifications } from "../../containers"
+import { useGameServerSubscriptionSecuredUser } from "../../hooks/useGameServer"
+import { GameServerKeys } from "../../keys"
+
+type BrowserNotification =
+    | {
+          title: "MECH_IN_BATTLE"
+          data: {
+              arena_id: string
+              arena_name: string
+              mech_alerts: {
+                  id: string
+                  name: string
+              }[]
+          }
+      }
+    | {
+          title: "TEXT"
+          data: {
+              message: string
+          }
+      }
 
 export const GlobalBrowserNotifications = React.memo(function GlobalBrowserNotifications() {
     const { sendBrowserNotification } = useGlobalNotifications()
+    const history = useHistory()
 
-    useEffect(() => {
-        setTimeout(() => {
-            sendBrowserNotification.current({ title: "title please", text: "taggg" })
-        }, 5000)
-    }, [sendBrowserNotification])
+    useGameServerSubscriptionSecuredUser<BrowserNotification>(
+        {
+            URI: "/browser_alert",
+            key: GameServerKeys.SubBrowserNotifications,
+        },
+        (payload) => {
+            if (!payload) return
+
+            if (payload.title === "MECH_IN_BATTLE") {
+                sendBrowserNotification.current({
+                    title: "Your mechs are in battle!",
+                    text: `You have ${payload.data.mech_alerts.length} in battle arena ${payload.data.arena_name}.`,
+                    onClick: () => history.push(`/?arenaName=${payload.data.arena_name}`),
+                })
+            }
+        },
+    )
 
     return null
 })
